1a6400eefa41933dcb626a6b543aff229609b15d
drownedout <danielcdonnelly@gmail.com>
2015-12-07 20:55:19 -0500

    Added  Case views, controller

---
+++ dcaf_case__management/app/controllers/case_controller.rb
@@ -0,0 +1,17 @@
+class CaseController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+end
--- dcaf_case__management/config/routes.rb
+++ dcaf_case__management/config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  resources :case
+  root 'case#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
