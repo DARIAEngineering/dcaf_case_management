005c20cd1612c46be7803a3ce8f334dbd35941cf
drownedout <danielcdonnelly@gmail.com>
2015-12-21 20:36:37 -0500

    Configured Mongo DB

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,9 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'sprockets/railtie'
+require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -21,6 +24,10 @@ module DcafCaseManagement
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    # config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g| 
+      g.orm :mongoid
+    end
   end
 end
