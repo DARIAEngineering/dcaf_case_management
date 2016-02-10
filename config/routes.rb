de91ff18daee4e72d0c0307c6bc70f9523ff26ac
Alexis Johnson <alexis.johnson052@gmail.com>
2016-02-10 16:54:01 -0500

    create and new routes for call

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  authenticate :user do 
+  authenticate :user do
     root to: 'cases#index', as: :authenticated_root
     resources :cases
-    resources :patients, only: [ :create ]
+    resources :patients, only: [ :create ] do
+      member do
+        resources :call, only: [ :new, :create ]
+      end
+    end
   end
   root :to => redirect('/users/sign_in')
   devise_for :users
