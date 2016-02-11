1b7f583575cd03b35ee90ec624f6517620e3f3c2
Alexis Johnson <alexis.johnson052@gmail.com>
2016-02-11 09:26:48 -0500

    pass call status to controller

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -1,6 +1,12 @@
 class CallsController < ApplicationController
 
   def create
+    byebug
+    patient = Patient.find(params[:id])
+    @call = patient.calls.new(params[:status])
+		if @call.save
+			redirect_to root_path
+		end
   end
 
 end
