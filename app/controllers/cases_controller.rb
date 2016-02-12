97d0d639c287844e3abddd372eb5403e0fd94df5
Alexis Johnson <alexis.johnson052@gmail.com>
2016-02-12 17:51:14 -0500

    update cases#search for first or last search

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -1,13 +1,13 @@
 class CasesController < ApplicationController
   def index
-  	@patient = Patient.new
     @urgent_cases = Case.where(urgent_flag: true)
   end
 
   def search
-    @patient = Patient.new
     @urgent_cases = Case.where(urgent_flag: true)
-    patients = Patient.where(last_name: params[:keyword])
+    last_match = Patient.where(last_name: params[:keyword])
+    first_match = Patient.where(first_name: params[:keyword])
+    patients = last_match + first_match
     @results = []
     patients.each do |patient|
       @results << patient.cases.most_recent
