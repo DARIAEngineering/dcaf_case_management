21e91e44657b1ad30fcef68091e52b7cfe8b41c3
colinxfleming <c3flemin@gmail.com>
2016-02-15 15:26:04 -0500

    stub auth test

---
+++ test/integration/authorization_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class AuthorizationTest < ActionDispatch::IntegrationTest
+  describe 'sign in page' do 
+    it 'should root to the sign in page if not authenticated' do 
+      visit '/'
+      assert redirect?
+    end
+  end
+
+    # visit '/'
+
+  describe 'logging in successfully' do 
+  end
+
+  describe 'logging in unsuccessfully' do 
+  end
+
+  # test "the truth" do
+  #   assert true
+  # end
+end
