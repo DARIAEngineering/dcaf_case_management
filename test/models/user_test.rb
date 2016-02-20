b2bf688462cdee51123ce2c68e9cc5bfbcb88291
colinxfleming <c3flemin@gmail.com>
2016-02-20 18:26:26 -0500

    basic validation tests for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = create :user
+  end
+
+  # devise handles most of this
+
+  describe 'basic validations' do 
+    it 'should be able to build an object' do 
+      assert @user.valid? 
+    end
+
+    %w(email name).each do |attribute|
+      it "should require content in #{attribute}" do 
+        @user[attribute.to_sym] = nil 
+        assert_not @user.valid? 
+        assert_equal "can't be blank", @user.errors.messages[attribute.to_sym].first
+      end
+    end
+  end
 end
