2a5182086c5ec432f3fb2876e41f6b2dfe63bd19
colinxfleming <c3flemin@gmail.com>
2016-02-20 00:36:43 -0500

    better description

--- test/integration/routing_test.rb
+++ test/integration/routing_test.rb
@@ -5,7 +5,7 @@ class RoutingTest < ActionDispatch::IntegrationTest
     visit root_url 
   end
 
-  it 'should root to the sign in page' do 
+  it 'should have a root with a login form' do 
     assert_equal current_path, new_user_session_path
     assert_text 'You need to sign in or sign up before continuing'
     assert has_field? 'Email'
