a08ee6171996a5b39beee07bb71407e355640a90
colinxfleming <c3flemin@gmail.com>
2016-02-20 18:14:08 -0500

    finish off core patients controller test

--- test/controllers/patients_controller_test.rb
+++ test/controllers/patients_controller_test.rb
@@ -18,11 +18,27 @@ class PatientsControllerTest < ActionController::TestCase
     end
 
     it 'should redirect to the root path afterwards' do 
+      post :create, patient: @patient
+      assert_redirected_to root_path
     end
 
-    it 'should require a name and primary phone number' do 
-      @patient[:name] = nil 
-      assert_no_difference 'Patient.count' do 
+    it 'should fail to save if name is blank' do 
+      @patient[:name] = ''
+      assert_no_difference 'Patient.count' do
+        post :create, patient: @patient
+      end
+    end
+
+    it 'should fail to save if primary phone is blank is blank' do 
+      @patient[:primary_phone] = ''
+      assert_no_difference 'Patient.count' do
+        post :create, patient: @patient
+      end
+    end
+
+    it 'should allow save if primary phone is blank is blank' do 
+      @patient[:secondary_phone] = ''
+      assert_difference 'Patient.count', 1 do
         post :create, patient: @patient
       end
     end
