fea4c365732bb6def6cb54c9b04331f7caab2ffd
colinxfleming <c3flemin@gmail.com>
2016-02-21 10:58:22 -0500

    remove redundant include

--- test/controllers/cases_controller_test.rb
+++ test/controllers/cases_controller_test.rb
@@ -1,8 +1,6 @@
 require 'test_helper'
 
 class CasesControllerTest < ActionController::TestCase
-  include Devise::TestHelpers
-  
   it "should get index" do
     get :index
     assert_response :success
