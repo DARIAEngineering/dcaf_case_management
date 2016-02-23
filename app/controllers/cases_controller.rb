aac659731a473543ba25f50da422f48384193d0a
Alexis Johnson <alexis.johnson052@gmail.com>
2016-02-22 19:06:05 -0500

    fix reference to first_match in search

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -8,10 +8,10 @@ class CasesController < ApplicationController
   def search
     @patient = Patient.new
     @urgent_cases = Case.where(urgent_flag: true)
-    last_match = Patient.where(name: params[:keyword])
+    name_match = Patient.where(name: params[:keyword])
     primary_match = Patient.where(primary_phone: params[:keyword])
     secondary_match = Patient.where(secondary_phone: params[:keyword])
-    patients = last_match + first_match + primary_match + secondary_match
+    patients = name_match + primary_match + secondary_match
     @results = []
     patients.each do |patient|
       @results << patient.cases.most_recent
