d84c2ee29d428f9740649326c3c47d2a21ce4393
Alexis Johnson <alexis.johnson052@gmail.com>
2016-02-22 23:14:01 -0500

    remove old search results upon making a new search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # whitelists name attributes in devise
+  # whitelists name attribute in devise
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
     devise_parameter_sanitizer.for(:account_update) << :name
