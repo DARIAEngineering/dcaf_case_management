c5f0e8f203531f8bb29fba7d857792d6f8b7c001
colinxfleming <c3flemin@gmail.com>
2016-02-25 00:50:37 -0500

    slight routes rmx

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
     resources :patients, only: [ :create ]
     patch 'users/:user_id/add_case/:id', to: 'users#add_case'
     patch 'users/:user_id/remove_case/:id', to: 'users#remove_case'
+    post 'search', to: 'cases#search'
   end
   root :to => redirect('/users/sign_in')
   devise_for :users
-  post 'search', to: 'cases#search'
 end
