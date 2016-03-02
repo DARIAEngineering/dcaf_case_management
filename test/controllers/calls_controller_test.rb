1b653e9f56278b30cc538ff95840a4d88ecbe31a
Kevin Wei <kevin.tw.wei@gmail.com>
2016-03-02 18:49:27 -0500

    fix rest of controller

--- test/controllers/calls_controller_test.rb
+++ test/controllers/calls_controller_test.rb
@@ -13,21 +13,21 @@ class CallsControllerTest < ActionController::TestCase
     end
 
     it 'should create and save a new call' do
-      assert_difference 'Pregnancy.find(@prengancy).calls.count', 1 do
-        post :create, call: @call, id: @prengancy
+      assert_difference 'Pregnancy.find(@pregnancy).calls.count', 1 do
+        post :create, call: @call, id: @pregnancy
       end
       assert_response :redirect
     end
 
     it 'should redirect to the root path afterwards' do
-      post :create, call: @call, id: @prengancy
+      post :create, call: @call, id: @pregnancy
       assert_redirected_to root_path
     end
 
     it 'should not save if status is blank for some reason' do
       @call[:status] = nil
-      assert_no_difference 'Pregnancy.find(@prengancy).calls.count' do
-        post :create, call: @call, id: @prengancy
+      assert_no_difference 'Pregnancy.find(@pregnancy).calls.count' do
+        post :create, call: @call, id: @pregnancy
       end
     end
   end
