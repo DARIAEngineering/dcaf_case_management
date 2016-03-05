edc3f4d5edd1ce1c0a6851bd2d267129f720a7a1
colinxfleming <c3flemin@gmail.com>
2016-03-04 22:34:26 -0500

    move add_pregnancy to an ajax call

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,22 @@
 class UsersController < ApplicationController
+  before_action :get_user_and_pregnancy
+
   def add_pregnancy
-    p = Pregnancy.find(params[:id])
-    user = User.find(params[:user_id])
-    user.pregnancies << p
-    redirect_to root_path
+    @user.pregnancies << @pregnancy
+    # @all_pregnancies = @user.pregnancies
+    # redirect_to root_path
+    respond_to { |format| format.js }
   end
 
   def remove_pregnancy
-    p = Pregnancy.find(params[:id])
-    user = User.find(params[:user_id])
-    user.pregnancies.delete(p)
+    @user.pregnancies.delete(@pregnancy)
     redirect_to root_path
+  end
+
+  private 
+
+  def get_user_and_pregnancy
+    @pregnancy = Pregnancy.find(params[:id])
+    @user = User.find(params[:user_id])
   end
 end
