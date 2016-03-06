18022ef4b8f96a06ecbee177b6ee9587802d17b7
colinxfleming <c3flemin@gmail.com>
2016-03-06 00:15:09 -0500

    working mailer tests

--- test/integration/password_reset_test.rb
+++ test/integration/password_reset_test.rb
@@ -3,14 +3,48 @@ require 'test_helper'
 class PasswordResetTest < ActionDispatch::IntegrationTest
   def setup
     @user = create :user
-    visit root_url 
   end
 
-  describe 'resending password' do 
-    it 'should work' do 
+  describe 'password reset page accessibility' do 
+    before do
+      visit root_url 
+    end 
+
+    it 'should have a link to the password reset page' do 
       assert has_link? 'Forgot password?'
       click_link 'Forgot password?'
-      assert_routing '/users/password/new'
+      assert_routing new_user_password_path, controller: 'devise/passwords', action: 'new'
+      assert_text 'Password Reset'
+    end
+  end
+
+  describe 'password reset page function' do 
+    before do 
+      visit new_user_password_path
+    end
+
+    after do 
+      Devise.mailer.deliveries.clear
+    end
+
+    it 'should errror if email does not exist' do 
+      fill_in 'Email', with: 'not_a_real_email@gmail.com'
+      click_button 'Send me password reset instructions' 
+      assert_text 'Email not found'
+      assert_routing new_user_password_path, controller: 'devise/passwords', action: 'new'
+      assert_equal Devise.mailer.deliveries.count, 0
+    end
+
+    it 'should send a password reset if email does exist' do 
+      fill_in 'Email', with: @user.email
+      click_button 'Send me password reset instructions' 
+      assert_text 'You will receive an email with instructions on how to reset your password in a few minutes.'
+      assert_routing new_user_session_path, controller: 'devise/sessions', action: 'new'
+
+      # confirm email got sent
+      assert_equal Devise.mailer.deliveries.count, 1
+      
+
     end
   end
 end
