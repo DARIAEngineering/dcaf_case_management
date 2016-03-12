9889be4befd08c2e8f6edb05f2b624cc184fab96
colinxfleming <c3flemin@gmail.com>
2016-03-12 11:35:06 -0500

    minimally functional test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,9 +2,18 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
   def setup
+    @user = create :user
+    sign_in @user
+    @patient_1 = create :patient, name: 'Susan Everyteen'
+    @pregnancy_1 = create :pregnancy, patient: @patient_1
+    @patient_2 = create :patient, name: 'Yolo Goat'
+    @pregnancy_2 = create :pregnancy, patient: @patient_2
   end
 
   describe 'add_pregnancy method' do
+    it 'should do a thing' do 
+      patch :add_pregnancy, id: @pregnancy_1, user_id: @user, format: :js
+    end
   end
 
   describe 'remove_pregnancy method' do 
