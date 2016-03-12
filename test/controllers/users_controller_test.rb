67836822002639e93aa94d9b62b179fc7216f70d
colinxfleming <c3flemin@gmail.com>
2016-03-12 12:08:25 -0500

    confirm some test behavior, good to go

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  def setup
+  before do
     @user = create :user
     sign_in @user
     @patient_1 = create :patient, name: 'Susan Everyteen'
@@ -11,13 +11,15 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   describe 'add_pregnancy method' do
-    it 'should respond successfully' do 
+    before do 
       patch :add_pregnancy, id: @pregnancy_1, user_id: @user, format: :js
+    end
+
+    it 'should respond successfully' do 
       assert_response :success
     end
 
     it 'should add a patient to a users call list' do 
-      patch :add_pregnancy, id: @pregnancy_1, user_id: @user, format: :js
       assert_equal @user.pregnancies.count, 1
       assert_difference '@user.pregnancies.count', 1 do
         patch :add_pregnancy, id: @pregnancy_2, user_id: @user, format: :js
@@ -25,16 +27,49 @@ class UsersControllerTest < ActionController::TestCase
       assert_equal @user.pregnancies.count, 2
     end
 
+    it 'should not adjust the count if a pregnancy is already in the list' do 
+      assert_no_difference '@user.pregnancies.count' do 
+        patch :add_pregnancy, id: @pregnancy_1, user_id: @user, format: :js
+      end
+    end
+
     it 'should should return bad request on sketch ids' do 
       patch :add_pregnancy, id: '12345678', user_id: @user, format: :js
       assert_response :bad_request
+      patch :add_pregnancy, id: @pregnancy_1, user_id: 'bronsonmissouri', format: :js
+      assert_response :bad_request
     end
-
   end
 
   describe 'remove_pregnancy method' do 
-  end
+    before do
+      patch :add_pregnancy, id: @pregnancy_1, user_id: @user, format: :js
+      patch :add_pregnancy, id: @pregnancy_2, user_id: @user, format: :js
+      patch :remove_pregnancy, id: @pregnancy_1, user_id: @user, format: :js
+    end
 
-  describe 'get_user_and_pregnancy method' do 
+    it 'should respond successfully' do 
+      assert_response :success
+    end
+
+    it 'should remove a pregnancy' do 
+      assert_difference '@user.pregnancies.count', -1 do 
+        patch :remove_pregnancy, id: @pregnancy_2, user_id: @user, format: :js
+      end
+    end
+
+    it 'should do nothing if the pregnancy is not currently in the call list' do 
+      assert_no_difference '@user.pregnancies.count' do 
+        patch :remove_pregnancy, id: @pregnancy_1, user_id: @user, format: :js
+      end
+      assert_response :success
+    end
+
+    it 'should should return bad request on sketch ids' do 
+      patch :remove_pregnancy, id: '12345678', user_id: @user, format: :js
+      assert_response :bad_request
+      patch :remove_pregnancy, id: @pregnancy_1, user_id: 'bronsonmissouri', format: :js
+      assert_response :bad_request
+    end
   end
 end
