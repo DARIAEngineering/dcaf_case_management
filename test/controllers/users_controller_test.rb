2998a40b5771d7d104bd6b6fc4b9e1064c53fd89
colinxfleming <c3flemin@gmail.com>
2016-03-12 03:02:45 -0500

    create testfile

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionController::TestCase
+  def setup
+  end
+
+  describe 'add_pregnancy method' do
+  end
+
+  describe 'remove_pregnancy method' do 
+  end
+
+  describe 'get_user_and_pregnancy method' do 
+  end
+end
