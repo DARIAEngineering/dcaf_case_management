b8ae481174027437fac18141b4caf32b7332af6d
colinxfleming <c3flemin@gmail.com>
2016-03-19 22:43:46 -0400

    alter  call model attributes to account for user id creating a call

--- app/models/call.rb
+++ app/models/call.rb
@@ -3,8 +3,14 @@ class Call
 	include Mongoid::Timestamps
 	include Mongoid::History
 
+  allowed_statuses = ['Reached patient', 'Left voicemail', "Couldn't reach patient"]
+
 	field :status, type: String
+  # necessary because embedding things blows up belongs_to relationships
+  field :creating_user_id, type: String
 	embedded_in :pregnancy
 
 	validates_presence_of :status
+  validates :status,  presence: true, 
+                      inclusion: { in: allowed_statuses }
 end
