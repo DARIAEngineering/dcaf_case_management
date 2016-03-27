91582404a926adc858fcd0097e5fd0fba597a7ec
colinxfleming <c3flemin@gmail.com>
2016-03-27 16:13:43 -0400

    basic sign up integration test

--- test/integration/sign_up_test.rb
+++ test/integration/sign_up_test.rb
@@ -7,51 +7,22 @@ class SignUpTest < ActionDispatch::IntegrationTest
   end
 
   describe 'creating a new user' do 
-    it 'should work' do 
-      fill_in 'Email', with: 'billy.everyteen@gmail.com'
+    before do 
+      fill_in 'Email', with: Faker::Internet.email
       fill_in 'Name', with: 'A Real Person'
-      fill_in 'Password', with: 'password'
-      fill_in 'Password confirmation', with: 'password'
-      click_button 'Sign up'
+      fill_in 'Password', with: 'password', match: :prefer_exact
+      fill_in 'Password confirmation', with: 'password', match: :prefer_exact
+    end
+
+    it 'should create a user' do 
+      assert_difference 'User.count', 1 do 
+        click_button 'Sign up'
+      end
+    end
+
+    it 'should save that user name' do 
+      click_button 'Sign up' 
+      assert_link 'A Real Person'
     end
   end
-
-
-
-
-
-
-
-
-
-
-
-
-  # describe 'attribute whitelisting beyond the devise minimum' do 
-  #   before do 
-  #     @user_attributes = { email: "billy2@everyteen.com", password: "password" }
-  #   end
-
-  #   # confirm that each permitted parameter is allowed
-  #   %w(name).each do |whitelisted_attribute|
-  #     it "should permit #{whitelisted_attribute} in sign up" do
-  #       @user_attributes.merge!({ whitelisted_attribute.to_sym => 'thing'})
-  #       @request.env["devise.mapping"] = Devise.mappings[:user]
-  #       post :create, controller: 'devise/registrations', user: @user_attributes
-  #       # assert_response :success
-
-
-  #       # p @user_2
-  #       # p request.env
-  #     end
-
-  #     it "should permit #{whitelisted_attribute} in account_update" do 
-  #     end
-  #   end
-    
-  #   it 'should reject non-whitelisted attributes' do 
-  #   end
-
-  # end
-
 end
