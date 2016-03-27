b699048d959a3fe6e5744ae4d35efb4f631f57b2
colinxfleming <c3flemin@gmail.com>
2016-03-27 17:26:59 -0400

    a little model tidying

--- app/models/call.rb
+++ app/models/call.rb
@@ -3,12 +3,13 @@ class Call
   include Mongoid::Timestamps
   include Mongoid::History
 
+  embedded_in :pregnancy
+
   allowed_statuses = ['Reached patient', 'Left voicemail', "Couldn't reach patient"]
 
   field :status, type: String
   # necessary because embedding things blows up belongs_to relationships
   field :creating_user_id, type: String
-  embedded_in :pregnancy
 
   validates :status,  presence: true,
                       inclusion: { in: allowed_statuses }
