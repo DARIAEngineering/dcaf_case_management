8b51dad2f8b7541938f808a3dec413b301c1160f
Alexis Johnson <alexis.johnson052@gmail.com>
2016-03-28 19:22:46 -0400

    add notes controller
    add calls#create
    
    add redirect to notes#create

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,27 @@
+class NotesController < ApplicationController
+  before_action :find_pregnancy only: [:create]
+
+  def create
+    @note = @pregnancy.notes.new(note_params)
+    if @call.save
+      redirect_to edit_pregnancy_path(@pregnancy)
+      # respond_to { |format| format.js }
+    else
+      flash[:alert] = 'Note failed to save! Please submit the note again.'
+      redirect_to root_path
+    end
+  end
+
+  def update
+  end
+
+  private
+
+    def note_params
+      params.require(:note).permit(:notes)
+    end
+
+    def find_pregnancy
+      @pregnancy = Pregnancy.find params[:id]
+    end
+end
