19d0dbc5932213a1689e1807ddbf9fa69a45974f
colinxfleming <c3flemin@gmail.com>
2016-04-03 17:16:28 -0400

    Revert "Revert "generate notes stuff""
    This reverts commit a6a149f91262eaef16ee409962c71e32a813164f.

---
+++ test/controllers/notes_controller_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class NotesControllerTest < ActionController::TestCase
+  before do 
+  end
+
+  describe 'create method' do 
+  end
+
+  describe 'update method' do 
+  end
+end
