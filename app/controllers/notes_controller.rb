1c19c25c2d98ebaa68438e78f5f740b2c588365d
colinxfleming <c3flemin@gmail.com>
2016-04-03 02:17:05 -0400

    working test for create, add create.js.erb

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,20 +4,20 @@ class NotesController < ApplicationController
   def create
     @note = @pregnancy.notes.new(note_params)
     @note.created_by = current_user
+    @note.save
     if @note.save
-      redirect_to edit_pregnancy_path(@pregnancy)
-      # respond_to { |format| format.js }
+      # redirect_to edit_pregnancy_path(@pregnancy)
+      respond_to { |format| format.js }
     else
-      p @note.errors
       flash[:alert] = 'Note failed to save! Please submit the note again.'
       redirect_to root_path
     end
   end
 
   def update
-    @note = Note.find params[:id]
-    @note.update_attributes note_params
-    respond_to { |format| format.js }
+    # @note = Note.find params[:id]
+    # @note.update_attributes note_params
+    # respond_to { |format| format.js }
   end
 
   private
--- test/controllers/notes_controller_test.rb
+++ test/controllers/notes_controller_test.rb
@@ -10,13 +10,25 @@ class NotesControllerTest < ActionController::TestCase
   describe 'create method' do 
     before do
       @note = attributes_for :note, full_text: 'This is a note'
-      post :create, id: @pregnancy, note: @note#, format: :js
+      post :create, id: @pregnancy, note: @note, format: :js
     end
 
     it 'should create and save a new note' do 
       assert_difference 'Pregnancy.find(@pregnancy).notes.count', 1 do 
-        post :create, id: @pregnancy, note: @note#, format: :js
+        post :create, id: @pregnancy, note: @note, format: :js
       end
+    end
+
+    it 'should respond success if the note submits' do 
+      assert_response :success
+    end
+
+    it 'should render create.js.erb if it successfully saves' do
+      assert_template 'notes/create'
+    end
+
+    it 'should log the creating user' do 
+      assert_equal Pregnancy.find(@pregnancy).notes.last.created_by, @user
     end
   end
 
