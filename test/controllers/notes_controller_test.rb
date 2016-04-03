25023e6b6128acae096f81972e80c1c3e242a1a4
colinxfleming <c3flemin@gmail.com>
2016-04-03 01:45:11 -0400

    add a factory and mod the test

--- test/controllers/notes_controller_test.rb
+++ test/controllers/notes_controller_test.rb
@@ -8,7 +8,16 @@ class NotesControllerTest < ActionController::TestCase
   end
 
   describe 'create method' do 
-    
+    before do
+      @note = attributes_for :note
+      post :create, id: @pregnancy, note: @note, format: :js
+    end
+
+    it 'should create and save a new note' do 
+      assert_difference 'Pregnancy.find(@pregnancy).calls.count', 1 do 
+        post :create, call: @call, id: @prengnacy, format: :js
+      end
+    end
   end
 
   describe 'update method' do 
