fcdeb212476b81d5e44a2ada5595d440cc56b421
colinxfleming <c3flemin@gmail.com>
2016-04-03 01:44:37 -0400

    create basic controller test

--- test/controllers/notes_controller_test.rb
+++ test/controllers/notes_controller_test.rb
@@ -2,9 +2,13 @@ require 'test_helper'
 
 class NotesControllerTest < ActionController::TestCase
   before do 
+    @user = create :user
+    sign_in @user
+    @pregnancy = create :pregnancy
   end
 
   describe 'create method' do 
+    
   end
 
   describe 'update method' do 
