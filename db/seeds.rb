9462a587011955cdfdac83c2aa54520adb06202f
Michael Chelen <michael.chelen@gmail.com>
2016-04-09 15:44:03 -0400

    cleanup code for selectively generating >10 call log

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,11 +25,11 @@ patients.each do |patient|
     flag = false
   end
   pregnancy = patient.pregnancies.create({last_menstrual_period_time: DateTime.new(2016,1,1), urgent_flag: flag})
-  8.times do
+  5.times do
     pregnancy.calls.create({status: 'Left voicemail', creating_user_id: user.id})
   end
-  if(patient.name[-1, 1].to_i == 0) then
-    5.times do
+  if patient.name == 'Patient 0'
+    10.times do
       pregnancy.calls.create({status: 'Reached patient', creating_user_id: user.id})
     end
   end
