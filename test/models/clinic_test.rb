600c5f43e5cd42e900897150e0348315eb52d537
colinxfleming <c3flemin@gmail.com>
2016-04-10 03:09:02 -0400

    userstamp and timestamp tests (and factories) for everything

--- test/models/clinic_test.rb
+++ test/models/clinic_test.rb
@@ -1,27 +1,27 @@
 require 'test_helper'
 
-class PatientTest < ActiveSupport::TestCase
+class ClinicTest < ActiveSupport::TestCase
   before do 
     @user = create :user
-    @patient = create :patient, created_by: @user
+    @clinic = create :clinic, created_by: @user
   end
 
   describe 'mongoid attachments' do
     it 'should have timestamps from Mongoid::Timestamps' do
       [:created_at, :updated_at].each do |field|
-        assert @patient.respond_to? field
-        assert @patient[field]
+        assert @clinic.respond_to? field
+        assert @clinic[field]
       end
     end
 
     it 'should respond to history methods' do
-      assert @patient.respond_to? :history_tracks
-      assert @patient.history_tracks.count > 0 
+      assert @clinic.respond_to? :history_tracks
+      assert @clinic.history_tracks.count > 0 
     end
 
     it 'should have accessible userstamp methods' do 
-      assert @patient.respond_to? :created_by
-      assert @patient.created_by
+      assert @clinic.respond_to? :created_by
+      assert @clinic.created_by
     end
   end
 end
--- test/models/pregnancy_test.rb
+++ test/models/pregnancy_test.rb
@@ -2,12 +2,14 @@ require 'test_helper'
 
 class PregnancyTest < ActiveSupport::TestCase
   before do
+    @user = create :user
     @pt_1 = create :patient, name: 'Susan Everywoman', primary_phone: '123-456-6789'
     @pt_2 = create :patient, name: 'Susan Everyteen', primary_phone: '123-456-6789'
     @pt_3 = create :patient, name: 'Susan Everygirl', secondary_phone: '999-999-9999'
     [@pt_1, @pt_2, @pt_3].each do |pt|
-      create :pregnancy, patient: pt
+      create :pregnancy, patient: pt, created_by: @user
     end
+    @pregnancy = @pt_1.pregnancies.first
   end
 
   describe 'search method' do
@@ -27,4 +29,23 @@ class PregnancyTest < ActiveSupport::TestCase
       assert_equal Patient.search('999-999-9999').count, 1
     end
   end
+
+  describe 'mongoid attachments' do
+    it 'should have timestamps from Mongoid::Timestamps' do
+      [:created_at, :updated_at].each do |field|
+        assert @pregnancy.respond_to? field
+        assert @pregnancy[field]
+      end
+    end
+
+    it 'should respond to history methods' do
+      assert @pregnancy.respond_to? :history_tracks
+      assert @pregnancy.history_tracks.count > 0 
+    end
+
+    it 'should have accessible userstamp methods' do 
+      assert @pregnancy.respond_to? :created_by
+      assert @pregnancy.created_by
+    end
+  end  
 end
