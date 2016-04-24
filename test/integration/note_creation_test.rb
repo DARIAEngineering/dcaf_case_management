75b733aebe25ff0a3a62e231f2f9146730d443d0
oldmaneddie <parkes.edward@gmail.com>
2016-04-24 18:28:45 +0000

    209: Update test copy

--- test/integration/note_creation_test.rb
+++ test/integration/note_creation_test.rb
@@ -1,46 +1,34 @@
 require 'test_helper'
 
-class NotesCreationTest < ActionDispatch::IntegrationTest
+class NoteCreationTest < ActionDispatch::IntegrationTest
   before do
     @user = create :user
     log_in_as @user
     @patient = create :patient
-    @pregnancy = create :pregnancy, appointment_date: nil, patient: @patient
+    @pregnancy = create :pregnancy, patient: @patient
     visit edit_pregnancy_path(@pregnancy)
   end
  
   describe 'add patient pregnancy notes' do
-    it 'should display a pregnancy notes form and current notes' do
+    it 'should display a case notes form and current notes' do
         click_link 'Notes'
         
         visit current_path 
         
         within('#notes')do
             assert_text 'Notes' #confirm notes header is visible 
+            page.has_field? "note[full_text]"
             assert has_button? 'Create Note'
         end
     end 
     
-    it 'should let you add a new pregnancy note' do
-        fill_in 'note[full_text]', with: 'Sample Note Body'
-       
-        assert_difference 'Note.count', 1 do 
-          click_on 'Create Note'
-        end 
-        
-        visit current_path 
-        
-        assert_text 'Sample Note Body'
-    end 
+    it 'should let you add a new case note' do
+        fill_in 'note[full_text]', with: 'Sample new note creation body'
+        click_button "Create Note"
 
-    # it 'should let you edit a pregnancy note' do
-    #     click_on 'edit'
-    #     fill_in 'note[full_text]', with: 'Sample Note Body Edit'
-    #     click_on 'Create Note'
-        
-    #     visit current_path
-        
-    #     assert_text 'Sample Note Body Edit'
-    # end 
+        visit current_path 
+                
+        assert_text 'Sample new note creation body'
+    end 
   end 
 end 
