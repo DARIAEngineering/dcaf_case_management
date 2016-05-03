88122e581b7e24705ade6821079e75b7cd8a40b8
Ashlynn Pai <ashlynnpai@gmail.com>
2016-05-03 03:42:25 +0000

    dashboard link integration test

--- test/integration/dashboard_link_test.rb
+++ test/integration/dashboard_link_test.rb
@@ -1,35 +1,35 @@
 require 'test_helper'
 
 class DashboardLinkTest < ActionDispatch::IntegrationTest
-
-  def setup
+  before do
     @user = create :user
     log_in_as @user
   end
-
+  
   describe 'visiting the dashboard' do
     before do
       visit authenticated_root_url
     end
     it 'should not display the dashboard link' do
-      assert_not_equal '/Dashboard', authenticated_root_url
+      refute has_link? 'Dashboard', href: authenticated_root_url   
     end
   end
   
   describe 'visiting a page other than the dashboard' do
     before do
-      @patient = create :patient,
-                      name: 'Susie Everyteen',
-                      primary_phone: '123-456-7890',
-                      secondary_phone: '333-444-5555'
-      @pregnancy = create :pregnancy, appointment_date: nil, patient: @patient
-      @clinic = create :clinic, name: 'Standard Clinic', pregnancy: @pregnancy
+      @patient = create :patient
+      @pregnancy = create :pregnancy, patient: @patient
       visit edit_pregnancy_path(@pregnancy)
     end
     it 'should display the dashboard link' do
-      assert_equal '/Dashboard', authenticated_root_url
+      assert has_link? 'Dashboard', href: authenticated_root_url
     end  
+    it 'should direct the user to the dashboard' do
+      click_link 'Dashboard'
+      assert_equal current_path, authenticated_root_path
+      refute has_link? 'Dashboard', href: authenticated_root_url
+    end
   end 
 end
 
-  
+
