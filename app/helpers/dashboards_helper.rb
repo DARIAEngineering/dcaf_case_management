20e469a9573706b32b980908cd74903bfa47843b
sco-bo <scottebobbitt@gmail.com>
2016-05-21 15:20:21 -0400

    add week range helper method

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -1,2 +1,9 @@
 module DashboardsHelper
+
+  def week_range
+    today = DateTime.now
+    week_start = today.beginning_of_week(:monday).strftime("%B %d")
+    week_end = today.end_of_week(:monday).strftime("%d")
+    "#{week_start} - #{week_end}"
+  end
 end
