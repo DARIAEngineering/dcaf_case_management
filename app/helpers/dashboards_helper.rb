4f79b94d418a13b794d2a1dcb0105e14d333727a
sco-bo <scottebobbitt@gmail.com>
2016-05-22 08:41:32 -0400

    fix week_range method for weeks spanning 2 months and allow optional arguments

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -1,9 +1,10 @@
 module DashboardsHelper
 
-  def week_range
-    today = DateTime.now
-    week_start = today.beginning_of_week(:monday).strftime("%B %d")
-    week_end = today.end_of_week(:monday).strftime("%d")
-    "#{week_start} - #{week_end}"
+  def week_range(date: DateTime.now, start_day: :monday)
+    week_start = date.beginning_of_week(start_day)
+    week_end = date.end_of_week(start_day)
+    week_start_string = week_start.strftime("%B %d")
+    week_end_string = week_start.month == week_end.month ? week_end.strftime("%d") : week_end.strftime("%B %d")
+    "#{week_start_string} - #{week_end_string}"
   end
 end
