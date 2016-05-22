d9e9e5994601dd6275abe1263aa3217df499b827
sco-bo <scottebobbitt@gmail.com>
2016-05-22 12:54:11 -0400

    adjust test for week range helper method

--- test/helpers/dashboards_helper_test.rb
+++ test/helpers/dashboards_helper_test.rb
@@ -1,15 +1,21 @@
 require 'test_helper'
 
 class DashboardsHelperTest < ActionView::TestCase
+  before do 
+    @date_1 = DateTime.new(2016,5,21)
+    @date_2 = DateTime.new(2016,5,31)
+    @monday_start = :monday 
+  end
 
   describe 'week_range method' do
-    it 'should return current date range' do 
-      first_day_of_week = :monday
-      today = DateTime.now
-      start_of_week_date = today.beginning_of_week(first_day_of_week).strftime("%B %d")
-      end_of_week_date = today.end_of_week(first_day_of_week).strftime("%d")
-      expected = "#{start_of_week_date} - #{end_of_week_date}"
-      assert_equal expected, week_range
+    it 'should return correct date range when start and end months are same' do 
+      expected = "May 16 - 22"
+      assert_equal expected, week_range(date: @date_1, start_day: @monday_start)
     end 
+
+    it 'should return correct date range when start and end months are different' do 
+      expected = "May 30 - June 5"
+      assert_equal expected, week_range(date: @date_2, start_day: @monday_start)
+    end
   end
 end
