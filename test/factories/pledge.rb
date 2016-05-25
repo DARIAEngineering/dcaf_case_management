ce705f2dc20c613dd3e1ba88ee88bd71d433e593
Kevin Wei <kevin.tw.wei@gmail.com>
2016-05-25 10:22:51 -0400

    fix factory

--- test/factories/pledge.rb
+++ test/factories/pledge.rb
@@ -3,5 +3,8 @@ FactoryGirl.define do
     pregnancy
     pledge_type 'Soft'
     amount 100
+    after :build do |pledge, eval|
+      pledge.created_by_id = FactoryGirl.create(:user)
+    end
   end
 end
