9132195b9f9b907cfbb7381e0843b05c3e006375
Kevin Wei <kevin.tw.wei@gmail.com>
2016-05-26 10:41:35 -0400

    follow project pattern for valid factory

--- test/factories/pledge.rb
+++ test/factories/pledge.rb
@@ -3,8 +3,6 @@ FactoryGirl.define do
     pregnancy
     pledge_type 'Soft'
     amount 100
-    after :build do |pledge, eval|
-      pledge.created_by_id = FactoryGirl.create(:user)
-    end
+    created_by { FactoryGirl.create(:user) }
   end
 end
