5554215f7b11d0de7265d0ea8a0b7b94328754a7
colinxfleming <c3flemin@gmail.com>
2016-05-30 00:38:23 -0400

    finish off those functional tests. those beautiful, beautiful integration tests

--- test/integration/new_patient_creation_test.rb
+++ test/integration/new_patient_creation_test.rb
@@ -18,13 +18,22 @@ class NewPatientCreationTest < ActionDispatch::IntegrationTest
       fill_in 'Phone Number', with: '555-666-7777'
       fill_in 'Name', with: 'Susan Everyteen 2'
       fill_in 'Initial Call Date', with: '03/04/2016'
-
-      page.save_screenshot '~/Desktop/capybara.png'
-      click_button 'Create new patient'
+      find('button', text: /Create new patient/).trigger('click')
+      sleep 1
+      fill_in 'search', with: 'Susan Everyteen 2'
+      click_button 'Search'
     end
 
-    it 'should ' do
+    it 'should make that patient retrievable' do
+      within :css, '#search_results' do
+        assert has_text? 'Susan Everyteen 2'
+        assert has_text? '555-666-7777'
+      end
     end
 
+    it 'should make them viewable from the pregnancy edit page' do
+      click_link 'Susan Everyteen 2'
+      assert current_path, edit_pregnancy_path(Pregnancy.last)
+    end
   end
 end
