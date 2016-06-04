da2ae82c9350b2830d36510a4c8fa0d78a67a0c1
Cam <camlatimer@gmail.com>
2016-06-04 14:23:30 -0400

    now going the correct route to to  checks to see if user is logged in, also tweaks tests to check for signed in messages

--- test/integration/routing_test.rb
+++ test/integration/routing_test.rb
@@ -7,7 +7,7 @@ class RoutingTest < ActionDispatch::IntegrationTest
 
   it 'should have a root with a login form' do
     assert_equal current_path, new_user_session_path
-    assert_text 'You need to sign in or sign up before continuing'
+    refute has_text? 'You need to sign in or sign up before continuing'
     assert has_field? 'Email'
     assert has_field? 'Password'
     assert has_button? 'Sign in'
