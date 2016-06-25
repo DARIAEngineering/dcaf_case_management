1d86dfec28112dec7e6b7e017a4a8eedd8045712
colinxfleming <c3flemin@gmail.com>
2016-06-25 11:54:47 -0400

    adjust to wait for elements to show up

--- test/integration/note_creation_test.rb
+++ test/integration/note_creation_test.rb
@@ -13,13 +13,13 @@ class NoteCreationTest < ActionDispatch::IntegrationTest
     it 'should display a case notes form and current notes' do
       click_link 'Notes'
       within('#notes') do
-        assert_text 'Notes' # confirm notes header is visible
-        page.has_field? 'note[full_text]'
+        assert has_text? 'Notes' # confirm notes header is visible
         assert has_button? 'Create Note'
       end
     end
 
     it 'should let you add a new case note' do
+      assert has_field? 'note[full_text]'
       fill_in 'note[full_text]', with: 'Sample new note creation body'
       click_button 'Create Note'
       assert_text 'Sample new note creation body'
