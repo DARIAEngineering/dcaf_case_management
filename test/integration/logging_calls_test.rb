172dffd4aacfe0ef562b41c9f2d6af614b2b6d73
colinxfleming <c3flemin@gmail.com>
2016-06-25 12:16:31 -0400

    clean up logging calls test

--- test/integration/logging_calls_test.rb
+++ test/integration/logging_calls_test.rb
@@ -48,7 +48,7 @@ class LoggingCallsTest < ActionDispatch::IntegrationTest
     end
 
     it 'should be able to save more than one reached patient call' do
-      click_link 'Dashboard'
+      visit authenticated_root_path
       fill_in 'search', with: 'Susan Everyteen'
       click_button 'Search'
       find("a[href='#call-123-123-1234']").click
@@ -63,27 +63,26 @@ class LoggingCallsTest < ActionDispatch::IntegrationTest
   ['Left voicemail', "Couldn't reach patient"].each do |call_status|
     describe "logging #{call_status}", js: true do
       before do
-        if call_status == 'Left voicemail'
-          @link_text = 'I left a voicemail for the patient'
-        elsif call_status == "Couldn't reach patient"
-          @link_text = "I couldn't reach the patient"
-        else
-          raise 'Not a recognized call status'
-        end
-        @timestamp = Time.now
+        @link_text =  if call_status == 'Left voicemail'
+                        'I left a voicemail for the patient'
+                      elsif call_status == "Couldn't reach patient"
+                        "I couldn't reach the patient"
+                      else
+                        raise 'Not a recognized call status'
+                      end
+        @timestamp = Time.zone.now
+        find('a', text: @link_text).click
       end
 
       it "should close the modal when clicking #{call_status}" do
-        click_link @link_text
         assert_equal current_path, authenticated_root_path
         assert has_no_text? 'Call Susan Everyteen now'
         assert has_no_link? @link_text
       end
 
       it "should be visible on the call log after clicking #{call_status}" do
-        click_link @link_text
-        click_link 'Susan Everyteen'
-        click_link 'Call Log'
+        visit edit_pregnancy_path(@pregnancy)
+        find('a', text: 'Call Log').click
 
         within :css, '#call_log' do
           assert has_text? @timestamp.localtime.strftime('%-m/%-d')
