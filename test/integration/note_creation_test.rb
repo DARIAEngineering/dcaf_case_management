bb42de6725c6bad2ec7949e469de15e7ccb78ac0
colinxfleming <c3flemin@gmail.com>
2016-06-25 12:04:30 -0400

    wait for notes link in before hook

--- test/integration/note_creation_test.rb
+++ test/integration/note_creation_test.rb
@@ -7,11 +7,11 @@ class NoteCreationTest < ActionDispatch::IntegrationTest
     @patient = create :patient
     @pregnancy = create :pregnancy, patient: @patient
     visit edit_pregnancy_path(@pregnancy)
+    find('a', text: 'Notes').click
   end
 
   describe 'add patient pregnancy notes' do
     it 'should display a case notes form and current notes' do
-      click_link 'Notes'
       within('#notes') do
         assert has_text? 'Notes' # confirm notes header is visible
         assert has_button? 'Create Note'
@@ -19,7 +19,6 @@ class NoteCreationTest < ActionDispatch::IntegrationTest
     end
 
     it 'should let you add a new case note' do
-      assert has_field? 'note[full_text]'
       fill_in 'note[full_text]', with: 'Sample new note creation body'
       click_button 'Create Note'
       assert_text 'Sample new note creation body'
