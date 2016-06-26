44584260c88cbd805e390cc968b61faedd45815c
colinxfleming <c3flemin@gmail.com>
2016-06-26 19:08:28 -0400

    config edits

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,14 +57,16 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
+  # Prevent host header injection
+  config.action_controller.default_url_options = { host: (ENV['SITE_URL'] || 'localhost') }
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = (ENV['SITE_URL'] || 'localhost')
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.default_url_options = { :host => 'casemanagerdemo.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => (ENV['SITE_URL'] || 'localhost') }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
