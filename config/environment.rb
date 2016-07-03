a90a67d16620726a5c388a5a8dbdb84f43d9df7d
colinxfleming <c3flemin@gmail.com>
2016-07-03 12:37:26 -0400

    caps for the caps god

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV['sendgrid_username'],
-  :password       => ENV['sendgrid_password'],
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
   :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }
