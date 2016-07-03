4710dd417656c63b9e4ee0bd6bd1cc1827152e80
colinxfleming <c3flemin@gmail.com>
2016-07-03 11:33:18 -0400

    restore rememberable options in devise initializer because it's failing occasionally for no reason

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -138,7 +138,7 @@ Devise.setup do |config|
 
   # Options to be passed to the created cookie. For instance, you can set
   # secure: true in order to force SSL only cookies.
-  # config.rememberable_options = {}
+  config.rememberable_options = {}
 
   # ==> Configuration for :validatable
   # Range for password length.
