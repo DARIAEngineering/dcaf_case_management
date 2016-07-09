5fc0e5e84c39bcfea884af7207454920e4c8e09b
colinxfleming <c3flemin@gmail.com>
2016-07-09 13:58:09 -0400

    use localtime instead of getlocal

--- config/initializers/time.rb
+++ config/initializers/time.rb
@@ -1,11 +1,11 @@
 # Extends time class with convenience methods
 class Time
   def display_date
-    getlocal.strftime("%Y-%m-%d")
+    localtime.strftime("%Y-%m-%d")
   end
 
   def display_time
-    getlocal.strftime("%-l:%M %P")
+    localtime.strftime("%-l:%M %P")
   end
 
   def display_timestamp
