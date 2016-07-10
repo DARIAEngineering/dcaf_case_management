5d4352bf1d8e0d281981d8db9ce1f88b125091ec
colinxfleming <c3flemin@gmail.com>
2016-07-10 01:08:08 -0400

    dry up helper, get view working

--- app/helpers/last_menstrual_period_helper.rb
+++ app/helpers/last_menstrual_period_helper.rb
@@ -2,8 +2,7 @@ module LastMenstrualPeriodHelper
   # last menstrual period calculator methods
   def last_menstrual_period_display
     return nil unless last_menstrual_period_now
-    "#{(last_menstrual_period_now / 7).floor} weeks, " \
-    "#{(last_menstrual_period_now % 7).to_i} days"
+    display_as_weeks last_menstrual_period_now
   end
 
   def last_menstrual_period_display_short
@@ -12,8 +11,9 @@ module LastMenstrualPeriodHelper
   end
 
   def last_menstrual_period_at_appt
-    return nil unless last_menstrual_period_now
-    (appointment_date.to_date - last_menstrual_period_now).to_date
+    return nil unless last_menstrual_period_now && appointment_date
+    lmp = last_menstrual_period_on_date appointment_date
+    display_as_weeks lmp
   end
 
   private
@@ -27,5 +27,10 @@ module LastMenstrualPeriodHelper
     weeks = 7 * (last_menstrual_period_weeks || 0)
     days = (last_menstrual_period_days || 0)
     (date - initial_call_date) + weeks + days
+  end
+
+  def display_as_weeks(num)
+    "#{(num / 7).floor} weeks, " \
+    "#{(num % 7).to_i} days"
   end
 end
