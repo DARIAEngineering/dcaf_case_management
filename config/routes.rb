79bf4b70276c040a05c5851d4a72563f3868b69f
colinxfleming <c3flemin@gmail.com>
2016-07-23 17:18:38 -0400

    add reorder pregnancies route

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
     end
     patch 'users/:user_id/add_pregnancy/:id', to: 'users#add_pregnancy', as: 'add_pregnancy', defaults: { format: :js }
     patch 'users/:user_id/remove_pregnancy/:id', to: 'users#remove_pregnancy', as: 'remove_pregnancy', defaults: { format: :js }
+    patch 'users/:user_id/reorder_pregnancies', to: 'users#reorder_pregnancies', as: 'reorder_pregnancies', default: { format: :js }
   end
   root :to => redirect('/users/sign_in')
   devise_for :users, skip: [:registrations]
