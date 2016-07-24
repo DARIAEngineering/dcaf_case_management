29b0beca01ff49135ea8c4f54f12be3796d998ec
colinxfleming <c3flemin@gmail.com>
2016-07-24 00:26:20 -0400

    pop in some wait for page to loads

--- test/integration/logging_calls_test.rb
+++ test/integration/logging_calls_test.rb
@@ -39,7 +39,8 @@ class LoggingCallsTest < ActionDispatch::IntegrationTest
     end
 
     it 'should be viewable on the call log' do
-      visit edit_pregnancy_path(@pregnancy)
+      visit edit_pregnancy_path @pregnancy
+      wait_for_page_to_load
       find('a', text: 'Call Log').click
 
       within :css, '#call_log' do
@@ -53,7 +54,7 @@ class LoggingCallsTest < ActionDispatch::IntegrationTest
 
   describe 'logging multiple calls' do
     # problematic test
-    it 'should let you save more than one reached patient call' do
+    it 'should let you save more than one call' do
       3.times do
         visit authenticated_root_path
         fill_in 'search', with: 'Susan Everyteen'
@@ -72,7 +73,7 @@ class LoggingCallsTest < ActionDispatch::IntegrationTest
   end
 
   ['Left voicemail', "Couldn't reach patient"].each do |call_status|
-    describe "logging #{call_status}", js: true do
+    describe "logging #{call_status}" do
       before do
         @link_text =  if call_status == 'Left voicemail'
                         'I left a voicemail for the patient'
@@ -86,25 +87,26 @@ class LoggingCallsTest < ActionDispatch::IntegrationTest
       end
 
       # problematic test
-      # it "should close the modal when clicking #{call_status}" do
-      #   assert_equal current_path, authenticated_root_path
-      #   assert has_no_text? 'Call Susan Everyteen now'
-      #   assert has_no_link? @link_text
-      # end
+      it "should close the modal when clicking #{call_status}" do
+        assert_equal current_path, authenticated_root_path
+        assert has_no_text? 'Call Susan Everyteen now'
+        assert has_no_link? @link_text
+      end
 
       # problematic test
-      # it "should be visible on the call log after clicking #{call_status}" do
-      #   assert_equal current_path, authenticated_root_path
-      #   visit edit_pregnancy_path(@pregnancy)
-      #   find('a', text: 'Call Log').click
+      it "should be visible on the call log after clicking #{call_status}" do
+        assert_equal current_path, authenticated_root_path
+        visit edit_pregnancy_path @pregnancy
+        wait_for_page_to_load
+        find('a', text: 'Call Log').click
 
-      #   within :css, '#call_log' do
-      #     assert has_text? @timestamp.display_date
-      #     assert has_text? @timestamp.display_time
-      #     assert has_text? call_status
-      #     assert has_text? @user.name
-      #   end
-      # end
+        within :css, '#call_log' do
+          assert has_text? @timestamp.display_date
+          assert has_text? @timestamp.display_time
+          assert has_text? call_status
+          assert has_text? @user.name
+        end
+      end
     end
   end
 
