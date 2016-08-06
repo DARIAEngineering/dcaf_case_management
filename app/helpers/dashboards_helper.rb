20bb32324e286d18612f2c9d5ad624158f970a52
colinxfleming <c3flemin@gmail.com>
2016-08-06 19:32:19 -0400

    add voicemail options helper method

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -15,4 +15,13 @@ module DashboardsHelper
     return nil unless text.length > 41
     "<span class='glyphicon glyphicon-plus-sign' aria-hidden='true' data-toggle='popover' data-placement='bottom' title='Most recent note' data-content='#{text}'></span><span class='sr-only'>Full note</span>".html_safe
   end
+
+  def voicemail_options
+    enum_text = { default: 'No instructions; no ID VM',
+                  no: 'Do not leave a voicemail',
+                  yes: 'Voicemail OK' }
+    vm_options = Pregnancy::VOICEMAIL_PREFERENCE
+
+    vm_options.map { |option| [enum_text[option], option] }
+  end
 end
