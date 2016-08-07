68cf65f1f58c0a0ba7520bd8e5540c583b46ba58
colinxfleming <c3flemin@gmail.com>
2016-08-07 02:01:49 -0400

    fix tests to reflect new field

--- test/integration/update_patient_info_test.rb
+++ test/integration/update_patient_info_test.rb
@@ -81,7 +81,7 @@ class UpdatePatientInfoTest < ActionDispatch::IntegrationTest
       fill_in 'City', with: 'Washington'
       fill_in 'State', with: 'DC'
       fill_in 'ZIP', with: '90210'
-      check 'Voicemail OK?'
+      find('#pregnancy_voicemail_preference').select 'Voicemail OK'
       check 'Spanish Only'
 
       find('#pregnancy_employment_status').select 'Part-time'
@@ -109,7 +109,7 @@ class UpdatePatientInfoTest < ActionDispatch::IntegrationTest
         assert has_field? 'State', with: 'DC'
         assert has_field? 'ZIP', with: '90210'
         assert has_field? 'Special circumstances', with: 'Stuff'
-        assert has_checked_field? 'Voicemail OK?'
+        assert_equal 'Voicemail OK', find('#pregnancy_voicemail_preference').value
         assert has_checked_field? 'Spanish Only'
 
         assert_equal 'Part-time', find('#pregnancy_employment_status').value
