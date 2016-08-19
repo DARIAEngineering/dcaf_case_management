c544439835b62a4ba15e8c9a6cf3b33533bfa8c6
Benjamin Crane <crane.benjamin@gmail.com>
2016-08-19 19:20:44 +0000

    Correct typo

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
   test 'should send an email' do
-    @user = creaete :user
+    @user = create :user
     mail = UserMailer.password_changed(@user.id)
     assert_equal "Password changed", mail.subject
     assert_equal ["to@example.org"], mail.to
