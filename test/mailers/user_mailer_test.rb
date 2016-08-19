649d1035ba68cd71df4c1c14fd02d14bdca15a7a
Benjamin Crane <crane.benjamin@gmail.com>
2016-08-19 19:25:07 +0000

    Update test text

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -4,7 +4,7 @@ class UserMailerTest < ActionMailer::TestCase
   test 'should send an email' do
     @user = create :user
     mail = UserMailer.password_changed(@user.id)
-    assert_equal "Password changed", mail.subject
+    assert_equal "Your password has changed", mail.subject
     assert_equal ["to@example.org"], mail.to
     assert_equal ["from@example.com"], mail.from
     assert_match "Hi", mail.body.encoded
