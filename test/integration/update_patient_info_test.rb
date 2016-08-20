7609d90d6c8048dafba20052c2bdc05c947597ad
colinxfleming <c3flemin@gmail.com>
2016-08-20 05:08:27 -0400

    fix last integration test I think

--- test/integration/update_patient_info_test.rb
+++ test/integration/update_patient_info_test.rb
@@ -7,7 +7,7 @@ class UpdatePatientInfoTest < ActionDispatch::IntegrationTest
     @patient = create :patient
     @pregnancy = create :pregnancy, patient: @patient
     log_in_as @user
-    visit edit_pregnancy_path @pregnancy
+    visit edit_patient_path @patient
     has_text? 'First and last name' # wait until page loads
   end
 
@@ -18,21 +18,21 @@ class UpdatePatientInfoTest < ActionDispatch::IntegrationTest
   describe 'changing patient dashboard information' do
     before do
       fill_in 'First and last name', with: 'Susie Everyteen 2'
-      find('#pregnancy_last_menstrual_period_weeks').select '5 weeks'
-      find('#pregnancy_last_menstrual_period_days').select '2 days'
+      find('#patient_pregnancy_last_menstrual_period_weeks').select '5 weeks'
+      find('#patient_pregnancy_last_menstrual_period_days').select '2 days'
       fill_in 'Appointment date', with: '2016-09-01'
       fill_in 'Phone number', with: '123-666-8888'
 
       click_away_from_field
       visit authenticated_root_path
-      visit edit_pregnancy_path @pregnancy
+      visit edit_patient_path @patient
     end
 
     it 'should alter the information' do
       within :css, '#patient_dashboard' do
         assert has_field?('First and last name', with: 'Susie Everyteen 2')
-        assert_equal find('#pregnancy_last_menstrual_period_weeks').value, '5'
-        assert_equal find('#pregnancy_last_menstrual_period_days').value, '2'
+        assert_equal find('#patient_pregnancy_last_menstrual_period_weeks').value, '5'
+        assert_equal find('#patient_pregnancy_last_menstrual_period_days').value, '2'
         assert has_field?('Appointment date', with: '2016-09-01')
         assert has_field? 'Phone number', with: '123-666-8888'
       end
@@ -42,7 +42,7 @@ class UpdatePatientInfoTest < ActionDispatch::IntegrationTest
   describe 'changing abortion information' do
     before do
       click_link 'Abortion Information'
-      find('#pregnancy_clinic_name').select 'Sample Clinic 1'
+      find('#patient_clinic_name').select 'Sample Clinic 1'
       # TODO: finish this after implementing clinic logic
       fill_in 'Abortion cost', with: '300'
       fill_in 'Patient contribution', with: '200'
@@ -52,20 +52,20 @@ class UpdatePatientInfoTest < ActionDispatch::IntegrationTest
 
       click_away_from_field
       visit authenticated_root_path
-      visit edit_pregnancy_path @pregnancy
+      visit edit_patient_path @patient
       click_link 'Abortion Information'
     end
 
     # problematic test
     it 'should alter the information' do
       within :css, '#abortion_information' do
-        assert_equal 'Sample Clinic 1', find('#pregnancy_clinic_name').value
+        assert_equal 'Sample Clinic 1', find('#patient_clinic_name').value
         # TK after clinic logic
         assert has_field? 'Abortion cost', with: '300'
         assert has_field? 'Patient contribution', with: '200'
         assert has_field? 'National Abortion Federation pledge', with: '50'
         assert has_field? 'DCAF soft pledge', with: '25'
-        assert_equal '1', find('#pregnancy_resolved_without_dcaf').value
+        assert_equal '1', find('#patient_pregnancy_resolved_without_dcaf').value
       end
     end
   end
@@ -77,24 +77,24 @@ class UpdatePatientInfoTest < ActionDispatch::IntegrationTest
       fill_in 'Other phone', with: '123-666-7777'
       fill_in 'Relationship to other contact', with: 'Friend'
       fill_in 'Age', with: '24'
-      find('#pregnancy_race_ethnicity').select 'White/Caucasian'
+      find('#patient_race_ethnicity').select 'White/Caucasian'
       fill_in 'City', with: 'Washington'
       fill_in 'State', with: 'DC'
       fill_in 'ZIP', with: '90210'
-      find('#pregnancy_voicemail_preference').select 'Voicemail OK'
+      find('#patient_voicemail_preference').select 'Voicemail OK'
       check 'Spanish Only'
 
-      find('#pregnancy_employment_status').select 'Part-time'
-      find('#pregnancy_income').select '$30,000-34,999 ($577-672/week)'
-      find('#pregnancy_household_size_adults').select '1'
-      find('#pregnancy_household_size_children').select '3'
-      find('#pregnancy_insurance').select 'Other state Medicaid'
-      find('#pregnancy_referred_by').select 'Other abortion fund'
+      find('#patient_employment_status').select 'Part-time'
+      find('#patient_income').select '$30,000-34,999 ($577-672/week)'
+      find('#patient_household_size_adults').select '1'
+      find('#patient_household_size_children').select '3'
+      find('#patient_insurance').select 'Other state Medicaid'
+      find('#patient_referred_by').select 'Other abortion fund'
       fill_in 'Special circumstances', with: 'Stuff'
 
       click_away_from_field
       visit authenticated_root_path
-      visit edit_pregnancy_path @pregnancy
+      visit edit_patient_path @patient
     end
 
     # problematic test
@@ -105,21 +105,21 @@ class UpdatePatientInfoTest < ActionDispatch::IntegrationTest
         assert has_field? 'Other phone', with: '123-666-7777'
         assert has_field? 'Relationship to other contact', with: 'Friend'
         assert has_field? 'Age', with: '24'
-        assert_equal 'White/Caucasian', find('#pregnancy_race_ethnicity').value
+        assert_equal 'White/Caucasian', find('#patient_race_ethnicity').value
         assert has_field? 'City', with: 'Washington'
         assert has_field? 'State', with: 'DC'
         assert has_field? 'ZIP', with: '90210'
         assert has_field? 'Special circumstances', with: 'Stuff'
-        assert_equal 'yes', find('#pregnancy_voicemail_preference').value
+        assert_equal 'yes', find('#patient_voicemail_preference').value
         assert has_checked_field? 'Spanish Only'
 
-        assert_equal 'Part-time', find('#pregnancy_employment_status').value
-        assert_equal '$30,000-34,999 ($577-672/week)', find('#pregnancy_income').value
-        assert_equal '1', find('#pregnancy_household_size_adults').value
-        assert_equal '3', find('#pregnancy_household_size_children').value
-        assert_equal 'Other state Medicaid', find('#pregnancy_insurance').value
-        assert_equal 'Other abortion fund', find('#pregnancy_referred_by').value
-        assert_equal 'Stuff', find('#pregnancy_special_circumstances').value
+        assert_equal 'Part-time', find('#patient_employment_status').value
+        assert_equal '$30,000-34,999 ($577-672/week)', find('#patient_income').value
+        assert_equal '1', find('#patient_household_size_adults').value
+        assert_equal '3', find('#patient_household_size_children').value
+        assert_equal 'Other state Medicaid', find('#patient_insurance').value
+        assert_equal 'Other abortion fund', find('#patient_referred_by').value
+        assert_equal 'Stuff', find('#patient_pregnancy_special_circumstances').value
       end
     end
   end
