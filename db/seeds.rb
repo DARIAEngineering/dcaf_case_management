e553698f402c6356a1cc59889f84ffba6585fcf2
Jason Allen <jasonallenphotography@gmail.com>
2016-10-04 13:10:54 -0400

    Complete changes to seedfile satisfying Issue #515

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,37 +18,6 @@ user2 = User.create name: 'testuser2', email: 'test2@test.com',
                  created_by: user2
 end
 
-# Patients should have names that reflect what we're looking for (e.g. 'Patient With Other Contact' rather than 'Patient 2')
-# Add example of patient with other contact info
-  patient1 = Patient.find_by( name: "Patient 1")
-  patient1.update_attributes( name: "Patient 1 with Other Contact info", other_contact: "Jane Doe", other_phone: "234-456-6789", other_contact_relationship: "Sister" )
-  patient1.save!
-
-# Add example of patient with appointment one week from today && clinic selected
-  patient2 = Patient.find_by( name: "Patient 2")
-  patient2.update_attributes( name: "Patient 2 with clinic and appointment(1wk)",clinic_name: "Sample Clinic 1", appointment_date: Time.now + (7*24*60*60) )
-  patient2.save!
-
-# # Add example of patient with a pledge submitted
-#   patient3 = Patient.find_by( name: "Patient 3")
-#   patient3.update_attributes( name: "Patient 2 with a pledge submitted",clinic_name: "Sample Clinic 1", appointment_date: Time.now + (7*24*60*60) )
-#   patient3.save!  
-
-
-# # Add example of patient should be marked resolved without DCAF
-#   patient4 = Patient.find_by( name: "Patient 4")
-#   patient4.update_attributes( name: "Patient 4 resolved without DCAF", clinic_name: "Sample Clinic 1", appointment_date: Time.now + (7*24*60*60) )
-#   patient4.save!  
-
-
-# # Add example of patient should have special circumstances
-#   patient4 = Patient.find_by( name: "Patient 4")
-#   patient4.update_attributes( name: "Patient 4 resolved without DCAF", clinic_name: "Sample Clinic 1", appointment_date: Time.now + (7*24*60*60) )
-#   patient4.save!  
-
-# All patients except one or two should have notes
-
-
 # Create active pregnancies for every patient
 Patient.all.each do |patient|
   # If the patient number is even, flag as urgent
@@ -82,21 +51,63 @@ Patient.all.each do |patient|
   end
 end
 
-# Add a note to Patient 2
+# Patients should have names that reflect what we're looking for (e.g. 'Patient With Other Contact' rather than 'Patient 2')
+# Add example of patient with other contact info
+  patient1 = Patient.find_by( name: "Patient 1")
+  patient1.update_attributes( name: "Patient with Other Contact info - 1", other_contact: "Jane Doe", other_phone: "234-456-6789", other_contact_relationship: "Sister" )
+  patient1.save!
+
+# Add example of patient with appointment one week from today && clinic selected
+  patient2 = Patient.find_by( name: "Patient 2")
+  patient2.update_attributes( name: "Patient with clinic and appointment(1wk) - 2",clinic_name: "Sample Clinic 1", appointment_date: Time.now + (7*24*60*60) )
+  patient2.save!
+
+# # Add example of patient with a pledge submitted
+  patient3 = Patient.find_by( name: "Patient 3")
+  pledge = patient3.pledges.new()
+    pledge.created_by = user2
+    pledge.pledge_type = "naf"
+    pledge.amount = "3000"
+    pledge.save!
+  patient3.update_attributes( name: "Patient with a pledge submitted - 3", clinic_name: "Sample Clinic 1", appointment_date: Time.now + (10*24*60*60) )
+  patient3.save!  
+
+
+# # Add example of patient should be marked resolved without DCAF
+  patient5 = Patient.find_by( name: "Patient 5")
+  patient5.pregnancy.resolved_without_dcaf = true
+  patient5.update_attributes( name: "Patient resolved without DCAF - 5")
+  patient5.save!  
+
+
+# Add example of patient should have special circumstances
+  patient4 = Patient.find_by( name: "Patient 4")
+  patient4.update_attributes( name: "Patient with Special Circumstances - 4", special_circumstances: ["Prison", "Fetal anomaly"] )
+  patient4.save!  
+
+# All patients except one or two should have notes
 note_text = 'This is a note ' * 10
-Patient.find_by(name: 'Patient 2 with clinic and appointment(1wk)')
-       .notes.create! full_text: note_text,
-                      created_by: user2
+additional_note_text = 'Additional note ' * 10
+Patient.all.each do |patient|
+  unless patient.name == "Patient 0" || patient.name == "Patient 1 with Other Contact info"
+    patient.notes.create! full_text: note_text,
+                  created_by: user2
+  end
+  if patient.name[-1, 1].to_i.even?
+    patient.notes.create! full_text: additional_note_text,
+                  created_by: user2
+  end
+end
 
 # Adds Patients 0 thru 4 to regular call list
 user.add_patient Patient.find_by(name: 'Patient 0')
-user.add_patient Patient.find_by(name: 'Patient 1 with Other Contact info')
-user.add_patient Patient.find_by(name: 'Patient 2 with clinic and appointment(1wk)')
-user.add_patient Patient.find_by(name: 'Patient 3')
-user.add_patient Patient.find_by(name: 'Patient 4')
+user.add_patient Patient.find_by(name: 'Patient with Other Contact info - 1')
+user.add_patient Patient.find_by(name: 'Patient with clinic and appointment(1wk) - 2')
+user.add_patient Patient.find_by(name: 'Patient with a pledge submitted - 3')
+user.add_patient Patient.find_by(name: 'Patient resolved without DCAF - 5')
 
 # Add Patient 5 to completed calls list
-patient_in_completed_calls = Patient.find_by(name: 'Patient 5')
+patient_in_completed_calls = Patient.find_by(name: 'Patient with Special Circumstances - 4')
 user.add_patient patient_in_completed_calls
 patient_in_completed_calls.calls.create status: 'Left voicemail',
                                         created_by: user
