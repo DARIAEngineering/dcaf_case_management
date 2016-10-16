3d0a2c563e5fef8999dbb812d3b76ababde0abba
Matt Lively <matt.lively@mycase.com>
2016-10-16 16:54:17 -0700

    ml: remove unneeded error message method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,2 @@
 module ApplicationHelper
-  # helper method to display form validation errors
-  # see /users/new for examples
-  def error_message(obj, field)
-    return if obj.errors[field].empty?
-    str = ''
-    obj.errors[field].each do |error|
-      str += "<span class='form-error'>#{field.to_s.humanize} #{error}</span><br/>"
-    end
-    str.html_safe
-  end
 end
