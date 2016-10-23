488bf77b496d32ac3d88a91b013a5f140dad8364
colinxfleming <c3flemin@gmail.com>
2016-10-23 12:09:30 -0400

    programmatically generate random password on user create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,8 @@ class UsersController < ApplicationController
   def create
     raise 'Permission Denied' unless current_user.admin?
     @user = User.new(user_params)
+    hex = SecureRandom.urlsafe_base64
+    @user.password, @user.password_confirmation = hex
     if @user.save
       flash[:success] = 'User created!'
       redirect_to session.delete(:return_to)
