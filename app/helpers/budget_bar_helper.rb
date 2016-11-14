a1be3f7eb2374b1be2c41ab779dfbf270ffef875
colinxfleming <c3flemin@gmail.com>
2016-11-13 22:59:54 -0500

    reduce helper stuff

--- app/helpers/budget_bar_helper.rb
+++ app/helpers/budget_bar_helper.rb
@@ -1,43 +1,14 @@
 module BudgetBarHelper
-  def budget_bar(expenditures)
-    expenditures = { pledged: 500, sent: 250 }
-
-    content_tag :div, id: 'budget_bar' do
-      content_tag :div, class: 'progress' do
-        progress_bars expenditures
-      end
-    end
-  end
-
-  # TODO account for overflow
-  # TODO set min width
-  # TODO some simple explanatory text like in https://github.com/colinxfleming/dcaf_case_management/issues/710
-
-  private
-
-  def progress_bar(type, value)
-    content_tag :div, class: progress_bar_class(type),
-                      style: width(value) do
-      "$#{value.to_s}"
-    end
-  end
-
-  def progress_bars(expenditures)
-    set = [:pledged, :sent]
-    bars = set.map do |type|
-      progress_bar type, expenditures[type]
-    end
-    safe_join bars, ''
-  end
-
-  def progress_bar_class(type)
+  def progress_bar_color(type)
     color = type == :pledged ? 'warning' : 'success'
-    "progress-bar progress-bar-#{color} progress-bar-striped"
+    "progress-bar-#{color}"
   end
 
-  def width(value)
+  def progress_bar_width(value)
     "width: #{to_pct(value)}%"
   end
+
+  private
 
   def to_pct(value)
     pct = (value.to_f / 1_350.to_f) * 100
