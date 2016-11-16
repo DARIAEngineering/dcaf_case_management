620d064858005581ee8f24ce33c8fd9ca4cd4245
DarthHater <jeffryxtron@gmail.com>
2016-11-15 16:23:09 -0900

    WIP plus adjust controllers test

--- app/controllers/accountants_controller.rb
+++ app/controllers/accountants_controller.rb
@@ -5,19 +5,7 @@ class AccountantsController < ApplicationController
   def search
     @results = Patient.search params[:search]
 
-    @patient = Patient.new
-    @today = Time.zone.today.to_date
-    @phone = searched_for_phone?(params[:search]) ? params[:search] : ''
-    @name = searched_for_name?(params[:search]) ? params[:search] : ''
-
     respond_to { |format| format.js }
   end
 
-  def searched_for_phone?(query)
-    !/[a-z]/i.match query
-  end
-
-  def searched_for_name?(query)
-    /[a-z]/i.match query
-  end
 end
