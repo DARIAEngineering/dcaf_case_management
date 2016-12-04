72d9d5f416a977aabff0ee1ea000ece3d54a0a77
colinxfleming <c3flemin@gmail.com>
2016-12-03 21:21:50 -0500

    rewrite pledge test to use poltergeist, sppooooooooooky

--- test/integration/create_new_external_pledge_test.rb
+++ test/integration/create_new_external_pledge_test.rb
@@ -2,21 +2,28 @@ require 'test_helper'
 
 class CreateNewExternalPledgeTest < ActionDispatch::IntegrationTest
   before do
+    Capybara.current_driver = :poltergeist
     @user = create :user
     @patient = create :patient
     @pregnancy = create :pregnancy, patient: @patient
     log_in_as @user
     visit edit_patient_path @patient
+    wait_for_element 'Abortion Information'
+    click_link 'Abortion Information'
   end
+
+  after { Capybara.use_default_driver }
 
   describe 'creating and viewing a pledge' do
     it 'should let you create a new pledge' do
       select 'Baltimore Abortion Fund', from: 'Source'
-      fill_in 'Amount', with: '1001'
+      fill_in 'Amount', with: '30000'
       click_button 'Create External pledge'
+      wait_for_element 'Patient Information'
+      click_link 'Abortion Information'
+      wait_for_element 'Abortion information'
 
-      assert has_content? 'Baltimore Abortion Fund pledge'
-      assert has_content? '1001'
+      assert has_field? 'Baltimore Abortion Fund pledge', with: '30000'
     end
   end
 end
