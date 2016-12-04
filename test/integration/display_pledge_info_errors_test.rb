f8d7a0341d42717149c4610f63ff4bcbc9cbe40d
colinxfleming <c3flemin@gmail.com>
2016-12-03 22:21:30 -0500

    overhaul display pledge errors

--- test/integration/display_pledge_info_errors_test.rb
+++ test/integration/display_pledge_info_errors_test.rb
@@ -20,16 +20,21 @@ class DisplayPledgeInfoErrorsTest < ActionDispatch::IntegrationTest
       find('#submit-pledge-button').click
       assert has_text? 'Confirm the following information is correct'
       assert has_text? 'Data required:'
-      # TODO: Refute that button will let you continue
+      assert has_css? 'a.disabled', text: 'Continue'
     end
 
     it 'should not show errors when information is present' do
-      @patient = create :patient, clinic_name: 'Nice Clinic', appointment_date: DateTime.now + 14
+      @patient = create :patient, clinic_name: 'Nice Clinic',
+                                  appointment_date: 14.days.from_now
       @pregnancy = create :pregnancy, patient: @patient, dcaf_soft_pledge: 500
       visit edit_patient_path @patient
 
       find('#submit-pledge-button').click
       assert has_text? 'Confirm the following information is correct'
+      assert has_text? @patient.name
+      assert has_text? @patient.identifier
+      # TODO: check for pledge, appt date, and clinic
+
       find('#submit-pledge-to-p2').click
       refute has_text? 'Confirm the following information is correct'
     end
