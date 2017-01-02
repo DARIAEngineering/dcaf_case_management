97c239f214963a2587e724298f793bd84b93213f
DarthHater <jeffryxtron@gmail.com>
2017-01-02 13:48:02 -0900

    Changed a few things around, getting further

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
-      # You need to implement the method below in your model (e.g. app/models/user.rb)
       @user = User.from_omniauth(request.env["omniauth.auth"])
 
       if @user.persisted?
