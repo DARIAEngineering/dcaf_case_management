fab73b64befd6f9aedbb3c3e29316270903af943
DarthHater <jeffryxtron@gmail.com>
2017-01-03 12:21:00 -0900

    Remove comment, decided we don't want to create a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -83,13 +83,6 @@ class User
     data = access_token.info
     user = User.where(:email => data["email"]).first
 
-    # Uncomment the section below if you want users to be created if they don't exist
-    # unless user
-    #     user = User.create(name: data["name"],
-    #        email: data["email"],
-    #        password: Devise.friendly_token[0,20]
-    #     )
-    # end
     user
   end
 
