8a2164e2665329174cbde85296123ed631c3b63a
colinxfleming <c3flemin@gmail.com>
2017-01-08 14:30:24 -0500

    test failure to log in too

--- test/integration/google_sso_test.rb
+++ test/integration/google_sso_test.rb
@@ -5,15 +5,23 @@ class GoogleSSOTest < ActionDispatch::IntegrationTest
   describe 'access top page' do
     before do
       mock_omniauth
-      create :user, email: 'test@gmail.com'
     end
 
     it 'can sign in with Google Auth Account' do
-      visit('/users/sign_in')
+      @user = create :user, email: 'test@gmail.com'
+      visit root_path
       page.has_content?('Sign in with Google')
       click_link('Sign in with Google')
 
       assert has_content? 'Sign out'
+    end
+
+    it 'will reject sign ins if email is not associated with a user' do
+      visit root_path
+      assert has_content? 'Sign in'
+      click_link 'Sign in with Google'
+
+      assert has_content? 'Sign in'
     end
   end
 end
