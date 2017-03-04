b51e3dbf59fa46f46639c478dcbfb413f502a12a
Rob Froetscher <robfroetscher@gmail.com>
2017-03-04 15:20:11 -0800

    actually integrate the amounts into the front end

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,7 @@
 class ReportsController < ApplicationController
   def index
-    gon.weekly_report = Reports::LineSummary.generate(1.week.ago, Date.today)
-    gon.monthly_report = Reports::LineSummary.generate(1.month.ago, Date.today)
-    gon.yearly_report = Reports::LineSummary.generate(1.year.ago, Date.today)
+    @weekly_report = gon.weekly_report = Reports::LineSummary.generate(1.week.ago, Date.today)
+    @monthly_report = gon.monthly_report = Reports::LineSummary.generate(1.month.ago, Date.today)
+    @yearly_report = gon.yearly_report = Reports::LineSummary.generate(1.year.ago, Date.today)
   end
 end
