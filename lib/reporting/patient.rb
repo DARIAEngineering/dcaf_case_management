556199df0164d8e0d505fce2213dd23fb035e844
Rob Froetscher <robfroetscher@gmail.com>
2017-03-04 12:10:18 -0800

    make actual report logic work

--- lib/reporting/patient.rb
+++ lib/reporting/patient.rb
@@ -2,11 +2,31 @@ module Reporting
   class Patient
     class << self
       def contacted_for_line(line, start_date, end_date)
-        ::Patient.where(_line: line).count
+        ::Patient.where(_line: line).inject(0) do |acc, patient|
+          eligible_calls = patient
+            .calls
+            .where(created_at: (start_date..end_date))
+            .where(status: 'Reached patient')
+            .count
+
+          if eligible_calls > 0
+            acc += 1
+          end
+
+          acc
+        end
       end
 
       def new_contacted_for_line(line, start_date, end_date)
-        ::Patient.where(_line: line).count
+        ::Patient.where(_line: line).inject(0) do |acc, patient|
+          first_reached_call = patient.calls.where(status: 'Reached patient').order(created_at: 'asc').first
+
+          if first_reached_call && first_reached_call.created_at >= start_date && first_reached_call.created_at <= end_date
+            acc += 1
+          end
+
+          acc
+        end
       end
 
       def pledges_sent_for_line(line, start_date, end_date)
