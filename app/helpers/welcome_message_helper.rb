875a5db9f2534c3d280533eec1df7540d6a6201c
Rob Froetscher <robfroetscher@gmail.com>
2017-03-05 14:07:07 -0800

    just have the js on the specific page plus fix that array

--- app/helpers/welcome_message_helper.rb
+++ app/helpers/welcome_message_helper.rb
@@ -13,8 +13,8 @@ module WelcomeMessageHelper
       "Keep up the good work, you're doing great!",
       "We're all rooting for you!",
       'We appreciate all that you do!',
-      "From San Francisco with love!"
-      "You're a real gem - the client_finder gem team"
+      "From San Francisco with love!",
+      "You're a real gem - the client_finder gem team",
       "You're rad and your work is much appreciated!"
     ]
   end
