f3c69067caf9968859d63c63a6945a3a67f4a30d
colinxfleming <c3flemin@gmail.com>
2017-03-05 15:25:14 -0800

    titlize clinic name

--- app/controllers/clinicfinders_controller.rb
+++ app/controllers/clinicfinders_controller.rb
@@ -8,6 +8,7 @@ class ClinicfindersController < ApplicationController
     @nearest = @abortron.locate_nearest_clinic patient_zipcode: params[:zip].to_i.to_s,
                                                   gestational_age: params[:gestation].to_i
     @cheapest = @abortron.locate_cheapest_clinic(gestational_age: params[:gestation].to_i)
+
     respond_to { |format| format.js }
   end
 
