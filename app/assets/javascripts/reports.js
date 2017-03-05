bbc60a040c3b3c0757f36cc417e3f6eaefffe0dc
Rob Froetscher <robfroetscher@gmail.com>
2017-03-04 20:53:55 -0800

    remove useless comment

--- app/assets/javascripts/reports.js
+++ app/assets/javascripts/reports.js
@@ -1,7 +1,3 @@
-// # Place all the behaviors and hooks related to the matching controller here.
-// # All this logic will automatically be available in application.js.
-// # You can use CoffeeScript in this file: http://coffeescript.org/
-
 $(function() {
   ['weekly_report', 'monthly_report', 'yearly_report'].forEach(function(reportName) {
     var chart = c3.generate({
