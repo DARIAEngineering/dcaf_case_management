e536f887066ff6d538f5946eb7a6055e20e54fce
colinxfleming <c3flemin@gmail.com>
2017-03-23 20:01:23 -0400

    linting

--- app/services/pledge_form_generator.rb
+++ app/services/pledge_form_generator.rb
@@ -6,29 +6,28 @@ class PledgeFormGenerator
     @user = user
   end
 
-
   def patient_amount
     @patient.pregnancy.dcaf_soft_pledge
   end
 
   # TODO when clinics actually exist instead of just name, do this
-  def get_provider_address
-    "Washington, District of Colombia"
+  def provider_address
+    'Washington, District of Colombia'
   end
 
-  def get_patient_provider_name
+  def patient_provider_name
     patient.clinic_name
   end
 
-  def get_patient_date
+  def patient_date
     patient.appointment_date.strftime "%B%e, %Y"
   end
 
-  def get_patient_name
+  def patient_name
     patient.name
   end
 
-  def get_user_name
+  def user_name
     user.name
   end
 
@@ -37,22 +36,23 @@ class PledgeFormGenerator
 
     # header
     y_position = pdf.cursor
-    pdf.bounding_box([0, y_position], :width => 200, :height => 100) do
-      pdf.image "#{Rails.root.join("public", "dcaf_logo.png")}", position: :left, width: 200
+    pdf.bounding_box([0, y_position], width: 200, height: 100) do
+      pdf.image '#{Rails.root.join("public", "dcaf_logo.png")}',
+                position: :left, width: 200
     end
-    pdf.bounding_box([250, y_position], :width => 200, :height => 100) do
-      pdf.text "DC Abortion Fund"
-      pdf.text "P.O. Box 65061"
-      pdf.text "Tel: 202-452-7464"
-      pdf.text "E-mail: info@dcabortionfund.org"
-      pdf.text "Web: www.dcabortionfund.org"
+    pdf.bounding_box([250, y_position], width: 200, height: 100) do
+      pdf.text 'DC Abortion Fund'
+      pdf.text 'P.O. Box 65061'
+      pdf.text 'Tel: 202-452-7464'
+      pdf.text 'E-mail: info@dcabortionfund.org'
+      pdf.text 'Web: www.dcabortionfund.org'
     end
     pdf.stroke_horizontal_rule
 
     # main text with patient info
     pdf.move_down 10
     patient_info_block = <<-TEXT
-      We are writing to confirm that the DC Abortion Fund (DCAF) is pledging assistance in the amount of $#{patient_amount} toward the cost of abortion care for Patient #{get_patient_name} (D0-0000) on #{get_patient_date}. This payment will be remitted to the abortion provider, #{get_patient_provider_name} located in #{get_provider_address}.
+      We are writing to confirm that the DC Abortion Fund (DCAF) is pledging assistance in the amount of $#{patient_amount} toward the cost of abortion care for Patient #{patient_name} (D0-0000) on #{patient_date}. This payment will be remitted to the abortion provider, #{patient_provider_name} located in #{provider_address}.
 
       In order to receive payment, the abortion provider must mail a copy of this pledge form to:
     TEXT
@@ -76,15 +76,15 @@ class PledgeFormGenerator
     # signature portion
     next_y_position = pdf.cursor
     pdf.move_down 10
-    pdf.bounding_box([0, next_y_position], :width => 200, :height => 50) do
-      pdf.draw_text get_user_name, at: [10, 35]
-      pdf.line [0,25], [225, 25]
-      pdf.draw_text "Case Manager, DC Abortion Fund", at: [10,10]
+    pdf.bounding_box([0, next_y_position], width: 200, height: 50) do
+      pdf.draw_text user_name, at: [10, 35]
+      pdf.line [0, 25], [225, 25]
+      pdf.draw_text 'Case Manager, DC Abortion Fund', at: [10, 10]
     end
-    pdf.bounding_box([250, next_y_position], :width => 200, :height => 50) do
-      pdf.draw_text "February 26, 2017",at: [10,35]
-      pdf.line [0,25], [225, 25]
-      pdf.draw_text "Date", at: [10,10]
+    pdf.bounding_box([250, next_y_position], width: 200, height: 50) do
+      pdf.draw_text 'February 26, 2017', at: [10, 35]
+      pdf.line [0, 25], [225, 25]
+      pdf.draw_text 'Date', at: [10, 10]
     end
     pdf.move_down 10
 
