51f92dc7ffb4dd6f64240fbea32dd74dd75e188c
colinxfleming <c3flemin@gmail.com>
2017-04-22 02:21:46 -0400

    well i'll be, it works

--- app/models/concerns/history_trackable.rb
+++ app/models/concerns/history_trackable.rb
@@ -2,6 +2,15 @@
 module HistoryTrackable
   extend ActiveSupport::Concern
 
+  included do
+    include Mongoid::History::Trackable
+    track_history on: self.fields.keys + [:updated_by_id],
+                  version_field: :version,
+                  track_create: true,
+                  track_update: true,
+                  track_destroy: true
+  end
+
   def recent_history_tracks
     history_tracks.select { |ht| ht.updated_at > 6.days.ago }
   end
