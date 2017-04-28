354112288c1e2636a20a1e5d9b94a555e8c69a27
colinxfleming <c3flemin@gmail.com>
2017-04-28 12:52:53 -0700

    remove pregnancy from call list and change log test

--- test/integration/call_list_test.rb
+++ test/integration/call_list_test.rb
@@ -4,11 +4,8 @@ class CallListTest < ActionDispatch::IntegrationTest
   before do
     Capybara.current_driver = :poltergeist
     @patient = create :patient, name: 'Susan Everyteen'
-    @pregnancy = create :pregnancy, patient: @patient
     @patient_2 = create :patient, name: 'Thorny'
-    @pregnancy_2 = create :pregnancy, patient: @patient_2
     @va_patient = create :patient, name: 'James Hetfield', line: 'VA'
-    @va_pregnancy = create :pregnancy, patient: @va_patient
     @user = create :user
     @user.add_patient @va_patient
     log_in_as @user
