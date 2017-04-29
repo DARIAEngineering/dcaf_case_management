640c5811915e05b99961b0cbc15b093ccd2d9a07
DarthHater <jeffryxtron@gmail.com>
2017-04-29 13:42:02 -0800

    One more try at this

--- scripts/gemnasium_autoupdate.sh
+++ scripts/gemnasium_autoupdate.sh
@@ -4,6 +4,5 @@ if [[ $CIRCLE_BRANCH =~ "gemnasium-auto-update" ]]; then
   echo "skipping build"; 
 else
   echo "running autoupdate"
-  gemnasium auth login --token $GEMNASIUM_TOKEN
-  gemnasium autoupdate run;
+  gemnasium --token $GEMNASIUM_TOKEN autoupdate run;
 fi
