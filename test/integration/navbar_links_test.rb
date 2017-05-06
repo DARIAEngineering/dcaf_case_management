5eb0b98fe4dc9db72cfb0c36db2ae350d4d29f2a
Ashlynn Pai <ashlynnpai@gmail.com>
2017-05-06 23:56:18 +0000

    remove dashboard links tests

--- test/integration/navbar_links_test.rb
+++ test/integration/navbar_links_test.rb
@@ -6,37 +6,6 @@ class NavbarLinksTest < ActionDispatch::IntegrationTest
     log_in_as @user
   end
 
-<<<<<<< HEAD
-=======
-  describe 'link to dashboard' do
-    describe 'visiting the dashboard' do
-      it 'should not display the dashboard link' do
-        visit authenticated_root_path
-        wait_for_element 'Sign out'
-      end
-    end
-
-    describe 'visiting a page other than the dashboard' do
-      before do
-        @patient = create :patient
-        visit edit_patient_path(@patient)
-        wait_for_element 'Patient information'
-      end
-
-      it 'should display the dashboard link' do
-        assert has_link? 'Dashboard', href: authenticated_root_path
-      end
-
-      it 'should direct the user to the dashboard' do
-        find('a', text: 'Dashboard').click
-        wait_for_element 'Build your call list'
-        assert_equal current_path, authenticated_root_path
-        refute has_link? 'Dashboard', href: authenticated_root_path
-      end
-    end
-  end
-
->>>>>>> 19942bfe640d6bfeb2782f5c2c66808cd4e74781
   describe 'additional resources link' do
     it 'should display if env var is set' do
       ENV['CM_RESOURCES_URL'] = 'www.google.com'
