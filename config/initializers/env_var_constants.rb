2fb6b4ffc3bed27cba86c2368325286abbe7ff40
colinxfleming <c3flemin@gmail.com>
2017-05-20 00:35:21 -0400

    fund and fund acronym as env vars

--- config/initializers/env_var_constants.rb
+++ config/initializers/env_var_constants.rb
@@ -7,3 +7,7 @@ LINES = if ENV['LINES'].present?
         else
           %w(DC MD VA).map(&:to_sym).freeze
         end
+
+# Definition of fund
+FUND = ENV['FUND'] || 'DC Abortion Fund'
+FUND_ACRONYM = ENV['DARIA_FUND_ABBREV'] || 'DCAF'
