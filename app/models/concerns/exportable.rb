61d60707258e0187b9542c732a223ed79e97a89e
colinxfleming <c3flemin@gmail.com>
2017-05-20 17:07:58 -0400

    add age range and pregnancy fields

--- app/models/concerns/exportable.rb
+++ app/models/concerns/exportable.rb
@@ -11,7 +11,7 @@ module Exportable
     "Voicemail Preference" => :voicemail_preference,
     "Line" => :line,
     "Spanish?" => :spanish,
-    "Age" => :age,
+    "Age" => :age_range,
     "State" => :state,
     "County" => :county,
     "Race/Ethnicity" => :race_ethnicity,
@@ -24,11 +24,50 @@ module Exportable
     "Appointment Date" => :appointment_date,
     "Initial Call Date" => :initial_call_date,
     "Urgent?" => :urgent_flag,
-    "Special Circumstances" => :special_circumstances 
-  }
+    "Special Circumstances" => :special_circumstances,
+    "Intake LMP" => :last_menstrual_period_weeks,
+    "Race or Ethnicity" => :race_ethnicity,
+    "Employment status" => :employment_status,
+    "Insurance" => :insurance,
+    "Income" => :income,
+    "Patient referred by" => :referred_by,
+    "Referred to clinic by fund" => :referred_to_clinic,
+    "Abortion cost" => :procedure_cost,
+    "Patient contribution" => :patient_contribution,
+    "NAF pledge" => :naf_pledge,
+    "Fund pledge" => :dcaf_soft_pledge,
+    "Abortion cost" => :procedure_cost,
+    "Pledge sent" => :pledge_sent,
+    "Resolved without fund assistance" => :resolved_without_dcaf,
+    "Pledge generated time" => :pledge_generated_at
+
+    # TODO clinic stuff
+    # TODO call stuff
+    # TODO fulfillment stuff
+    # TODO external pledges
+  }.freeze
 
   def has_alt_contact?
     other_contact.present? || other_phone.present? || other_contact_relationship.present?
+  end
+
+  def age_range
+    case age
+    when nil
+      nil
+    when 0..18
+      'Under 18'
+    when 18..24
+      '18-24'
+    when 25..34
+      '25-34'
+    when 35..44
+      '35-44'
+    when 45..54
+      '45-54'
+    when 55..100
+      '55+'
+    end
   end
 
   def get_field_value_for_serialization(field)
