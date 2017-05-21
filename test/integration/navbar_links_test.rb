898b511bab4edc3b3e86eb2fedc75ac88a7c7fac
colinxfleming <c3flemin@gmail.com>
2017-05-20 23:37:42 -0400

    add link to CSV, data specific options, and adjust tests

--- test/integration/navbar_links_test.rb
+++ test/integration/navbar_links_test.rb
@@ -19,26 +19,30 @@ class NavbarLinksTest < ActionDispatch::IntegrationTest
       refute has_link? 'CM Resources'
     end
   end
-  
+
   describe 'user dropdown' do
     before { click_link @user.name }
     it 'should display the profile link' do
-      assert has_link? 'My Profile', edit_user_registration_path
+      assert has_link? 'My Profile', href: edit_user_registration_path
     end
   end
-  
+
   describe 'admin dropdown' do
     before { click_link 'Admin' }
     it 'should display the Clinic Management link' do
-      assert has_link? 'Clinic Management', clinics_path
+      assert has_link? 'Clinic Management', href: clinics_path
     end
-    
+
     it 'should display the Accounting link' do
-      assert has_link? 'Accounting', accountants_path
+      assert has_link? 'Accounting', href: accountants_path
     end
-    
+
     it 'should display the Reporting link' do
-      assert has_link? 'Reporting', reports_path
+      assert has_link? 'Reporting', href: reports_path
+    end
+
+    it 'should display the export link' do
+      assert has_link? 'Export anonymized CSV', href: patients_path(format: :csv)
     end
   end
 end
