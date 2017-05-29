1af9d27978055b29ef3bf0dc3f0d4001cb46efc8
colinxfleming <c3flemin@gmail.com>
2017-05-29 10:10:36 -0400

    abstract DCAF out of the mailers

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'no-reply@dcaf.org'
+  default from: "no-reply@#{ENV['FUND_DOMAIN']}.org"
   layout 'mailer'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,14 +1,14 @@
 # app/mailers/user_mailer.rb
 class UserMailer < Devise::Mailer
-  default from: 'no-reply@dcabortionfund.org'
+  default from: "no-reply@#{ENV['FUND_DOMAIN']}.org"
 
   def password_changed(id)
     @user = User.find(id)
-    mail to: @user.email, subject: 'Your password has changed'
+    mail to: @user.email, subject: 'Your DARIA password has changed'
   end
 
   def account_created(id)
     @user = User.find(id)
-    mail to: @user.email, subject: 'Your DCAF account has been created'
+    mail to: @user.email, subject: 'Your DARIA account has been created'
   end
 end
