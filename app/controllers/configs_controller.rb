ac03e75c37208862ea6802485a908c6dd047fa80
colinxfleming <c3flemin@gmail.com>
2017-06-04 15:23:12 -0400

    dug myself into a hole on this one

--- app/controllers/configs_controller.rb
+++ app/controllers/configs_controller.rb
@@ -5,20 +5,21 @@ class ConfigsController < ApplicationController
     @configs = Config.all
   end
 
-  def create
-    new_config = Config.new config_params
-    new_config.created_by = current_user
-    if new_config.save
-      flash[:success] = 'Config created successfully'
-      redirect_to config_path
-    else
-      flash[:danger] = 'Config failed to create'
-      render 'index'
-    end
-  end
+  # def create
+  #   new_config = Config.new config_params
+  #   new_config.created_by = current_user
+  #   if new_config.save
+  #     flash[:success] = 'Config created successfully'
+  #     redirect_to config_path
+  #   else
+  #     flash[:danger] = 'Config failed to create'
+  #     render 'index'
+  #   end
+  # end
 
   def update
     @config = Config.find params[:id]
+    @config.config_value[:options] = config_params # this is not going to work
     if @config.save
       flash[:success] = 'Config updated successfully'
       redirect_to config_path
@@ -26,5 +27,11 @@ class ConfigsController < ApplicationController
       flash[:danger] = 'Config failed to update'
       render 'index'
     end
+  end
+
+  private
+
+  def config_params
+    params.require(:config).permit(:option_value)
   end
 end
