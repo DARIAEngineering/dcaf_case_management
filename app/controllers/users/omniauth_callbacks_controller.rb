67e60ecae7a49f5592e0ef288476daad9667cafa
colinxfleming <c3flemin@gmail.com>
2017-06-08 17:08:12 -0400

    break up omniauth callbacks controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,12 +6,22 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env['omniauth.auth'])
 
     if @user.persisted?
-      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success',
-                              kind: 'Google'
-      sign_in_and_redirect @user, event: :authentication
+      log_in_user_via_google
     else
-      session['devise.google_data'] = request.env['omniauth.auth'].except(:extra)
-      redirect_to root_path
+      reject_login
     end
+  end
+
+  private
+
+  def log_in_user_via_google
+    flash[:notice] = I18n.t 'devise.omniauth_callbacks.success',
+                            kind: 'Google'
+    sign_in_and_redirect @user, event: :authentication
+  end
+
+  def reject_login
+    session['devise.google_data'] = request.env['omniauth.auth'].except(:extra)
+    redirect_to root_path
   end
 end
