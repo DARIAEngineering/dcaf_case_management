194f215b7e5c5fedf9ed5048ba9c9bfaaf0cb00c
colinxfleming <c3flemin@gmail.com>
2017-06-24 13:13:48 -0400

    adjust whitelisted fields to just name and email from user update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,9 @@ class ApplicationController < ActionController::Base
   # whitelists attributes in devise
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update) do |user|
+      user.permit :name, :current_password
+    end
   end
 
   private
