430a87fcb8167dc45a2b64e832762ba470175730
Ting Lew <ting.lew@gmail.com>
2017-06-26 13:20:28 -0400

    working rendering async, allowing unsafe-inline

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,25 +1,21 @@
 # Controller for automatically generated service reporting across lines.
 class ReportsController < ApplicationController
   def index
-    # @weekly_report = gon.weekly_report = Reports::LineSummary.generate(1.week.ago, Date.today)
-    # @monthly_report = gon.monthly_report = Reports::LineSummary.generate(1.month.ago, Date.today)
-    # @yearly_report = gon.yearly_report = Reports::LineSummary.generate(1.year.ago, Date.today)
+
   end
 
   def weekly_report
-    puts 'going async'
-    @weekly_report = gon.weekly_report = Reports::LineSummary.generate(1.week.ago, Date.today)
+    @weekly_report = Reports::LineSummary.generate(1.week.ago, Date.today)
     render partial: "call_line", locals: {weekly_report: @weekly_report}
   end
 
   def monthly_report
-    puts 'going async'
-    @monthly_report = gon.monthly_report = Reports::LineSummary.generate(1.month.ago, Date.today)
-    render partial: "past_thirty_days"
+    @monthly_report = Reports::LineSummary.generate(1.month.ago, Date.today)
+    render partial: "past_thirty_days", locals: {monthly_report: @monthly_report}
   end
 
   def yearly_report
-    @yearly_report = gon.yearly_report = Reports::LineSummary.generate(1.year.ago, Date.today)
-    render partial: "year_to_date"
+    @yearly_report = Reports::LineSummary.generate(1.year.ago, Date.today)
+    render partial: "year_to_date", locals: {yearly_report: @yearly_report}
   end
 end
