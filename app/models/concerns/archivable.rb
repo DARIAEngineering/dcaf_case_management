33dc026010c1c7b44e9b5a0571ca5b5cbe81abfc
Kathryn Tipton <kathryn.tipton@gmail.com>
2017-08-01 20:08:41 -0400

    Cleanup archivable concern
    Bumps DCAFEngineering/dcaf_case_management#812

--- app/models/concerns/archivable.rb
+++ app/models/concerns/archivable.rb
@@ -3,94 +3,9 @@ require 'csv'
 # Methods pertaining to patient export
 module Archivable
   extend ActiveSupport::Concern
-  # Only these fields will survive the purge
-  PATIENT_ARCHIVE_SAFE = {
-    "BSON ID" => :id,
-    "Archived" => :archived, #new field
-    "Line" => :line,
-    "Age" => :age_range,
-#    "Race/Ethnicity" => :race_ethnicity,
-#    "Spanish?" => :spanish,
-#    "Voicemail Preference" => :voicemail_preference,
-#    "State" => :state,
-#    "County" => :county,
-#    "City" => :city,
-#    "Has Alt Contact?" => :has_alt_contact?, #new field
-#
-#    "Employment Status" => :employment_status,
-#    "Insurance" => :insurance,
-#    "Income" => :income,
-#    "Referred By" => :referred_by,
-#    "Referred to clinic by fund" => :referred_to_clinic,
-#    "Appointment Date" => :appointment_date,
-#    "Initial Call Date" => :initial_call_date,
-#    "Urgent?" => :urgent_flag,
-#    "LMP at intake (weeks)" => :last_menstrual_period_weeks,
-#    "Race or Ethnicity" => :race_ethnicity,
-#    "Employment status" => :employment_status,
-#    "Abortion cost" => :procedure_cost,
-#    "Patient contribution" => :patient_contribution,
-#    "NAF pledge" => :naf_pledge,
-#    "Fund pledge" => :fund_pledge,
-#    "Clinic" => :export_clinic_name,
-#    "Pledge sent" => :pledge_sent,
-#    "Resolved without fund assistance" => :resolved_without_fund,
-#    "Pledge generated time" => :pledge_generated_at
-
-    # TODO clinic stuff
-    # TODO call stuff
-    # TODO fulfillment stuff
-    # TODO external pledges
-    # TODO test to confirm that specific blacklisted fields aren't being exported
-  }.freeze
-
-  def fulfilled
-    fulfillment.try :fulfilled
-  end
-
-  def procedure_date
-    fulfillment.try :procedure_date
-  end
-
-  def gestation_at_procedure
-    fulfillment.try :gestation_at_procedure
-  end
-
-  def procedure_cost_amount
-    fulfillment.try :procedure_cost
-  end
-
-  def check_number
-    fulfillment.try :check_number
-  end
-
-  def date_of_check
-    fulfillment.try :date_of_check
-  end
-
-  def first_call_timestamp
-    calls.first.try :created_at
-  end
-
-  def last_call_timestamp
-    calls.last.try :created_at
-  end
-
-  def call_count
-    calls.count
-  end
-
-  def reached_patient_call_count
-    reached_calls = calls.select {|call| call.status == "Reached patient"}
-    reached_calls.count
-  end
 
   def has_alt_contact?
     other_contact.present? || other_phone.present? || other_contact_relationship.present?
-  end
-
-  def export_clinic_name
-    clinic.try :name
   end
 
   def age_range
@@ -111,25 +26,6 @@ module Archivable
       '55+'
     else
       'Bad value'
-    end
-  end
-
-  def preferred_language
-    case language
-    when nil, ''
-      'English'
-    else
-      language
-    end
-  end
-
-  def get_field_value_for_serialization(field)
-    value = public_send(field)
-    if value.is_a?(Array)
-      # Use simpler serialization for Array values than the default (`to_s`)
-      value.reject(&:blank?).join(', ')
-    else
-      value
     end
   end
 
@@ -189,20 +85,23 @@ module Archivable
     end
 end
 
-#  CSV_EXPORT_FIELDS = {
+#  # Only these fields will survive the purge
+#  TODO : mark as Save, Scramble, Clear, Transform
+#
+#  PATIENT_ARCHIVE_SAFE = {
 #    "BSON ID" => :id,
-#    # "Identifier" => :identifier,
-#    "Has Alt Contact?" => :has_alt_contact?,
-#    "Voicemail Preference" => :voicemail_preference,
+#    "Archived" => :archived, #new field
 #    "Line" => :line,
-#    "Language" => :preferred_language,
 #    "Age" => :age_range,
+#    "Race/Ethnicity" => :race_ethnicity,
+#    "Spanish?" => :spanish,
+#    "Voicemail Preference" => :voicemail_preference,
 #    "State" => :state,
 #    "County" => :county,
-#    "Race or Ethnicity" => :race_ethnicity,
+#    "City" => :city,
+#    "Has Alt Contact?" => :has_alt_contact?, #new field
+#
 #    "Employment Status" => :employment_status,
-#    "Minors in Household" => :household_size_children,
-#    "Adults in Household" => :household_size_adults,
 #    "Insurance" => :insurance,
 #    "Income" => :income,
 #    "Referred By" => :referred_by,
@@ -210,8 +109,9 @@ end
 #    "Appointment Date" => :appointment_date,
 #    "Initial Call Date" => :initial_call_date,
 #    "Urgent?" => :urgent_flag,
-#    # "Special Circumstances" => :special_circumstances,
 #    "LMP at intake (weeks)" => :last_menstrual_period_weeks,
+#    "Race or Ethnicity" => :race_ethnicity,
+#    "Employment status" => :employment_status,
 #    "Abortion cost" => :procedure_cost,
 #    "Patient contribution" => :patient_contribution,
 #    "NAF pledge" => :naf_pledge,
@@ -219,25 +119,12 @@ end
 #    "Clinic" => :export_clinic_name,
 #    "Pledge sent" => :pledge_sent,
 #    "Resolved without fund assistance" => :resolved_without_fund,
-#    "Pledge generated time" => :pledge_generated_at,
-#
-#    # Call related
-#    "Timestamp of first call" => :first_call_timestamp,
-#    "Timestamp of last call" => :last_call_timestamp,
-#    "Call count" => :call_count,
-#    "Reached Patient call count" => :reached_patient_call_count,
-#
-#    # Fulfillment related
-#    "Fulfilled" => :fulfilled,
-#    "Procedure date" => :procedure_date,
-#    "Gestation at procedure in weeks" => :gestation_at_procedure,
-#    "Procedure cost" => :procedure_cost,
-#    "Check number" => :check_number,
-#    "Date of Check" => :date_of_check
-#
-#    # TODO clinic stuff
-#    # TODO external pledges
-#    # TODO test to confirm that specific blacklisted fields aren't being exported
-#  }.freeze
+#    "Pledge generated time" => :pledge_generated_at
 
+    # TODO clinic stuff
+    # TODO call stuff
+    # TODO fulfillment stuff
+    # TODO external pledges
+    # TODO test to confirm that specific blacklisted fields aren't being exported
+  }.freeze
 
