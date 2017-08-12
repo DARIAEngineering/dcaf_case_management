422b1a141aa4e168549e35595c5c5d58ce3140b5
colinxfleming <c3flemin@gmail.com>
2017-08-12 15:45:54 -0400

    tidying

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,19 +7,12 @@ SimpleCov.formatter = SimpleCov::Formatter::Codecov
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-# require 'minitest/reporters'
+require 'minitest/reporters'
 require 'minitest/autorun'
-# require 'capybara/rails'
-require 'capybara/poltergeist'
-# require 'capybara-screenshot/minitest'
 require 'omniauth_helper'
 require 'rack/test'
-# require 'integration_system_test_helpers'
-# Minitest::Reporters.use! Minitest::Reporters::ProgressReporter
-
-# Capybara.register_driver :poltergeist do |app|
-  # Capybara::Poltergeist::Driver.new(app, js_errors: false)
-# end
+require 'capybara/poltergeist'
+Minitest::Reporters.use! Minitest::Reporters::ProgressReporter.new
 
 DatabaseCleaner.clean_with :truncation
 
@@ -30,11 +23,11 @@ class ActiveSupport::TestCase
   after  { DatabaseCleaner.clean }
 end
 
-# Save screenshots if integration tests fail
-Capybara.save_path = "#{ENV.fetch('CIRCLE_ARTIFACTS', Rails.root.join('tmp/capybara'))}" if ENV['CIRCLE_ARTIFACTS']
+# Save screenshots if integration tests fail - I don't THINK we need this anymore
+# Capybara.save_path = "#{ENV.fetch('CIRCLE_ARTIFACTS', Rails.root.join('tmp/capybara'))}" if ENV['CIRCLE_ARTIFACTS']
 
 class ActionDispatch::IntegrationTest
-  # include IntegrationSystemTestHelpers
+  # de facto controller tests
 
   def sign_in(user)
     post user_session_path \
@@ -45,9 +38,4 @@ class ActionDispatch::IntegrationTest
   def choose_line(line)
     post lines_path, params: { line: line.to_s }
   end
-end
-
-class ActionController::TestCase
-  # include Devise::TestHelpers
-  include Devise::Test::ControllerHelpers
 end
