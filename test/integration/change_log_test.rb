8a5ae90f2a52f21f7f889ee4fa19cf319940ed10
colinxfleming <c3flemin@gmail.com>
2017-08-22 23:50:59 -0400

    improve change log test

--- test/integration/change_log_test.rb
+++ test/integration/change_log_test.rb
@@ -4,14 +4,13 @@ class ChangeLogTest < ActionDispatch::IntegrationTest
   before do
     Capybara.current_driver = :poltergeist
     @user = create :user, email: 'first_user@email.com'
-    @user2 = create :user, email: 'second_user@email.com'
-    log_in_as @user
     @patient = create :patient, name: 'tester',
                                 primary_phone: '1231231234',
-                                created_by: @user2
+                                created_by: @user,
+                                city: 'Washington'
 
+    log_in_as @user
     visit edit_patient_path(@patient)
-    fill_in 'City', with: 'Washington'
   end
 
   after do
@@ -20,8 +19,9 @@ class ChangeLogTest < ActionDispatch::IntegrationTest
 
   describe 'viewing the changelog' do
     it 'should log patient accessibly' do
-      fill_in 'City', with: 'Washington'
-      visit edit_patient_path(@patient)
+      fill_in 'City', with: 'Canada'
+      click_away_from_field
+
       wait_for_element 'Change Log'
       click_link 'Change Log'
       wait_for_element 'Patient history'
@@ -29,7 +29,7 @@ class ChangeLogTest < ActionDispatch::IntegrationTest
       within :css, '#change_log' do
         assert has_text? Time.zone.now.display_date
         assert has_text? 'City'
-        assert has_text? 'Washington'
+        assert has_text? 'Canada'
         assert has_text? @user.name
       end
     end
