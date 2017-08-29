33c907429dd05c1cad186eaa8bf46d911220cebd
colinxfleming <c3flemin@gmail.com>
2017-08-28 21:27:59 -0400

    event stream rendering

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,7 +1,16 @@
 module EventsHelper
   def display_event(event)
-    # glyphicon = content_tag :span, class: "glyphicon glyphicon-#{event.glyphicon}"
-    # safe_join [glyphicon, event.to_log_entry], ' '
-    event.to_log_entry
+    safe_join [
+      # TODO style this so it isn't the same as he regular call buttons
+      tag(:span, class: ['glyphicon', "glyphicon-#{event.glyphicon}"]),
+      log_entry(event)
+    ], ' '
+  end
+
+  def log_entry(event)
+    time = "#{event.created_at.display_date} #{event.created_at.display_time}"
+    str = "-- #{event.cm_name} #{event.event_text}"
+    link = link_to event.patient_name, edit_patient_path(event.patient_id)
+    safe_join [time, str, link], ' '
   end
 end
