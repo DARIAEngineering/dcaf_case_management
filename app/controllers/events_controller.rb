df6d6b80bc92bfbc8ca9c0ba288627cbef242790
colinxfleming <c3flemin@gmail.com>
2017-09-02 14:02:15 -0400

    write and adjust to integration test

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,11 +3,9 @@ class EventsController < ApplicationController
   include LinesHelper
 
   def index
-    puts current_line
     @events = Event.where(line: current_line)
                    .order_by(created_at: :desc)
 
     render partial: 'events/events'
-    # respond_to { |format| format.js }
   end
 end
