327a464592d188a50d64914ee2028f31ee7dd101
Ting Lew <ting.lew@gmail.com>
2017-10-10 23:13:41 -0400

    allow only admins to access accounting page

--- app/controllers/accountants_controller.rb
+++ app/controllers/accountants_controller.rb
@@ -1,11 +1,10 @@
 # Controller pertaining to accountant functions, usually fulfillments.
 class AccountantsController < ApplicationController
+  before_action :confirm_admin_user
+  
   def index
     @patients = Patient.where('fulfillment.fulfilled' => true)
-    # change per to 25
     @patients = Kaminari.paginate_array(@patients).page(params[:page]).per(25)
-
-
   end
 
   def search
