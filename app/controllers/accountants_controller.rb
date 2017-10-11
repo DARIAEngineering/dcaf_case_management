ad5e3ca6e3280be080a08f91407b4f6856195d5b
Ting Lew <ting.lew@gmail.com>
2017-10-10 22:04:21 -0400

    working search by patient name

--- app/controllers/accountants_controller.rb
+++ app/controllers/accountants_controller.rb
@@ -3,14 +3,13 @@ class AccountantsController < ApplicationController
   def index
     @patients = Patient.where('fulfillment.fulfilled' => true)
     # change per to 25
-    @patients = Kaminari.paginate_array(@patients).page(params[:page]).per(1)
-    
+    @patients = Kaminari.paginate_array(@patients).page(params[:page]).per(25)
+
 
   end
 
   def search
-    @results = Patient.search params[:search]
-
+    @results = Patient.where('fulfillment.fulfilled' => true).where(:name => /#{params[:search]}/i)
     respond_to { |format| format.js }
   end
 end
