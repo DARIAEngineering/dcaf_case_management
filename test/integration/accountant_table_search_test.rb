fefb6549d1e2b71a6f939b6596f2eb9730fa2580
Ting Lew <ting.lew@gmail.com>
2017-10-11 00:39:35 -0400

    add basic integration test without phantom

--- test/integration/accountant_table_search_test.rb
+++ test/integration/accountant_table_search_test.rb
@@ -2,13 +2,28 @@ require 'test_helper'
 
 class AccountantTableSearchTest < ActionDispatch::IntegrationTest
   before do
-    Capybara.current_driver = :poltergeist
+    # Capybara.current_driver = :poltergeist
     @user = create :user
-    @patient = create :patient, name: 'Susan Everyteen'
-    @patient_2 = create :patient, name: 'Thorny'
+    @clinic = create :clinic, name: 'a real clinic'
+    @patient = create :patient, name: 'Susan Everyteen Quarry Eileene Zarha', clinic: @clinic
+    @patient_2 = create :patient, name: 'Thorny Zarha', clinic: @clinic
+    @patient_3_no_fulfillment = create :patient, name: 'Needs Fulfillment Texas'
+
+    @fulfillment = create :fulfillment, fulfilled: true, patient: @patient, created_by: @user
+    @fulfillment_2 = create :fulfillment, fulfilled: true, patient: @patient_2, created_by: @user
+
   end
 
+  # after { Capybara.use_default_driver }
+
   describe 'only admins can access billing information' do
+    before do
+      log_in_as @user
+    end
+    it 'should be unable to access accounting page' do
+      visit accountant_path
+      refute has_text? 'Accounting'
+    end
 
   end
 
@@ -16,10 +31,44 @@ class AccountantTableSearchTest < ActionDispatch::IntegrationTest
     before do
       @user.update role: :admin
       @user.reload
-      visit authenticated_root_path
+      log_in_as @user
+
+    end
+
+    it 'should navigate to accounting page' do
+      visit dashboard_path
+
       click_link 'Admin tools'
+      assert_text 'Accounting'
+
       wait_for_element 'Accounting'
       click_link 'Accounting'
+      wait_for_ajax
+      wait_for_element 'Search Cases'
+      assert has_content? 'Search Cases'
+    end
+
+    it 'populates with correct number of relevant patients' do
+      page.has_css?("tr", :count => 3)
+    end
+
+    it 'generates proper initials' do
+      assert has_text? 'SEQEZ'
+      assert has_text? 'TZ'
+      refute has_text? 'NFT'
+    end
+
+    it 'searching will find relevant patients' do
+      fill_in 'search', with: 'Thorny'
+      click_button 'Search'
+
+      page.has_css?("tr", :count => 2)
+
+      # fill_in 'search', with: 'Zarha'
+      # click_button 'Search'
+      #
+      # page.has_css?("tr", :count => 3)
+
     end
   end
 end
