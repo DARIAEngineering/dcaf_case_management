b9e525482df7a8016c710a469a3799a340eab836
colinxfleming <c3flemin@gmail.com>
2017-11-05 17:40:56 -0500

    lint accountants controller

--- app/controllers/accountants_controller.rb
+++ app/controllers/accountants_controller.rb
@@ -3,16 +3,18 @@ class AccountantsController < ApplicationController
   before_action :confirm_admin_user
 
   def index
-    @patients = Patient.where('fulfillment.fulfilled' => true)
-    @patients = Kaminari.paginate_array(@patients).page(params[:page]).per(25)
+    fulfilled_pts = Patient.where('fulfillment.fulfilled' => true)
+    @patients = Kaminari.paginate_array(fulfilled_pts)
+                        .page(params[:page]).per(25)
   end
 
   def search
-    unless params[:search] == ''
-      @results = Patient.where('fulfillment.fulfilled' => true).search(params[:search])
-    else
-      @results = Patient.where('fulfillment.fulfilled' => true)
-    end
+    @results = if params[:search] != ''
+                 Patient.where('fulfillment.fulfilled' => true)
+                        .search(params[:search])
+               else
+                 Patient.where('fulfillment.fulfilled' => true)
+               end
     respond_to { |format| format.js }
   end
 
