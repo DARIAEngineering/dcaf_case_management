8984c9916a522e65b0fb8a2894c677d915ec6fef
colinxfleming <c3flemin@gmail.com>
2017-11-28 23:16:39 -0500

    move filter x clinics tests

--- test/system/filter_naf_clinics_test.rb
+++ test/system/filter_naf_clinics_test.rb
@@ -18,7 +18,7 @@ class FilterNafClinicsTest < ApplicationSystemTestCase
                                                              @nonnaf_clinic.name]
 
       check 'naf_filter'
-      sleep 1
+      wait_for_ajax
       options_with_filter = find('#patient_clinic_id').all('option')
                                                       .map { |opt| { name: opt.text, disabled: opt['disabled'] } }
 
