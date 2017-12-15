f339550c0bb0020d07d4039a5ac23b9dc22d9e92
Michael Dworken <mdworken@gmail.com>
2017-12-15 14:45:21 +0000

    fixed move to controller

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -59,7 +59,7 @@ class PatientsController < ApplicationController
 
   def update
     @patient.update_attributes params[:pledge_sent]
-    if @patient.pledge_sent and @patient.pledge_sent_at.nil?
+    if @patient.pledge_sent and !@patient.pledge_sent_at
       @patient.pledge_sent_at = Time.zone.now
       @patient.pledge_sent_by = current_user
     elsif !@patient.pledge_sent 
