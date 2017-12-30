b52651053322a96b33e16661695995b44699b832
Michael Dworken <mdworken@gmail.com>
2017-12-30 17:36:04 +0000

    fix patients controller test coverage

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -116,7 +116,7 @@ class PatientsController < ApplicationController
                   :procedure_cost, :check_number, :date_of_check]
   ].freeze
 
-  OTHER_PARAMS = [:urgent_flag, :initial_call_date, :pledge_sent, :last_edited_by].freeze
+  OTHER_PARAMS = [:urgent_flag, :initial_call_date, :pledge_sent].freeze
 
   def patient_params
     params.require(:patient).permit(
