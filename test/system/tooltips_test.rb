a89b350a964e4a71e75c3e79dff401af54a92196
colinxfleming <c3flemin@gmail.com>
2018-02-06 01:24:36 -0500

    well, I'm out of ideas

--- test/system/tooltips_test.rb
+++ test/system/tooltips_test.rb
@@ -8,31 +8,32 @@ class TooltipsTest < ApplicationSystemTestCase
     log_in_as @user
   end
 
-  # describe 'an inline tooltip' do
-  #   # Using call list as an example of this
-  #   it 'should pop up with content' do
-  #     visit authenticated_root_path
+  describe 'an inline tooltip' do
+    # Using call list as an example of this
+    it 'should pop up with content' do
+      visit authenticated_root_path
 
-  #     within :css, '#call_list' do
-  #       find('.daria-tooltip').hover
-  #       assert has_content? 'This sortable list is used'
-  #     end
-  #   end
-  # end
+      within :css, '#call_list' do
+        find('.daria-tooltip').hover
+        assert has_content? 'This sortable list is used'
+      end
+    end
+  end
 
-  # describe 'a checkbox label tooltip' do
-  #   # Using resolved without assistance as an example of this
-  #   it 'should pop up with content' do
-  #     visit edit_patient_path(@patient)
-  #     click_link 'Abortion Information'
+  describe 'a checkbox label tooltip' do
+    # Using resolved without assistance as an example of this
+    it 'should pop up with content' do
+      visit edit_patient_path(@patient)
+      click_link 'Abortion Information'
 
-  #     within :css, '.tooltip-header-checkbox[for=patient_resolved_without_fund]' do
-  #       find('.tooltip-header-help').hover
-  #       assert has_content? 'This is used to indicate that'
-  #     end
-  #   end
-  # end
+      within :css, '.tooltip-header-checkbox[for=patient_resolved_without_fund]' do
+        find('.tooltip-header-help').hover
+        assert has_content? 'This is used to indicate that'
+      end
+    end
+  end
 
+  # Failing test. For some reason the data attributes don't show up?
   describe 'an input label tooltip' do
     # Using fund pledge as an example of this
     it 'should pop up with content' do
@@ -41,7 +42,6 @@ class TooltipsTest < ApplicationSystemTestCase
 
       within :css, '.tooltip-header-input[for=patient_fund_pledge]' do
         find('.tooltip-header-help').hover
-        sleep 3
         assert has_content? 'Pledge Limit Guidelines'
       end
     end
