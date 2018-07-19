1190261b4331a5faa445260390d41cc2b8104d46
Ting Lew <tingaloo@MBP-2016.home>
2018-07-18 20:10:47 -0400

    add kaminari paginating by 25 items to call list.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,9 +3,21 @@ class EventsController < ApplicationController
   include LinesHelper
 
   def index
-    @events = Event.where(line: current_line)
+    events = Event.where(line: current_line)
                    .order_by(created_at: :desc)
 
-    render partial: 'events/events'
+    @events = paginate_results(events)
+    respond_to do |format|
+      format.html { render partial: 'events/events' }
+      format.js { render :layout => false }
+    end
+  end
+
+  private
+
+  def paginate_results(results)
+    Kaminari.paginate_array(results)
+            .page(params[:page])
+            .per(25)
   end
 end
