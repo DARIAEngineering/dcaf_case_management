e9336cdba87782f33467263779724c503ec4b4b5
Jeffry Hesse <jhesse@sonatype.com>
2018-11-20 17:19:28 -0900

    Put this in a helper

--- app/helpers/navbar_helper.rb
+++ app/helpers/navbar_helper.rb
@@ -6,4 +6,14 @@ module NavbarHelper
       link_to t('navigation.cm_resources.label'), url, target: '_blank'
     end if url.present?
   end
+
+  def spanish_or_english_link
+    content_tag :li do
+      if I18n.locale == I18n.default_locale 
+        link_to "Español", { :locale=>'es' }
+      else
+        link_to "English", { :locale=>'en' }
+      end
+    end
+  end
 end
