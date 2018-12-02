c96c2aec80e61a914a0625b14b4b903f93d5612e
colinxfleming <c3flemin@gmail.com>
2018-12-02 10:22:34 -0500

    indicate what a zip code of 99999 means to clinicfinder in ux and controller comment

--- app/controllers/clinicfinders_controller.rb
+++ app/controllers/clinicfinders_controller.rb
@@ -4,8 +4,13 @@ class ClinicfindersController < ApplicationController
   def search
     return head :bad_request if params[:zip].blank?
 
+    # Clinics intentionally excluded from ClinicFinder are assigned the zip 99999.
+    # e.g. so a fund can have an 'OTHER CLINIC' catchall.
+    excluded_zip = '99999'
+
+    # Get all clinics except those with invalid zip codes.
     clinic_finder = ClinicFinder::Locator.new(
-      Clinic.where(:zip.nin => [nil, '', '99999']),
+      Clinic.where(:zip.nin => [nil, '', excluded_zip]),
       gestational_age: (params[:gestation].to_i || 0),
       naf_only: params[:naf_only] == '1',
       medicaid_only: params[:medicaid_only] == '1'
