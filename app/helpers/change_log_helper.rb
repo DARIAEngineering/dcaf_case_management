e5379836f9011f6eadc7820251fb001e598adb23
colinxfleming <c3flemin@gmail.com>
2019-01-27 01:48:40 -0500

    make entries line up by being clearer about what nil is

--- app/helpers/change_log_helper.rb
+++ app/helpers/change_log_helper.rb
@@ -3,4 +3,9 @@ module ChangeLogHelper
   def safe_join_fields(fields)
     safe_join fields, tag('br')
   end
+
+  def change_or_nil(field)
+    return field if field.present?
+    '(empty)'
+  end
 end
