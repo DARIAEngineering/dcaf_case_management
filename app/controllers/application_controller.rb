bbb99de4c69a3ce5e6d3484a76e29e51132609a2
Jeffry Hesse <jhesse@sonatype.com>
2019-02-10 15:48:27 -0900

    Switch to Spanish

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,10 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
