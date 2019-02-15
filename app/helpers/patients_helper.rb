5d73d85c855daf07b5c2d1af1bdcb7eb5fdcd018
Kathryn Tipton <kathryn.tipton@gmail.com>
2019-02-14 19:40:56 -0500

    Patient info helper translating

--- app/helpers/patients_helper.rb
+++ app/helpers/patients_helper.rb
@@ -10,54 +10,79 @@ module PatientsHelper
 
   def race_ethnicity_options
     [ [ nil ],
-     [ 'White/Caucasian', t('patient.helper.race.white_caucasian') ],
-     [ 'Black/African-American',  t('patient.helper.race.black_afican_american') ],
-     [ 'Hispanic/Latino', t('patient.helper.race.hispanic_latino')],
-     [ 'Asian or South Asian', t('patient.helper.race.asian_south_asian')],
-     [ 'Native Hawaiian or Pacific Islander', t('patient.helper.race.native_hawaiian_pacific_islander')],
-     [ 'Native American', t('patient.helper.race.native_american')],
-     [ 'Mixed Race/Ethnicity', t('patient.helper.race.mixed_race_ethnicity')],
-     [ 'Other', t('patient.helper.race.other')]]
+      [ t('patient.helper.race.white_caucasian'),                  'White/Caucasian' ],
+      [ t('patient.helper.race.black_afican_american'),            'Black/African-American' ],
+      [ t('patient.helper.race.hispanic_latino'),                  'Hispanic/Latino'],
+      [ t('patient.helper.race.asian_south_asian'),                'Asian or South Asian'],
+      [ t('patient.helper.race.native_hawaiian_pacific_islander'), 'Native Hawaiian or Pacific Islander'],
+      [ t('patient.helper.race.native_american'),                  'Native American'],
+      [ t('patient.helper.race.mixed_race_ethnicity'),             'Mixed Race/Ethnicity'],
+      [ t('patient.helper.race.other'),                            'Other' ]
+    ]
   end
 
+  # TODO how to i18n the Config?
   def language_options
-    standard_options = [['English', nil]]
+    standard_options = [ nil, [t('patient.helper.language.English'),'English']]
     standard_options + Config.find_or_create_by(config_key: 'language').options
   end
 
   def referred_by_options
-    standard_options = [nil, 'Clinic', 'Crime victim advocacy center',
-     "#{FUND} website or social media",
-     'Domestic violence crisis/intervention org', 'Family member', 'Friend',
-     'Google/Web search', 'Homeless shelter', 'Legal clinic', 'NAF', 'NNAF',
-     'Other abortion fund', 'Previous patient', 'School',
-     'Sexual assault crisis org', 'Youth outreach']
+    standard_options = [
+      nil,
+      [ t('patient.helper.refer.clinic'),                 'Clinic' ],
+      [ t('patient.helper.refer.cvac'),                   'Crime victim advocacy center' ],
+      [ t('patient.helper.refer.fund', fund: "#{FUND}"),  "#{FUND} website or social media" ],
+      [ t('patient.helper.refer.dv_org'),                 'Domestic violence crisis/intervention org' ],
+      [ t('patient.helper.refer.family'),                 'Family member' ],
+      [ t('patient.helper.refer.friend'),                 'Friend' ],
+      [ t('patient.helper.refer.search'),                 'Google/Web search' ],
+      [ t('patient.helper.refer.homeless'),               'Homeless shelter' ],
+      [ t('patient.helper.refer.legal'),                  'Legal clinic' ],
+      [ t('patient.helper.refer.naf'),                    'NAF' ],
+      [ t('patient.helper.refer.nnaf'),                   'NNAF' ],
+      [ t('patient.helper.refer.other_fund'),             'Other abortion fund' ],
+      [ t('patient.helper.refer.prev_patient'),           'Previous patient' ],
+      [ t('patient.helper.refer.school'),                 'School' ],
+      [ t('patient.helper.refer.crisis_org'),             'Sexual assault crisis org' ],
+      [ t('patient.helper.refer.youth'),                  'Youth outreach' ], ]
     standard_options + Config.find_or_create_by(config_key: 'referred_by').options
   end
 
   def employment_status_options
-    [nil, 'Full-time', 'Part-time', 'Unemployed', 'Odd jobs', 'Student']
+    [
+      nil,
+      [ t('patient.helper.employ.full'), 'Full-time'],
+      [ t('patient.helper.employ.part'), 'Part-time'],
+      [ t('patient.helper.employ.unemp'), 'Unemployed'],
+      [ t('patient.helper.employ.odd'), 'Odd jobs'],
+      [ t('patient.helper.employ.student'), 'Student'],
+    ]
   end
 
   def insurance_options
-    standard_options = ['No insurance', 'Don\'t know', 'Other (add to notes)']
+    standard_options = [
+      [ t('patient.helper.insurance.none'), 'No insurance' ],
+      [ t('patient.helper.insurance.unknown'), 'Don\'t know' ],
+      [ t('patient.helper.insurance.other'), 'Other (add to notes)' ],
+    ]
     [nil] + Config.find_or_create_by(config_key: 'insurance').options + standard_options
   end
 
   def income_options
     [nil,
-     ['Under $9,999 ($192/wk - $833/mo)', 'Under $9,999'],
-     ['$10,000-14,999 ($192-287/wk - $834-1250/mo)', '$10,000-14,999'],
-     ['$15,000-19,999 ($288-384/wk - $1251-1666/mo)', '$15,000-19,999'],
-     ['$20,000-24,999 ($385-480/wk - $1667-2083/mo)', '$20,000-24,999'],
-     ['$25,000-29,999 ($481-576/wk - $2084-2499/mo)', '$25,000-29,999'],
-     ['$30,000-34,999 ($577-672/wk - $2500-2916/mo)', '$30,000-34,999'],
-     ['$35,000-39,999 ($673-768/wk - $2917-3333/mo)', '$35,000-39,999'],
-     ['$40,000-44,999 ($769-864/wk - $3334-3749/mo)', '$40,000-44,999'],
-     ['$45,000-49,999 ($865-961/wk - $3750-4165/mo)', '$45,000-49,999'],
-     ['$50,000-59,999 ($962-1153/wk - $4166-4999/mo)', '$50,000-59,999'],
-     ['$60,000-74,999 ($1154-1441/wk - $5000-6249/mo)', '$60,000-74,999'],
-     ['$75,000 or more ($1442+ /wk - $6250+ /mo)', '$75,000 or more']
+     [ t('patient.helper.income.under_10'), 'Under $9,999'],
+     [ t('patient.helper.income.10_to_15'), '$10,000-14,999'],
+     [ t('patient.helper.income.15_to_20'), '$15,000-19,999'],
+     [ t('patient.helper.income.20_to_25'), '$20,000-24,999'],
+     [ t('patient.helper.income.25_to_30'), '$25,000-29,999'],
+     [ t('patient.helper.income.30_to_35'), '$30,000-34,999'],
+     [ t('patient.helper.income.35_to_40'), '$35,000-39,999'],
+     [ t('patient.helper.income.40_to_45'), '$40,000-44,999'],
+     [ t('patient.helper.income.45_to_50'), '$45,000-49,999'],
+     [ t('patient.helper.income.50_to_60'), '$50,000-59,999'],
+     [ t('patient.helper.income.60_to_75'), '$60,000-74,999'],
+     [ t('patient.helper.income.75_plus'), '$75,000 or more']
     ]
   end
 
