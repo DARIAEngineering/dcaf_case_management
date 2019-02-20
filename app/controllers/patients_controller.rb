db9d46717029a35cf29a9c621ae5aa516d91befa
Jeffry Hesse <jhesse@sonatype.com>
2019-02-20 14:25:37 -0900

    Ok

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -21,7 +21,7 @@ class PatientsController < ApplicationController
     if patient.save
       flash[:notice] = t('flash.new_patient_save')
     else
-      flash[:alert] = patient.errors.full_messages.to_sentence
+      flash[:alert] = t('flash.new_patient_error', error: patient.errors.full_messages.to_sentence)
     end
 
     current_user.add_patient patient
