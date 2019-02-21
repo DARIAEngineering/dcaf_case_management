da08f8eeb62dda64784f01a6a6b16b388cba9a76
Jeffry Hesse <jhesse@sonatype.com>
2019-02-21 09:26:38 -0900

    Woops

--- test/system/event_log_interaction_test.rb
+++ test/system/event_log_interaction_test.rb
@@ -50,13 +50,9 @@ end
 
 def assert_modal_visible
   expect(page).to have_selector(modal_wrapper_id, visible: true)
-rescue Capybara::TimeoutError
-  flunk 'Expected modal to be visible.'
 end
 
 def assert_modal_closed
   expect(page).to have_selector(modal_wrapper_id, visible: false)
-rescue Capybara::TimeoutError
-  flunk 'Expected modal to be hidden.'
 end
 
