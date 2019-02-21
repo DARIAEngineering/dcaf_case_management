d227a3ef456237f62a053596c8279f739fa020b0
Jeffry Hesse <jhesse@sonatype.com>
2019-02-21 10:32:25 -0900

    Try

--- test/system/event_log_interaction_test.rb
+++ test/system/event_log_interaction_test.rb
@@ -49,10 +49,10 @@ def modal_wrapper_id
 end
 
 def assert_modal_visible
-  find("//div[@id=\"${modal_wrapper_id}\"]").should be_visible
+  find_by_id(modal_wrapper_id).should be_visible
 end
 
 def assert_modal_closed
-  find("//div[@id=\"${modal_wrapper_id}\"]").should_not be_visible
+  find_by_id(modal_wrapper_id).should_not be_visible
 end
 
