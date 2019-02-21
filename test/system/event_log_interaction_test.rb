4fd8f65d86454b3057ef5e8cab14d0ce392e5956
Jeffry Hesse <jhesse@sonatype.com>
2019-02-21 11:01:27 -0900

    WOOF

--- test/system/event_log_interaction_test.rb
+++ test/system/event_log_interaction_test.rb
@@ -49,10 +49,10 @@ def modal_wrapper_id
 end
 
 def assert_modal_visible
-  expect(page).to have_css(modal_wrapper_id)
+  expect(page).to has_css(modal_wrapper_id)
 end
 
 def assert_modal_closed
-  expect(page).not_to have_css(modal_wrapper_id)
+  expect(page).not_to has_css(modal_wrapper_id)
 end
 
