91cff4449bdf623b4c18caf598c658f1b750859c
colinxfleming <c3flemin@gmail.com>
2019-03-04 21:05:26 -0500

    localize clinicfinder results

--- test/helpers/clinicfinders_helper_test.rb
+++ test/helpers/clinicfinders_helper_test.rb
@@ -28,12 +28,22 @@ class ClinicfindersHelperTest < ActionView::TestCase
 
     it 'should parse gestational_limit specially' do
       @clinic_struct.gestational_limit = 50
-      assert_equal 'Goes to 7w1d',
+      assert_equal '7w1d',
                    parse_clinicfinder_field(@clinic_struct, :gestational_limit)
 
       @clinic_struct.gestational_limit = nil
       assert_equal 'Not specified',
                    parse_clinicfinder_field(@clinic_struct, :gestational_limit)
+    end
+
+    it 'should parse accepts_naf specifically' do
+      @clinic_struct.accepts_naf = false
+      assert_equal 'No',
+                   parse_clinicfinder_field(@clinic_struct, :accepts_naf)
+
+      @clinic_struct.accepts_naf = true
+      assert_equal 'Yes',
+                   parse_clinicfinder_field(@clinic_struct, :accepts_naf)
     end
 
     it 'parses other fields too' do
