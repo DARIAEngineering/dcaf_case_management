87270a4c724c54ba19c5830c0a904e05e988a2a4
Brian Gibson <bggibson@hotmail.com>
2019-05-27 16:57:14 -0400

    Refactors code to include ES6 features

--- app/javascript/src/pledge_calculator.js
+++ app/javascript/src/pledge_calculator.js
@@ -1,4 +1,4 @@
-var updateBalance = function() {
+const updateBalance = () => {
     if ($("#patient_procedure_cost").val()) {
         $(".outstanding-balance-ctn").removeClass('hidden');
         return $("#outstanding-balance").text('$' + calculateRemainder());
@@ -7,19 +7,17 @@ var updateBalance = function() {
     }
 };
 
-var calculateRemainder = function() {
-    var contributions, total;
-    total = valueToNumber($("#patient_procedure_cost").val());
-    contributions =
+const calculateRemainder = () => {
+    const total = valueToNumber($("#patient_procedure_cost").val());
+    const contributions =
         valueToNumber($("#patient_patient_contribution").val()) +
         valueToNumber($("#patient_naf_pledge").val()) +
         valueToNumber($("#patient_fund_pledge").val()) +
         valueToNumber($(".external_pledge_amount").toArray().reduce(function(acc, next) {
-        return acc + valueToNumber($(next).val());
-    }, 0));
+             return acc + valueToNumber($(next).val());
+        }, 0));
     return total - contributions;
 };
 
-var valueToNumber = function(val) {
-    return +val || 0;
-};
+const valueToNumber = val => +val || 0;
+
