6b1d268b1ac6de21155b929f405b0e47b2cdf0f8
colinxfleming <c3flemin@gmail.com>
2019-06-12 22:11:47 -0400

    lint the life out of the js

--- app/assets/javascripts/patients.coffee
+++ app/assets/javascripts/patients.coffee
@@ -44,22 +44,12 @@ $(document).on 'turbolinks:load', ->
   $(document).on "change", ".edit_external_pledge", ->
     $(this).submit()
 
-  balanceFields = '#abortion-information-form input,
-  #external_pledges input,
-  #patient_procedure_cost,
-  #patient_patient_contribution,
-  #patient_naf_pledge,
-  #patient_fund_pledge'
-
-  $(document).on "change", balanceFields, ->
-    updateBalance()
-
-    $(document).on "submit", "#generate-pledge-form form", ->
-    if($("#case_manager_name").val())
-      true
-    else
-      $("#generate-pledge-form .alert").show()
-      false
+  $(document).on "submit", "#generate-pledge-form form", ->
+  if($("#case_manager_name").val())
+    true
+  else
+    $("#generate-pledge-form .alert").show()
+    false
 
   $(document).on "click", "#create-external-pledge", ->
 # timeout to handle mongo updating and rails appending new field
--- app/javascript/src/pledge_calculator.js
+++ app/javascript/src/pledge_calculator.js
@@ -1,23 +1,41 @@
-const updateBalance = () => {
-    if ($("#patient_procedure_cost").val()) {
-        $(".outstanding-balance-ctn").removeClass('hidden');
-        return $("#outstanding-balance").text('$' + calculateRemainder());
-    } else {
-        return $(".outstanding-balance-ctn").addClass('hidden');
-    }
-};
-
-const calculateRemainder = () => {
-    const total = valueToNumber($("#patient_procedure_cost").val());
-    const contributions =
-        valueToNumber($("#patient_patient_contribution").val()) +
-        valueToNumber($("#patient_naf_pledge").val()) +
-        valueToNumber($("#patient_fund_pledge").val()) +
-        valueToNumber($(".external_pledge_amount").toArray().reduce(function(acc, next) {
-             return acc + valueToNumber($(next).val());
-        }, 0));
-    return total - contributions;
-};
-
+// Coerce values into numbers, or zero if not set.
 const valueToNumber = val => +val || 0;
 
+// Calculate remaining cash needed based on difference of
+// procedure costs vs pledge fields.
+const calculateRemainder = () => {
+  const total = valueToNumber($('#patient_procedure_cost').val());
+  const contributions = valueToNumber($('#patient_patient_contribution').val())
+                        + valueToNumber($('#patient_naf_pledge').val())
+                        + valueToNumber($('#patient_fund_pledge').val())
+                        + valueToNumber($('.external_pledge_amount').toArray().reduce((acc, next) => {
+                          return acc + valueToNumber($(next).val());
+                        }, 0));
+  return total - contributions;
+};
+
+// Show or hide amount remaining based on fields.
+const updateBalance = () => {
+  if ($('#patient_procedure_cost').val()) {
+    // Show remainder if patient procedure cost is known.
+    $('.outstanding-balance-ctn').removeClass('hidden');
+    return $('#outstanding-balance').text(`${calculateRemainder()}`);
+  }
+  // Hide if there isn't a procedure cost.
+  return $('.outstanding-balance-ctn').addClass('hidden');
+};
+
+
+// Relevant fields and identifiers to listen for changes on
+const balanceFields = '#abortion-information-form input, '
+                      + '#external_pledges input, '
+                      + '#patient_procedure_cost, '
+                      + '#patient_patient_contribution, '
+                      + '#patient_naf_pledge, '
+                      + '#patient_fund_pledge';
+
+$(document).on('turblinks:load', () => {
+  $(balanceFields).on('change', () => {
+    updateBalance();
+  });
+});
