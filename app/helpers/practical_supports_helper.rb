b1e7693a641aeb01631e5c5ed07a5fa71e34ce37
colinxfleming <c3flemin@gmail.com>
2019-07-13 17:12:53 -0400

    lean on uniqs rather than conditionals

--- app/helpers/practical_supports_helper.rb
+++ app/helpers/practical_supports_helper.rb
@@ -6,17 +6,17 @@ module PracticalSupportsHelper
       [ t('patient.helper.practical_support.lodging'), 'Lodging' ],
       [ t('patient.helper.practical_support.companion'), 'Companion' ],
     ]
-    full_set = [nil] + standard_options + Config.find_or_create_by(config_key: 'practical_support').options
-    if current_value.present? && full_set.map { |opt| opt.is_a?(Array) ? opt[-1] : opt }.exclude?(current_value)
-      full_set.push current_value
-    end
-
+    full_set = [nil] +
+      standard_options +
+      Config.find_or_create_by(config_key: 'practical_support').options +
+      [current_value]
     full_set.uniq
   end
 
   def practical_support_source_options(current_value = nil)
-    full_set = [nil, FUND_FULL] + Config.find_or_create_by(config_key: 'external_pledge_source').options + ['Clinic', 'Other (see notes)']
-    full_set.push current_value if full_set.exclude? current_value
+    full_set = [nil, FUND_FULL] +
+      Config.find_or_create_by(config_key: 'external_pledge_source').options +
+      ['Clinic', 'Other (see notes)', current_value]
     full_set.uniq
   end
 end
