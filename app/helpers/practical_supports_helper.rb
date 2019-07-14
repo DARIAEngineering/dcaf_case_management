75511a43048b96c7ddec889b51d7e75408cc99a9
colinxfleming <c3flemin@gmail.com>
2019-07-14 01:45:34 -0400

    just use the i18n keys and not array pairs

--- app/helpers/practical_supports_helper.rb
+++ app/helpers/practical_supports_helper.rb
@@ -1,10 +1,10 @@
 module PracticalSupportsHelper
   def practical_support_options(current_value = nil)
     standard_options = [
-      [ t('patient.helper.practical_support.travel_to_area'), 'Travel to area' ],
-      [ t('patient.helper.practical_support.travel_inside_area'), 'Travel inside area' ],
-      [ t('patient.helper.practical_support.lodging'), 'Lodging' ],
-      [ t('patient.helper.practical_support.companion'), 'Companion' ],
+      t('patient.helper.practical_support.travel_to_area'),
+      t('patient.helper.practical_support.travel_inside_area'),
+      t('patient.helper.practical_support.lodging'),
+      t('patient.helper.practical_support.companion'),
     ]
     full_set = [nil] +
       standard_options +
