9be21e7ef7614200f15b97cb3de68fb843dad85d
colinxfleming <c3flemin@gmail.com>
2019-07-21 19:53:44 -0400

    adjust helpers to be array-friendly

--- app/helpers/practical_supports_helper.rb
+++ app/helpers/practical_supports_helper.rb
@@ -1,22 +1,39 @@
 module PracticalSupportsHelper
   def practical_support_options(current_value = nil)
-    standard_options = [
-      t('patient.helper.practical_support.travel_to_area'),
-      t('patient.helper.practical_support.travel_inside_area'),
-      t('patient.helper.practical_support.lodging'),
-      t('patient.helper.practical_support.companion'),
-    ]
-    full_set = [nil] +
-      standard_options +
-      Config.find_or_create_by(config_key: 'practical_support').options +
-      ['Other (see notes)', current_value]
-    full_set.uniq
+    options = [
+      nil,
+      [ t('patient.helper.practical_support.travel_to_region'), 'Travel to region' ],
+      [ t('patient.helper.practical_support.travel_inside_region'), 'Travel inside region' ],
+      [ t('patient.helper.practical_support.lodging'), 'Lodging' ],
+      [ t('patient.helper.practical_support.companion'), 'Companion' ],
+    ] + Config.find_or_create_by(config_key: 'practical_support').options +
+      [
+        [ t('patient.helper.practical_support.other'), 'Other (see notes)' ]
+      ]
+
+    options.push(current_value) if current_value_not_present?(options, current_value)
+    options
   end
 
   def practical_support_source_options(current_value = nil)
-    full_set = [nil, FUND_FULL] +
+    options = [nil, FUND_FULL] +
       Config.find_or_create_by(config_key: 'external_pledge_source').options +
-      ['Patient', 'Clinic', 'Other (see notes)', 'Not sure yet (see notes)', current_value]
-    full_set.uniq
+      [
+        [ t('common.patient'), 'Patient' ],
+        [ t('common.clinic'), 'Clinic' ],
+        [ t('patient.helper.practical_support.other'), 'Other (see notes)' ],
+        [ t('patient.helper.practical_support.not_sure_yet'), 'Not sure yet (see notes)' ]
+      ]
+
+    options.push(current_value) if current_value_not_present?(options, current_value)
+    options
+  end
+
+  private
+
+  def current_value_not_present?(options, current_value)
+    # Return true if current value is present in the values of options.
+    return false if current_value.blank?
+    options.map { |option| option.is_a?(Array) ? option[-1] : option }.exclude? current_value
   end
 end
