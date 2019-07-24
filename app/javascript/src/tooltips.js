97c85cc9ae9fd24bd54b862de2ec9801dd08c731
John Karvonen <jpkarvonen@gmail.com>
2019-07-23 21:01:40 -0400

    converted coffescript files to ES6

---
+++ app/javascript/src/tooltips.js
@@ -0,0 +1,32 @@
+// Pertaining to tooltips
+$(document).on('turbolinks:load', function() {
+  // For regular spans
+  $('.daria-tooltip').each(function() {
+    return $(this).tooltip({
+      html: true,
+      placement: 'bottom',
+      title: $(this).data('tooltip-text')
+    });
+  });
+
+  // Some evil magic for things attached to labels with rails-bootstrap-form
+  $('.tooltip-header-input').append(' <span class="tooltip-header-help">(?)</span>');
+  $('.tooltip-header-input').parent('.form-group' ).find('.form-control').each(function() {
+    return $(this).parents( '.form-group' ).find( '.tooltip-header-help' ).tooltip({
+      html: true,
+      placement: 'bottom',
+      title: $(this).data('tooltip-text')
+    });
+  });
+
+  // Some even eviller magic for checkboxes
+  $('.tooltip-header-checkbox').append(' <span class="tooltip-header-help">(?)</span>');
+  return $('.tooltip-header-checkbox').each(function() {
+    const text = $(this).parent().find('input[type=checkbox]').data('tooltip-text');
+    return $(this).parent().find( 'span.tooltip-header-help' ).tooltip({
+      html: true,
+      placement: 'bottom',
+      title: text
+    });
+  });
+});
