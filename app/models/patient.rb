037076b2cfa7c6831330068d82c1b657ce48b0ed
Kathryn Tipton <kathryn.tipton@gmail.com>
2019-10-10 20:17:59 -0400

    Monthly budget option 1.0!
      todo: tests

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -142,8 +142,8 @@ class Patient
   # Methods
   def self.pledged_status_summary(line)
     plucked_attrs = [:fund_pledge, :pledge_sent, :id, :name, :appointment_date, :fund_pledged_at]
-    start_of_period = Config.start_day.downcase == "monthly" ? Time.zone.today.beginning_of_month.in_time_zone
-                                                             : Time.zone.today.beginning_of_week(Config.start_day).in_time_zone
+    start_of_period = Config.start_day.downcase.to_s == "monthly" ? Time.zone.today.beginning_of_month.in_time_zone
+                                                                  : Time.zone.today.beginning_of_week(Config.start_day).in_time_zone
     # Get patients who have been pledged this week, as a simplified hash
     patients = Patient.in(line: line)
                       .where(:fund_pledge.nin => [0, nil, ''])
