07bd91f0db4e8012a5ff79d6d3aa6bdf355131de
kevin wei <kevin.tw.wei@gmail.com>
2019-11-05 20:18:45 -0500

    add uri service utility

--- app/services/uri_service.rb
+++ app/services/uri_service.rb
@@ -1,3 +1,11 @@
-class UriService
+class URIService
+  attr_accessor :uri
 
+  def initialize(uri)
+    @uri = URI.parse(URI.encode(uri))
+  end
+
+  def secure_scheme!
+    @uri.scheme = "https"
+  end
 end
---
+++ test/services/uri_service_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class URIServiceTest < ActiveSupport::TestCase
+  describe 'URI service utility' do
+    it 'instantiates a uri from string' do
+      uri = URIService.new("some yolo test uri").uri
+      assert uri.is_a?(URI)
+    end
+
+    it 'can force the scheme to use https' do
+      u = URIService.new("http://www.someyolourl.com")
+      assert_equal u.uri.scheme, "http"
+      u.secure_scheme!
+      assert_equal u.secure_scheme!, "https"
+    end
+  end
+end
