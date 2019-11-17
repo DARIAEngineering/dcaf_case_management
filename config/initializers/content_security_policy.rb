2fb1ae798384baa7ca70dac375c73bf436f9529b
colinxfleming <c3flemin@gmail.com>
2019-11-16 23:16:26 -0500

    run app update and take the meaningful changes

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,3 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# Define an application-wide content security policy
+# For further information see the following documentation
+# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
+
 Rails.application.config.content_security_policy do |policy|
   policy.default_src :self
   policy.font_src    :self, :https, :data
@@ -7,11 +13,15 @@ Rails.application.config.content_security_policy do |policy|
   policy.connect_src :self
   policy.script_src  :self, :unsafe_eval
   policy.style_src   :self, :unsafe_inline
+
+  # Specify URI for violation reports
   policy.report_uri  "https://#{ENV['CSP_VIOLATION_URI']}/csp/reportOnly"
 
+  # If you are using webpack-dev-server then specify webpack-dev-server host
   if Rails.env.development?
     policy.connect_src :self, "http://localhost:3035", "ws://localhost:3035"
   end
 end
 
+# If you are using UJS then enable automatic nonce generation
 Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
