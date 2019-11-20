928da8ffbbc5059e1933c5b8a5cca196e5dc4277
Rae <rae.rudie@gmail.com>
2019-11-19 21:03:27 -0500

    continuing style webpack integration

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,20 +1,42 @@
 const { environment } = require('@rails/webpacker');
-
 const webpack = require('webpack');
 
 // Add an ProvidePlugin
-environment.plugins.prepend('Provide',  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    jquery: 'jquery',
-  })
-)
+// environment.plugins.prepend('Provide',  new webpack.ProvidePlugin({
+//     $: 'jquery',
+//     jQuery: 'jquery',
+//     jquery: 'jquery',
+//   })
+// )
 
-const config = environment.toWebpackConfig();
+// const config = environment.toWebpackConfig();
 
-config.resolve.alias = {
-  jquery: "jquery/src/jquery",
-}
+// config.resolve.alias = {
+//   jquery: "jquery/src/jquery",
+// }
 
 // export the updated config
-module.exports = environment;
+module.exports = {
+  plugins: [
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        jquery: 'jquery',
+      })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ]
+  }
+}
