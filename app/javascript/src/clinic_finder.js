157ab6d6abd0bf222590a1aac15ba628571d8187
colinxfleming <c3flemin@gmail.com>
2019-11-30 11:16:32 -0500

    favor d-none over hide or hidden, except in multimodal

--- app/javascript/src/clinic_finder.js
+++ app/javascript/src/clinic_finder.js
@@ -1,6 +1,6 @@
 $(document).on('turbolinks:load', () => {
   // Click on the clinic-locator expand to show it.
   $(document).on('click', '.clinic-finder-expand', () => {
-    $('#clinic-finder-search-form').toggleClass('hidden');
+    $('#clinic-finder-search-form').toggleClass('d-none');
   });
 });
