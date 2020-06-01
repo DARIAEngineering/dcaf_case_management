8d94b4456e8adf4d76c79411ac8bbbd15490cab0
colinxfleming <c3flemin@gmail.com>
2020-06-01 02:16:20 -0400

    move over vendored js bootstrap modal steps

--- app/javascript/src/modals.js
+++ app/javascript/src/modals.js
@@ -1,6 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+import './vendor/jquery-bootstrap-modal-steps';
+
 $(document).on('turbolinks:load', () => {
   // select all elements with id ending with "modal"
   $('[id$=modal]').modalSteps();
