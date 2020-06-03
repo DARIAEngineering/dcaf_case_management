d1610dd09aa6d23a7a7bbdc7e79964f13f5677d1
colinxfleming <c3flemin@gmail.com>
2020-06-02 21:58:11 -0400

    refactor tooltips to effectively use es6

--- app/javascript/src/tooltips.js
+++ app/javascript/src/tooltips.js
@@ -1,28 +1,25 @@
 // Pertaining to tooltips
 $(document).on('turbolinks:load', () => {
+  const tooltipSpan = ' <span class="tooltip-header-help">(?)</span>';
   // For regular spans
-  $('.daria-tooltip').each(() => {
-    return $(this).tooltip({
-      html: true,
-      placement: 'bottom',
-      title: $(this).data('tooltip-text'),
-    });
-  });
+  $('.daria-tooltip').tooltip();
 
   // Some evil magic for things attached to labels with rails-bootstrap-form
-  $('.tooltip-header-input').append(' <span class="tooltip-header-help">(?)</span>');
-  $('.tooltip-header-input').parent('.form-group').find('.form-control').each(() => {
-    return $(this).parents('.form-group').find('.tooltip-header-help').tooltip({
+  // $('.tooltip-header-input').append(' <span class="tooltip-header-help">(?)</span>');
+  $('.tooltip-header-input').each(function() {
+    $(this).append(tooltipSpan);
+    const text = $(this).parent().find('input')[0].dataset.tooltipText;
+    return $(this).parent().find('span.tooltip-header-help').tooltip({
       html: true,
       placement: 'bottom',
-      title: $(this).data('tooltip-text'),
+      title: text,
     });
   });
 
   // Some even eviller magic for checkboxes
-  $('.tooltip-header-checkbox').append(' <span class="tooltip-header-help">(?)</span>');
-  return $('.tooltip-header-checkbox').each(() => {
-    const text = $(this).parent().find('input[type=checkbox]').data('tooltip-text');
+  $('.tooltip-header-checkbox').each(function() {
+    $(this).append(tooltipSpan);
+    const text = $(this).parent().find('input[type=checkbox]')[0].dataset.tooltipText;
     return $(this).parent().find('span.tooltip-header-help').tooltip({
       html: true,
       placement: 'bottom',
