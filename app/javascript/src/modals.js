4c17a9430e9e507ef8da8656070f8b6f59b7257c
colinxfleming <c3flemin@gmail.com>
2020-06-07 13:04:07 -0400

    does not fix a display bug but does fix behavior

--- app/javascript/src/modals.js
+++
@@ -1,9 +0,0 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-
-import './vendor/jquery-bootstrap-modal-steps';
-
-$(document).on('turbolinks:load', () => {
-  // select all elements with id ending with "modal"
-  $('[id$=modal]').modalSteps();
-});
