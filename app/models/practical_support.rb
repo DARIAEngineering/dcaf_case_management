1c0fb0bc29f204a0126d3e21ce40b7b73c029ca7
colinxfleming <c3flemin@gmail.com>
2020-08-08 02:34:38 -0400

    working psup tests

--- app/models/practical_support.rb
+++ app/models/practical_support.rb
@@ -2,7 +2,12 @@
 # either by this fund or other stakeholders.
 class PracticalSupport < ApplicationRecord
   has_paper_trail
+  include HistoryTrackable
 
   # Relationships
   belongs_to :can_support, polymorphic: true
+
+  # Validations
+  validates :source, :support_type, presence: true
+  validates :support_type, uniqueness: { scope: :can_support }
 end
--- test/models/practical_support_test.rb
+++ test/models/practical_support_test.rb
@@ -2,40 +2,26 @@ require 'test_helper'
 
 class PracticalSupportTest < ActiveSupport::TestCase
   before do
-    @user = create :user
     @patient = create :patient
-    @patient.practical_supports.create created_by: @user,
-                                support_type: 'Concert Tickets',
-                                source: 'Metallica Abortion Fund'
-    @patient.practical_supports.create created_by: @user,
-                                support_type: 'Swag',
-                                source: 'YOLO AF',
-                                confirmed: true
+    @patient.practical_supports.create support_type: 'Concert Tickets',
+                                       source: 'Metallica Abortion Fund'
+    @patient.practical_supports.create support_type: 'Swag',
+                                       source: 'YOLO AF',
+                                       confirmed: true
     @psupport1 = @patient.practical_supports.first
     @psupport2 = @patient.practical_supports.last
   end
 
-  describe 'mongoid attachments' do
-    it 'should have timestamps from Mongoid::Timestamps' do
-      [:created_at, :updated_at].each do |field|
-        assert @psupport1.respond_to? field
-        assert @psupport1[field]
-      end
-    end
-
+  describe 'attachments' do
     it 'should respond to history methods' do
-      assert @psupport1.respond_to? :history_tracks
-      assert @psupport1.history_tracks.count > 0
-    end
-
-    it 'should have accessible userstamp methods' do
+      assert @psupport1.respond_to? :versions
       assert @psupport1.respond_to? :created_by
-      assert @psupport1.created_by
+      assert @psupport1.respond_to? :created_by_id
     end
   end
 
   describe 'validations' do
-    [:created_by, :source, :support_type].each do |field|
+    [:source, :support_type].each do |field|
       it "should enforce presence of #{field}" do
         @psupport1[field.to_sym] = nil
         refute @psupport1.valid?
@@ -49,8 +35,12 @@ class PracticalSupportTest < ActiveSupport::TestCase
 
       support2 = @patient.practical_supports.new fields
       refute support2.save
-      assert_equal ['is already taken'],
+      assert_equal ['has already been taken'],
                    support2.errors.messages[:support_type]
+
+      pt2 = create :patient
+      support3 = pt2.practical_supports.new fields
+      assert support3.save
     end
   end
 end
