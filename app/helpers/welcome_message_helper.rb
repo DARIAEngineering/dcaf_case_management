95a2b1b34a485ac70647002df0f63acb137f76e5
ARJUN_RAWAL <arjunrawal07@gmail.com>
2020-08-20 19:23:52 -0400

    final welcome message from arjun

--- app/helpers/welcome_message_helper.rb
+++ app/helpers/welcome_message_helper.rb
@@ -48,13 +48,11 @@ module WelcomeMessageHelper
       "Thank you for all that you do!",
       "Not all superheroes wear capes, some are DCAF case managers! <3",
       "Thank you for being awesome!",
-      "You are amazing! 👏👏👏",
       "You are doing wonderful things! Thank you so much for your support and help during these trying times.",
       "We appreciate all that you're doing. Keep up the good work!",
       "Thank you so much. If you're ok with hugs and if I could hug you in real life, I would hug you! Your work is so important.",
       "Proud to be a part of this incredible project - keep up the great work!",
-      "You are a complete and total UNICORN! 🦄✨",
-      "You guys rock! Proud to be working on this project."
+      "Thank you for contributing your time and energy in doing this important work."
     ]
   end
 end
