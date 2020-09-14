17000a744f9823e1a6ca627a6b059850acc8d80b
ARJUN_RAWAL <arjunrawal07@gmail.com>
2020-09-14 11:14:08 -0400

    added zipcode test for data_entry

--- test/system/data_entry_test.rb
+++ test/system/data_entry_test.rb
@@ -27,6 +27,7 @@ class DataEntryTest < ApplicationSystemTestCase
       fill_in 'City', with: 'Washington'
       select 'DC', from: 'patient_state'
       fill_in 'County', with: 'Wash'
+      fill_in 'Zipcode', with: '20009'
       fill_in 'DCAF pledge', with: '100'
       fill_in 'Age', with: '30'
       select 'Other', from: 'patient_race_ethnicity'
@@ -76,6 +77,7 @@ class DataEntryTest < ApplicationSystemTestCase
         assert has_field? 'City', with: 'Washington'
         assert_equal 'DC', find('#patient_state').value
         assert has_field? 'County', with: 'Wash'
+        asser has_field? 'Zipcode', with: '20009'
         assert_equal 'English', find('#patient_language').text
         assert_equal 'no', find('#patient_voicemail_preference').value
         assert_equal 'Student', find('#patient_employment_status').value
