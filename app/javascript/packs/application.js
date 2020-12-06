1f9b50821c7e120d7b72aaba10e63b7e91946997
colinxfleming <c3flemin@gmail.com>
2020-12-06 15:41:19 -0500

    move rails-ujs over to webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+require('@rails/ujs').start()
 import 'bootstrap';
 import '../src/turbolinks';
 
