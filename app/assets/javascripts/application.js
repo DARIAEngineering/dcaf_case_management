25521db3cd378b206ccd4467f41639f3a9a95768
colinxfleming <c3flemin@gmail.com>
2020-12-07 01:53:41 -0500

    remove jquery from sprockets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
 //= require popper
 //= require jquery_ujs
 //= require jquery-ui
