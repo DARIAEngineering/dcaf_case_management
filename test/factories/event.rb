0953c10e407baa2a79e5bbc367ab00960dad1dfd
colinxfleming <c3flemin@gmail.com>
2020-12-12 00:14:29 -0500

    move event model to postgres; deprecate into mongomodel

--- test/factories/event.rb
+++ test/factories/event.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :event do
-    event_type { 'Reached patient' }
+    event_type { :reached_patient }
     cm_name { 'Yolorita' }
     patient_name { 'Susan Everyteen' }
     patient_id { 'sdfghjk' }
