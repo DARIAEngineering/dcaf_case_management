e9b87b5260407937586002f2740c6f1cc9d90e58
colinxfleming <c3flemin@gmail.com>
2021-03-07 15:42:05 -0500

    port clinic related stuff to pg

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,55 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_12_010730) do
+ActiveRecord::Schema.define(version: 2021_03_07_202735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "clinics", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "street_address"
+    t.string "city"
+    t.string "state"
+    t.string "zip"
+    t.string "phone"
+    t.string "fax"
+    t.boolean "active", default: true, null: false
+    t.boolean "accepts_naf"
+    t.boolean "accepts_medicaid"
+    t.integer "gestational_limit"
+    t.decimal "coordinates", array: true
+    t.integer "costs_5wks"
+    t.integer "costs_6wks"
+    t.integer "costs_7wks"
+    t.integer "costs_8wks"
+    t.integer "costs_9wks"
+    t.integer "costs_10wks"
+    t.integer "costs_11wks"
+    t.integer "costs_12wks"
+    t.integer "costs_13wks"
+    t.integer "costs_14wks"
+    t.integer "costs_15wks"
+    t.integer "costs_16wks"
+    t.integer "costs_17wks"
+    t.integer "costs_18wks"
+    t.integer "costs_19wks"
+    t.integer "costs_20wks"
+    t.integer "costs_21wks"
+    t.integer "costs_22wks"
+    t.integer "costs_23wks"
+    t.integer "costs_24wks"
+    t.integer "costs_25wks"
+    t.integer "costs_26wks"
+    t.integer "costs_27wks"
+    t.integer "costs_28wks"
+    t.integer "costs_29wks"
+    t.integer "costs_30wks"
+    t.string "mongo_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "configs", force: :cascade do |t|
     t.integer "config_key", null: false
