25e1e3c793b559e2250fcc3f399fa17937b88c22
colinxfleming <c3flemin@gmail.com>
2021-03-23 22:49:15 -0400

    adjust test to properly handle versioning

--- test/models/paper_trail_version_test.rb
+++ test/models/paper_trail_version_test.rb
@@ -72,9 +72,11 @@ class PaperTrailVersionTest < ActiveSupport::TestCase
     end
 
     it "should attach versions to config" do
-      assert @config.versions.count == 1
+      assert_equal 1, @config.versions.count
       assert_difference '@config.versions.count', 1 do
-        @config.update config_value: { options: 'Metallica' }
+        with_versioning do
+          @config.update config_value: { options: 'Metallica' }
+        end
       end
     end
   end
