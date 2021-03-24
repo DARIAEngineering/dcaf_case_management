e41375337104617d6143412922628f963c6ddc65
colinxfleming <c3flemin@gmail.com>
2021-03-23 22:23:37 -0400

    add the option of papertrail for non-ephemeral objects

--- app/models/concerns/history_trackable.rb
+++ app/models/concerns/history_trackable.rb
@@ -3,10 +3,20 @@ module HistoryTrackable
   extend ActiveSupport::Concern
 
   def assemble_audit_trails
-    history_tracks.includes(:created_by).sort_by(&:created_at).reverse
+    # This is only used in one place and should be killable after we port Patient over
+    if try(:superclass) == ApplicationRecord
+      versions
+    else
+      history_tracks.includes(:created_by).sort_by(&:created_at).reverse
+    end
   end
 
   def recent_history_tracks
-    history_tracks.select { |ht| ht.updated_at > 6.days.ago }
+    if try(:superclass) == ApplicationRecord
+      versions.where(updated_at: 6.days.ago..)
+    else
+      # Mongo compatibility
+      history_tracks.select { |ht| ht.updated_at > 6.days.ago }
+    end
   end
 end
