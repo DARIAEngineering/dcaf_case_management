82db37b5ef5cfcb69dc481fd764fced18dd5f58b
colinxfleming <c3flemin@gmail.com>
2021-05-02 01:10:55 -0400

    port over call list items

--- app/models/call_list_entry.rb
+++ app/models/call_list_entry.rb
@@ -1,25 +1,10 @@
 # Object representing a patient's call list.
-class CallListEntry
-  include Mongoid::Document
-  include Mongoid::Timestamps
-
+class CallListEntry < ApplicationRecord
   # Relationships
   belongs_to :user
   belongs_to :patient
 
-  # Fields
-  field :order_key, type: Integer
-  field :line, type: String
-
   # Validations
   validates :order_key, :line, presence: true
   validates :patient, uniqueness: { scope: :user }
-
-  # Indices
-  index line: 1
-
-  def self.destroy_orphaned_entries
-    includes(:patient).reject(&:patient)
-                      .each { |x| x.destroy }
-  end
 end
