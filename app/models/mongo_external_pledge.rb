357b0a390afb8536ca1c8afb8f1d2262a68f3cef
Colin <c3flemin@gmail.com>
2021-05-15 01:23:24 -0400

    port over external pledge to pg (#2199)

---
+++ app/models/mongo_external_pledge.rb
@@ -0,0 +1,31 @@
+# Object representing money from organizations that aren't the fund or NAF.
+# For primary fund pledges or NAF pledges, see the patient model.
+class MongoExternalPledge
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  include Mongoid::History::Trackable
+  include Mongoid::Userstamp
+
+  # Relationships
+  embedded_in :can_pledge, polymorphic: true
+
+  default_scope -> { where(active: true) }
+  scope :active, -> { where(active: true) }
+
+  # Fields
+  field :source, type: String # Name of outside organization or fund
+  field :amount, type: Integer
+  field :active, type: Boolean, default: true
+
+  # Validations
+  validates :created_by_id, :source, :amount, presence: true
+  validates :source, uniqueness: { scope: :active }
+
+  # History and auditing
+  track_history on: fields.keys + [:updated_by_id],
+                version_field: :version,
+                track_create: true,
+                track_update: true,
+                track_destroy: true
+  mongoid_userstamp user_model: 'User'
+end
