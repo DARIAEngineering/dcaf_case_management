c10fd63f7561ed4b144354a58f9f48ac683adf13
Eli Baum <elimbaum@gmail.com>
2021-06-02 10:34:52 -0400

    remove test accidentally carried over from other branch

--- test/system/accountant_workflow_test.rb
+++ test/system/accountant_workflow_test.rb
@@ -4,7 +4,6 @@ class AccountantWorkflowTest < ApplicationSystemTestCase
   before do
     @user = create :user, role: :admin
     @clinic = create :clinic, name: 'a real clinic'
-    @another_clinic = create :clinic, name: 'alternative clinic'
 
     @nonpledged_patient = create :patient, name: 'Eileene Zarha',
                                            initial_call_date: 5.weeks.ago
@@ -30,13 +29,6 @@ class AccountantWorkflowTest < ApplicationSystemTestCase
                                           appointment_date: 2.weeks.ago,
                                           fulfillment: @fulfillment_data,
                                           initial_call_date: 5.weeks.ago
-
-    @other_clinic_patient = create :patient, name: 'Marina Zarha',
-                                             clinic: @another_clinic,
-                                             pledge_sent: true,
-                                             fund_pledge: 123,
-                                             appointment_date: 2.weeks.ago,
-                                             initial_call_date: 5.weeks.ago
 
     log_in_as @user
     visit accountants_path
@@ -80,31 +72,6 @@ class AccountantWorkflowTest < ApplicationSystemTestCase
       wait_for_ajax
 
       fill_in 'Search', with: ''
-      click_button 'Search'
-      wait_for_ajax
-
-      assert has_content? @fulfilled_patient.name
-      assert has_content? @pledged_patient.name
-      assert has_content? @other_clinic_patient.name
-      refute has_content? @nonpledged_patient.name
-    end
-
-    it 'should search by clinic' do
-      fill_in 'Search', with: ''
-      select 'alternative clinic', from: 'clinic_id'
-      click_button 'Search'
-      wait_for_ajax
-
-      assert has_content? @other_clinic_patient.name
-
-      refute has_content? @fulfilled_patient.name
-      refute has_content? @pledged_patient.name
-      refute has_content? @nonpledged_patient.name
-    end
-
-    it 'should display everyone on search for all clinics' do
-      fill_in 'Search', with: ''
-      select '[All Clinics]', from: 'clinic_id'
       click_button 'Search'
       wait_for_ajax
 
