2745226df7accffb797958ae3ec13787ac0902cd
colinxfleming <c3flemin@gmail.com>
2021-06-13 02:35:17 -0400

    apparently this has been deprecated since rails 5

--- config/application.rb
+++ config/application.rb
@@ -48,8 +48,5 @@ module DcafCaseManagement
 
     # Throttling protection
     config.middleware.use Rack::Attack
-
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
   end
 end
