93d8334e0ce578c88c4d52add3f9c350ddc5fcf9
colinxfleming <c3flemin@gmail.com>
2021-07-11 16:44:13 -0400

    clean up now-deprecated code, and get clinic select to properly populate based on url params

--- app/controllers/accountants_controller.rb
+++ app/controllers/accountants_controller.rb
@@ -5,19 +5,16 @@ class AccountantsController < ApplicationController
   before_action :find_patient, only: [:edit]
 
   def index
-    @patients = pledged_base.page(params[:page] || 1)
+    @patients = Patient.where(pledge_sent: true)
+                       .includes(:clinic)
+                       .includes(:fulfillment)
+                       .order(pledge_sent_at: :desc)
+                       .page(params[:page] || 1)
+
     @patients = @patients.where(clinic_id: params[:clinic_id]) if params[:clinic_id].present?
     @patients = @patients.search(params[:search]) if params[:search].present?
-    @patients = @patients.page(params[:page] || 1)
+    @patients
   end
-
-  # def search
-  #   @patients = pledged_base
-
-  #   # @patients = paginate_results results
-
-  #   respond_to { |format| format.js }
-  # end
 
   def edit
     # This is a cheater method that populates the fulfillment partial into a
@@ -26,13 +23,6 @@ class AccountantsController < ApplicationController
   end
 
   private
-
-  def pledged_base
-    Patient.where(pledge_sent: true)
-           .includes(:clinic)
-           .includes(:fulfillment)
-           .order(pledge_sent_at: :desc)
-  end
 
   def find_patient
     @patient = Patient.find params[:id]
