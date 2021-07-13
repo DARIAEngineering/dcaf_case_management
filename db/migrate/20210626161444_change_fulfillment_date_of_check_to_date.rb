424717fe8f42b7a0a3e084a76f209420c9f8b2cf
colinxfleming <c3flemin@gmail.com>
2021-07-13 01:23:06 -0400

    use a converter with null handling

--- db/migrate/20210626161444_change_fulfillment_date_of_check_to_date.rb
+++ db/migrate/20210626161444_change_fulfillment_date_of_check_to_date.rb
@@ -1,6 +1,8 @@
 class ChangeFulfillmentDateOfCheckToDate < ActiveRecord::Migration[6.1]
   def up
-    change_column :fulfillments, :date_of_check, 'date USING date_of_check::date'
+    change_column :fulfillments,
+                  :date_of_check,
+                  "date USING case when date_of_check = '' then null else date_of_check::date end"
   end
 
   def down
