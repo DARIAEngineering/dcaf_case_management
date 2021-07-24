1654b6c984324b23e8ffbf8bb538db1f90d1e6f6
colinxfleming <c3flemin@gmail.com>
2021-07-24 13:32:18 -0400

    tfw you forget how method chaining actuall works

--- app/models/concerns/patient_searchable.rb
+++ app/models/concerns/patient_searchable.rb
@@ -20,9 +20,8 @@ module PatientSearchable
         matches = matches.or(base.where('primary_phone like ?', clean_phone))
                          .or(base.where('other_phone like ?', clean_phone))
       end
-
-      matches.order(updated_at: :desc)
-      matches.limit(search_limit) if search_limit.present?
+      matches = matches.order(updated_at: :desc)
+      matches = matches.limit(search_limit) if search_limit.present?
       matches
     end
   end
