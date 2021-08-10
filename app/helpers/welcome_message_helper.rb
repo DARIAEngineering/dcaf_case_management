9786568dfa02b4f7763dfe0828a73939a646bb36
rkaufman13 <16939591+rkaufman13@users.noreply.github.com>
2021-08-10 15:17:53 -0400

    For real this time (#2333)
    * add welcome message
    
    * now with comma!

--- app/helpers/welcome_message_helper.rb
+++ app/helpers/welcome_message_helper.rb
@@ -64,7 +64,8 @@ module WelcomeMessageHelper
       "Thank you for the critical, wonderful work you do. You're awesome!",
       "Keep on crushing it! You are awesome.",
       "Thank you! You ARE APPRECIATED! :)",
-      "Thank you for your incredible work!"
+      "Thank you for your incredible work!",
+      "My body, my choice. Thanks for fighting for reproductive justice!"
     ]
   end
 end
