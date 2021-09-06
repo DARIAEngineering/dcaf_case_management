bcea5d50ab5608c19182ee39b3e595c5f887808f
Larry Person <lp10011@gmail.com>
2021-09-06 14:09:38 -0400

    Nice message

--- app/helpers/welcome_message_helper.rb
+++ app/helpers/welcome_message_helper.rb
@@ -65,7 +65,8 @@ module WelcomeMessageHelper
       "Keep on crushing it! You are awesome.",
       "Thank you! You ARE APPRECIATED! :)",
       "Thank you for your incredible work!",
-      "My body, my choice. Thanks for fighting for reproductive justice!"
+      "My body, my choice. Thanks for fighting for reproductive justice!",
+      "Whoever saves a single life is considered by scripture to have saved the whole world. Thank you for your work!"
     ]
   end
 end
