a4ecd528eb77de6bd7609ab9a65595fa3b4a5e72
colinxfleming <c3flemin@gmail.com>
2021-09-28 22:18:44 -0400

    add tests for fund and proper scoping

--- app/models/fund.rb
+++ app/models/fund.rb
@@ -1,2 +1,13 @@
 class Fund < ApplicationRecord
+  # Concerns
+  # include PaperTrailable
+
+  # Validations
+  validates :name,
+            :subdomain,
+            :domain,
+            :full_name,
+            :site_domain,
+            :phone,
+            presence: true
 end
--- test/factories/fund.rb
+++ test/factories/fund.rb
@@ -3,8 +3,16 @@ FactoryBot.define do
     sequence :name do |n|
       "Fund #{n}"
     end
+    sequence :subdomain do |n|
+      "fund#{n}"
+    end
+    domain { 'google' }
     sequence :full_name do |n|
       "Fund #{n} of Cat Town"
     end
+    sequence :site_domain do |n|
+      "www.fund#{n}.pizza"
+    end
+    phone { '(939)-555-0113' }
   end
 end
