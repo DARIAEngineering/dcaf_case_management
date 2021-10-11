4797f40356e3cea8820316563d4f219283469603
colinxfleming <c3flemin@gmail.com>
2021-10-11 18:48:17 -0400

    fix errant name -> full_name, thx lomky

--- test/integration_helper.rb
+++ test/integration_helper.rb
@@ -71,7 +71,7 @@ module IntegrationHelper
   end
 
   def go_to_dashboard
-    click_on "DARIA - #{ActsAsTenant.current_tenant.name}"
+    click_on "DARIA - #{ActsAsTenant.current_tenant.full_name}"
   end
 
   def click_away_from_field
