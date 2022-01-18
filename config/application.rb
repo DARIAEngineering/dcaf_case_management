83dbfa107de15b13b7f7f9c89634418f48157e63
colinxfleming <c3flemin@gmail.com>
2022-01-17 22:26:36 -0500

    run thru rails app:update

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,6 @@ require "action_view/railtie"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
-
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -28,7 +26,7 @@ Bundler.require(*Rails.groups)
 module DcafCaseManagement
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 7.0
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -2,6 +2,9 @@
 
 # Configure sensitive parameters which will be filtered from the log file.
 Rails.application.config.filter_parameters += [
+  :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn,
+
+  # Custom
   :password,
   :email,
   :name,
