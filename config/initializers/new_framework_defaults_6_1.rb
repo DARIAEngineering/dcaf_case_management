7ccf7e253d00ead6df80419d23dd7e6bbba23aec
colinxfleming <c3flemin@gmail.com>
2022-01-18 21:30:41 -0500

    skip activestorage and actionmailbox

--- config/initializers/new_framework_defaults_6_1.rb
+++ config/initializers/new_framework_defaults_6_1.rb
@@ -10,10 +10,10 @@
 Rails.application.config.active_record.has_many_inversing = true
 
 # Track Active Storage variants in the database.
-Rails.application.config.active_storage.track_variants = true
+# Rails.application.config.active_storage.track_variants = true
 
 # Apply random variation to the delay when retrying failed jobs.
-Rails.application.config.active_job.retry_jitter = 0.15
+# Rails.application.config.active_job.retry_jitter = 0.15
 
 # Stop executing `after_enqueue`/`after_perform` callbacks if
 # `before_enqueue`/`before_perform` respectively halts with `throw :abort`.
@@ -48,16 +48,16 @@ Rails.application.config.active_record.legacy_connection_handling = false
 Rails.application.config.action_view.form_with_generates_remote_forms = false
 
 # Set the default queue name for the analysis job to the queue adapter default.
-Rails.application.config.active_storage.queues.analysis = nil
+# Rails.application.config.active_storage.queues.analysis = nil
 
 # Set the default queue name for the purge job to the queue adapter default.
-Rails.application.config.active_storage.queues.purge = nil
+# Rails.application.config.active_storage.queues.purge = nil
 
 # Set the default queue name for the incineration job to the queue adapter default.
-Rails.application.config.action_mailbox.queues.incineration = nil
+# Rails.application.config.action_mailbox.queues.incineration = nil
 
 # Set the default queue name for the routing job to the queue adapter default.
-Rails.application.config.action_mailbox.queues.routing = nil
+# Rails.application.config.action_mailbox.queues.routing = nil
 
 # Set the default queue name for the mail deliver job to the queue adapter default.
 Rails.application.config.action_mailer.deliver_later_queue_name = nil
