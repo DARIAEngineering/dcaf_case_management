e35f041b8a03c9424d08d494e170dbf9ea186de9
colinxfleming <c3flemin@gmail.com>
2022-01-18 21:04:01 -0500

    ratchet back framework defaults. guess we never did this eh

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ Bundler.require(*Rails.groups)
 module DcafCaseManagement
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 6.0
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
