8e56b74b84b70a3690dbed771dbcca809eba66bb
colinxfleming <c3flemin@gmail.com>
2022-02-21 01:33:37 -0500

    no secure?

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :active_record_store, key: '_my_app_session', secure: Rails.env.production?
+Rails.application.config.session_store :active_record_store, key: '_my_app_session'#, secure: Rails.env.production?
