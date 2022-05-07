3b18d23b8e488557fd76c4e36c2d72f1a71c66f6
Eli Baum <elimbaum@gmail.com>
2022-05-07 12:57:44 -0400

    actually get the class name right

--- db/migrate/20220506220133_urgent_flagged.rb
+++ db/migrate/20220506220133_urgent_flagged.rb
@@ -1,4 +1,4 @@
-class ChangePracticalSupportAmountToDecimal < ActiveRecord::Migration[6.1]
+class ChangeUrgentToFlagged < ActiveRecord::Migration[6.1]
     def change
       rename_column :patients, :urgent_flag, :flagged
     end
