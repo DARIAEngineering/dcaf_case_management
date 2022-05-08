afcc0de83259ea6e5331ae1ba02d8ea794f171b3
Eli Baum <elimbaum@gmail.com>
2022-05-08 14:45:05 -0400

    i18n and use rails g

--- db/migrate/20220508184141_rename_urgent_flag_to_flagged.rb
+++ db/migrate/20220508184141_rename_urgent_flag_to_flagged.rb
@@ -1,7 +1,6 @@
-class ChangeUrgentToFlagged < ActiveRecord::Migration[6.1]
+class RenameUrgentFlagToFlagged < ActiveRecord::Migration[6.1]
   def change
     rename_column :patients, :urgent_flag, :flagged
     rename_column :archived_patients, :urgent_flag, :flagged
   end
 end
-  
