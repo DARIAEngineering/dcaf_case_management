087c106b71332a788b389dd8d15aae4854552a47
colinxfleming <c3flemin@gmail.com>
2022-05-28 01:04:58 -0400

    get build working - sprockets called out as such. doesn't load jquery in esbuild yet though

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,8 @@
 // Entry point for the build script in your package.json
+import './src/turbolinks';
+import './src/call_list_drag_and_drop';
+import './src/clinic_finder';
+import './src/clinics';
+import './src/fontawesome';
+import './src/pledge_calculator';
+import './src/table_sorting';
