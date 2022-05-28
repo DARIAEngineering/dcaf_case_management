4584c3e4739c74f94f9f242c9a59a6cff45ddfb1
colinxfleming <c3flemin@gmail.com>
2022-05-28 15:52:16 -0400

    port tooltips js to es6

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -8,4 +8,5 @@ import './src/clinic_finder';
 import './src/clinics';
 import './src/fontawesome';
 import './src/pledge_calculator';
+import './src/tooltips';
 import './src/table_sorting';
---
+++ app/javascript/src/tooltips.js
@@ -0,0 +1,30 @@
+// Helper tooltips.
+const activateTooltips = () => {
+  // Regular spans
+  $('.daria-tooltip').tooltip();
+  console.log($('.daria-tooltip').tooltip());
+
+  // Some evil magic for things attached to labels with rails-bootstrap-form
+  $('.tooltip-header-input').append(' <span class="tooltip-header-help">(?)</span>');
+  $('.tooltip-header-input').parent('.form-group' ).find('.form-control').each((x) => {
+    $(x).parents( '.form-group' ).find( '.tooltip-header-help' ).tooltip({
+      html: true,
+      placement: 'bottom',
+      title: $(x).data('tooltip-text')
+    });
+  });
+
+  // Similarly evil magic for checkboxes
+  $('.tooltip-header-checkbox').append(' <span class="tooltip-header-help">(?)</span>')
+  $('.tooltip-header-checkbox').each((x) => {
+    let text = $(x).parent().find('input[type=checkbox]').data('tooltip-text');
+    $(x).parent().find( 'span.tooltip-header-help' ).tooltip({
+      html: true,
+      placement: 'bottom',
+      title: text,
+    });
+  });
+};
+
+
+$(document).on('turbolinks:load', activateTooltips);
