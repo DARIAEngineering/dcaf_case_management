1d1ff192af80154ef15d022e85bb59e8a2ee1e18
colinxfleming <c3flemin@gmail.com>
2022-05-28 16:26:17 -0400

    stash some autosave work

--- app/javascript/src/autosave.js
+++ app/javascript/src/autosave.js
@@ -8,32 +8,50 @@
 //   //   $("#toggle-call-log").html(html)
 
 
-// markFulfilledWhenFieldsChecked = ->
-//   pledge_fields = [
-//     '#patient_fulfillment_attributes_fund_payout'
-//     '#patient_fulfillment_attributes_check_number'
-//     '#patient_fulfillment_attributes_gestation_at_procedure'
-//     '#patient_fulfillment_attributes_date_of_check'
-//     '#patient_fulfillment_attributes_procedure_date'
-//   ]
+markFulfilledWhenFieldsChecked = ->
+  pledge_fields = [
+    '#patient_fulfillment_attributes_fund_payout'
+    '#patient_fulfillment_attributes_check_number'
+    '#patient_fulfillment_attributes_gestation_at_procedure'
+    '#patient_fulfillment_attributes_date_of_check'
+    '#patient_fulfillment_attributes_procedure_date'
+  ]
 
-//   i = 0
-//   empty = true
-//   el = $('#patient_fulfillment_attributes_fulfilled')
+  i = 0
+  empty = true
+  el = $('#patient_fulfillment_attributes_fulfilled')
 
-//   while i < pledge_fields.length
-//     if $(pledge_fields[i]).val().length > 0
-//       empty = false
-//       if el.prop 'checked'
-//         break;
-//       else
-//         el.prop 'checked', true
-//       i++
-//     else
-//       i++
-//   if empty == true
-//     el.prop 'checked', false
+  while i < pledge_fields.length
+    if $(pledge_fields[i]).val().length > 0
+      empty = false
+      if el.prop 'checked'
+        break;
+      else
+        el.prop 'checked', true
+      i++
+    else
+      i++
+  if empty == true
+    el.prop 'checked', false
 
+const fulfillmentFields = [
+  '#patient_fulfillment_attributes_fund_payout'
+  '#patient_fulfillment_attributes_check_number'
+  '#patient_fulfillment_attributes_gestation_at_procedure'
+  '#patient_fulfillment_attributes_date_of_check'
+  '#patient_fulfillment_attributes_procedure_date'
+]
+
+const _markFulfilledWhenFieldsChecked = () => {
+  const fulfillmentCheckbox = $('#patient_fulfillment_attributes_fulfilled');
+  fulfillmentFields.each((x) => {
+    const isChecked = $(x).val().length > 0;
+    if (isChecked) {
+      fulfillmentCheckbox.prop('checked', true);
+      break;
+    };
+  });
+};
 
 const activateAutosave = () => {
   // Main patient form
@@ -46,24 +64,21 @@ const activateAutosave = () => {
     $(this).submit();
   })
 
+  // Ext pledge form
+  $(document).on("change", ".edit_external_pledge", function() {
+    $(this).submit();
+  };
+
   // Fulfillment form
   // If any of the fields are chekced, mark the Fulfilled checkbox too
   $(document).on("change", "#pledge_fulfillment_form", function() {
-
-  }); 
-
-
+    markFulfilledWhenFieldsChecked();
+    $(this).submit();
+  });
 };
 
     
-
-  // $(document).on "change", "#pledge_fulfillment_form", ->
-  //   markFulfilledWhenFieldsChecked()
-  //   $(this).submit()
-
-  // $(document).on "change", ".edit_external_pledge", ->
-  //   $(this).submit()
-
+// COLIN: I don't know what this is. enforcing a signature?
   // $(document).on "submit", "#generate-pledge-form form", ->
   //   if($("#case_manager_name").val())
   //     true
