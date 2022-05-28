5d5ac13a1791f85edd1e205b8b3779bc101c4a02
colinxfleming <c3flemin@gmail.com>
2022-05-28 16:11:01 -0400

    port over multistep modal and part of autosave part one

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,10 +3,12 @@ import './src/jquery';
 import './src/jquery-ui';
 import './src/turbolinks';
 
+import './src/autosave';
 import './src/call_list_drag_and_drop';
 import './src/clinic_finder';
 import './src/clinics';
 import './src/fontawesome';
+import './src/multistep_modal';
 import './src/pledge_calculator';
-import './src/tooltips';
 import './src/table_sorting';
+import './src/tooltips';
