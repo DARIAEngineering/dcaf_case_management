0783b6426a522679ec9c73cec68f7b3a22c26c8e
colinxfleming <c3flemin@gmail.com>
2022-09-04 17:34:31 -0400

    don't really need this to render pages, it kicks it to errors controller regardless

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -3,21 +3,9 @@ class ErrorsController < ApplicationController
     true
   end
 
-  def error_404
-    respond_to do |format|
-      format.html { render status: 404 }
-      format.any  { render text: "404 Not Found", status: 404 }
-    end
-  end
+  def error_404; end
 
-  def error_422
-    respond_to do |format|
-      format.html { render status: 422 }
-      format.any  { render text: "422 Unprocessable Entity", status: 422 }
-    end
-  end
+  def error_422; end
 
-  def error_500
-    render text: 'Sorry, something went wrong! (500)'
-  end
+  def error_500; end
 end
