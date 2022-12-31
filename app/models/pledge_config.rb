9dc4c6016d2ff2aca625068846fd482c6b9e2259
colinxfleming <c3flemin@gmail.com>
2022-12-31 13:49:11 -0500

    strictly speaking, don't need all these fields anymore

--- app/models/pledge_config.rb
+++ app/models/pledge_config.rb
@@ -1,11 +1,6 @@
 class PledgeConfig < ApplicationRecord
   acts_as_tenant :fund
 
-  validates_presence_of :contact_email,
-                        :billing_email,
-                        :phone,
-                        :address1,
-                        :address2
   validates :contact_email, :billing_email, :phone, :logo_url, :address1, :address2,
             length: { maximum: 150 }
 end
--- test/models/pledge_config_test.rb
+++ test/models/pledge_config_test.rb
@@ -9,9 +9,13 @@ class PledgeConfigTest < ActiveSupport::TestCase
     end
 
     [:contact_email, :billing_email, :phone, :address1, :address2].each do |attrib|
-      it "should require #{attrib}" do
-        @config[attrib] = nil
+      it "should require #{attrib} to have length under 150 if set" do
+        @config[attrib] = 'a' * 151
         refute @config.valid?
+        @config[attrib] = 'a' * 150
+        assert @config.valid?
+        @config[attrib] = nil
+        assert @config.valid?
       end
     end
   end
