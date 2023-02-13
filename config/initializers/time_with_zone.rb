5504b85360563896546af0908d68a3c34ded35a4
Mercedes Bernard <mercedesrbernard@gmail.com>
2023-02-13 09:13:30 -0600

    fix typo for app config timezone

--- config/initializers/time_with_zone.rb
+++ config/initializers/time_with_zone.rb
@@ -2,12 +2,12 @@
 module ActiveSupport
   class TimeWithZone
     def display_date
-      tz = Config.time_zone || Rails.application.time_zone
+      tz = Config.time_zone || Rails.application.config.time_zone
       in_time_zone(tz).strftime("%m/%d/%Y")
     end
 
     def display_time
-      tz = Config.time_zone || Rails.application.time_zone
+      tz = Config.time_zone || Rails.application.config.time_zone
       in_time_zone(tz).strftime("%-l:%M %P")
     end
   end
