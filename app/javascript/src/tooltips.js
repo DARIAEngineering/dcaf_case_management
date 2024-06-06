7166f7577d54ff8a9cd52db66c92f23712b2958f
Mercedes Bernard <mercedesrbernard@gmail.com>
2024-06-06 09:01:48 -0500

    Tooltip component for displaying in form labels

--- app/javascript/src/tooltips.js
+++ app/javascript/src/tooltips.js
@@ -1,29 +1,37 @@
 // Helper tooltips.
-const activateTooltips = () => {
+export const activateTooltips = () => {
   // Regular spans
-  $('.daria-tooltip').tooltip();
-
+  $(".daria-tooltip").tooltip();
   // Some evil magic for things attached to labels with rails-bootstrap-form
-  $('.tooltip-header-input').append(' <span class="tooltip-header-help">(?)</span>');
-  $('.tooltip-header-input').parent('.form-group' ).find('.form-control').each((_, x) => {
-    $(x).parents( '.form-group' ).find( '.tooltip-header-help' ).tooltip({
-      html: true,
-      placement: 'bottom',
-      title: $(x).data('tooltip-text')
+  $(".tooltip-header-input:not(:has(span.tooltip-header-help))").append(
+    ' <span class="tooltip-header-help">(?)</span>'
+  );
+  $(".tooltip-header-input")
+    .parent(".form-group")
+    .find(".form-control")
+    .each((_, x) => {
+      $(x)
+        .parents(".form-group")
+        .find(".tooltip-header-help")
+        .tooltip({
+          html: true,
+          placement: "bottom",
+          title: $(x).data("tooltip-text"),
+        });
     });
-  });
 
   // Similarly evil magic for checkboxes
-  $('.tooltip-header-checkbox').append(' <span class="tooltip-header-help">(?)</span>')
-  $('.tooltip-header-checkbox').each((_, x) => {
-    let text = $(x).parent().find('input[type=checkbox]').data('tooltip-text');
-    $(x).parent().find( 'span.tooltip-header-help' ).tooltip({
+  $(".tooltip-header-checkbox:not(:has(span.tooltip-header-help))").append(
+    ' <span class="tooltip-header-help">(?)</span>'
+  );
+  $(".tooltip-header-checkbox").each((_, x) => {
+    let text = $(x).parent().find("input[type=checkbox]").data("tooltip-text");
+    $(x).parent().find("span.tooltip-header-help").tooltip({
       html: true,
-      placement: 'bottom',
+      placement: "bottom",
       title: text,
     });
   });
-
 };
 
-$(document).on('DOMContentLoaded', activateTooltips);
+$(document).on("DOMContentLoaded", activateTooltips);
