30adb56dd31cc9b9932771ef03f38228a72fff3e
colinxfleming <c3flemin@gmail.com>
2024-07-31 19:46:45 -0400

    roll out display text

--- app/models/practical_support.rb
+++ app/models/practical_support.rb
@@ -1,5 +1,7 @@
 # Representation of non-monetary assistance coordinated for a patient.
 class PracticalSupport < ApplicationRecord
+  include ActionView::Helpers::NumberHelper
+
   acts_as_tenant :fund
 
   encrypts :attachment_url
@@ -15,4 +17,12 @@ class PracticalSupport < ApplicationRecord
   validates :amount,
             allow_nil: true,
             numericality: { greater_than_or_equal_to: 0 }
+
+  def display_text
+    content = [support_type, "from #{source}"]
+    content.push("for #{number_to_currency(amount)}") if amount.present?
+    content.push('(confirmed)') if confirmed?
+    content.push("on #{support_date.display_date}") if support_date.present?
+    content.join(' ')
+  end
 end
