192f15a1ff2cee21efcdf819c2e066b91b1e06e6
colinxfleming <c3flemin@gmail.com>
2024-08-02 05:23:59 -0400

    test fix

--- test/helpers/practical_supports_helper_test.rb
+++ test/helpers/practical_supports_helper_test.rb
@@ -169,7 +169,7 @@ class PracticalSupportsHelperTest < ActionView::TestCase
 
     it 'should display' do
       assert_equal 'Concert Tickets from Metallica Abortion Fund', practical_support_display_text(@psupport1)
-      assert_equal "Swag from YOLO AF (confirmed) on #{2.days.from_now.display_date}", practical_support_display_text(@psupport2)
+      assert_equal "(Confirmed) (Fulfilled) Swag from YOLO AF on #{2.days.from_now.display_date}", practical_support_display_text(@psupport2)
       assert_equal 'Companion from Cat for $32.00', practical_support_display_text(@psupport3)
     end
   end
