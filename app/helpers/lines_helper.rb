53e0b4615382aebc8dfd4202d0d881feeb8c57f1
colinxfleming <c3flemin@gmail.com>
2024-09-06 21:27:05 -0400

    if 2+ lines add a switcher

--- app/helpers/lines_helper.rb
+++ app/helpers/lines_helper.rb
@@ -1,10 +1,22 @@
 # Convenience function for displaying lines around the app.
 module LinesHelper
   def current_line_display
+    return if !session[:line_id]
+
+    # If multiple lines, link to the switcher
+    if Line.count > 2
+      return content_tag :li do
+        link_to t('navigation.current_line.helper') + ": #{current_line.name}",
+                new_line_path,
+                class: 'nav-link navbar-text-alt'
+      end
+    end
+
+    # Otherwise just display the content
     content_tag :li do
       content_tag :span, t('navigation.current_line.helper') + ": #{current_line.name}",
                          class: 'nav-link navbar-text-alt'
-    end if session[:line_id]
+    end
   end
 
   def current_line
