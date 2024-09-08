9f5d8ecf2b47cab181bf585e6213d993346e7ff4
Colin <c3flemin@gmail.com>
2024-09-08 00:28:39 -0400

    In navbar, if 2+ lines add a link to the line switcher (#3272)
    * if 2+ lines add a switcher
    
    * ha 1+

--- test/helpers/lines_helper_test.rb
+++ test/helpers/lines_helper_test.rb
@@ -4,25 +4,34 @@ class LinesHelperTest < ActionView::TestCase
   include ERB::Util
 
   describe 'convenience methods' do
-    before do
+    it 'should return empty if not set' do
+      assert_nil current_line
+      assert_nil current_line_display
+    end
+
+    it 'should show a link if 2+ lines' do
       @lines = [
         create(:line, name: 'DC'),
         create(:line, name: 'MD'),
         create(:line, name: 'VA')
       ]
-    end
-
-    it 'should return current line' do
-      assert_nil current_line
-      assert_nil current_line_display
 
       @lines.each do |line|
         session[:line_id] = line.id
         session[:line_name] = line.name
         assert_equal current_line_display,
-                     "<li><span class=\"nav-link navbar-text-alt\">Your current line: #{session[:line_name]}</span></li>"
+                     "<li><a class=\"nav-link navbar-text-alt\" href=\"/lines/new\">Your current line: #{session[:line_name]}</a></li>"
         assert_equal current_line, line
       end
+    end
+
+    it 'should show text if just one line' do
+      line = create(:line, name: 'DC')
+      session[:line_id] = line.id
+      session[:line_name] = line.name
+      assert_equal current_line_display,
+                    "<li><span class=\"nav-link navbar-text-alt\">Your current line: #{session[:line_name]}</span></li>"
+      assert_equal current_line, line
     end
   end
 end
