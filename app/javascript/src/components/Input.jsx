8b69d2ac2336ed9a627e274b22969eb4087eb67b
Mercedes Bernard <mercedesrbernard@gmail.com>
2025-02-25 19:18:34 -0600

    allow Input to behave either like a controlled or uncontrolled component

--- app/javascript/src/components/Input.jsx
+++ app/javascript/src/components/Input.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default Input = ({
   label,
@@ -18,6 +18,13 @@ export default Input = ({
   const [value, setValue] = useState(initialValue || "")
   const labelClassNames = `${required ? 'required' : ''} ${labelClassName || ''}`
   const inputClassNames = `form-control ${className || ''} ${labelClassNames.includes('sr-only') ? 'mt-6' : ''}`
+
+  // if we have an onChange handler, we'll manage state like a traditional controlled component (and don't need the useEffect)
+  // if we don't have an onChange handler, this component behaves more like an uncontrolled component
+  // the useEffect lets us always get the value from the prop
+  if (!onChange) {
+    useEffect(() => setValue(initialValue), [initialValue]);
+  }
 
   const handleChange = (e) => {
     setValue(e.target.value)
