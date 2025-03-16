5eab602b1276e2e27e90d9e9517d46141ad1f1ff
Mercedes Bernard <mercedesrbernard@gmail.com>
2025-03-16 12:05:15 -0500

    use correct class name to offset select with sr-only label

--- app/javascript/src/components/Select.jsx
+++ app/javascript/src/components/Select.jsx
@@ -14,7 +14,8 @@ export default Select = ({
   ...props
 }) => {
   const labelClassNames = `${required ? 'required' : ''} ${labelClassName || ''}`
-  const selectClassNames = `form-control ${className || ''} ${labelClassNames.includes('sr-only') ? 'mt-6' : ''}`
+
+  const selectClassNames = `form-control ${className || ''} ${labelClassNames.includes('sr-only') ? 'mt-4' : ''}`
 
   const selectedValue = !!value || value === 0 ? value : ""
   if (!!onChange) {
