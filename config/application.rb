a4e8d2dccb5845bbca5b654f3919eedfe741df93
colin <c3flemin@gmail.com>
2025-10-26 00:35:31 -0400

    MEANINGFUL: application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,54 +1,20 @@
 require_relative "boot"
 
-# We require individual items instead of rails/all because 
-# we don't need ActiveRecord, and ActiveRecord freaks out
-# if there's no connection pool. -CF
-
-require "rails"
-# Pick the frameworks you want:
-require "active_model/railtie"
-# require "active_job/railtie"
-require "active_record/railtie"
-# require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-# require "action_mailbox/engine"
-# require "action_text/engine"
-require "action_view/railtie"
-# require "action_cable/engine"
-require "sprockets/railtie"
-require "rails/test_unit/railtie"
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module DARIA
+module Daria
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.load_defaults 8.1
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = 'Eastern Time (US & Canada)' # this is bad. Should hardset to UTC and find some way to programmatically determine timezone based on user.
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
-    config.generators do |g|
-      g.orm :active_record
-    end
-
-    # Throttling protection
-    config.middleware.use Rack::Attack
-
-    # Force JSON cookies for security reasons
-    config.action_dispatch.cookies_serializer = :json
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -58,8 +24,12 @@ module DARIA
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    # Customs
     # Custom exceptions pages
     config.exceptions_app = self.routes
+
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    config.time_zone = 'Eastern Time (US & Canada)' # this is bad. Should hardset to UTC and find some way to programmatically determine timezone based on user.
 
     # temporary, can be removed in separate release once data migration for encrypted data is complete
     config.active_record.encryption.support_unencrypted_data = true
