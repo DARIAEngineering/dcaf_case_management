d1ebf2410d0371704596ac2b6027d7eca509748d
colin <c3flemin@gmail.com>
2025-11-29 01:44:54 -0500

    install cache/cable/queue migrations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,10 +48,11 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  # config.cache_store = :solid_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
-  # config.active_job.queue_adapter     = :resque
+  # config.active_job.queue_adapter = :solid_queue
+  # config.solid_queue.connects_to = { database: { writing: :queue } }
   # config.active_job.queue_name_prefix = "dcaf_case_management_production"
 
   config.action_mailer.perform_caching = false
