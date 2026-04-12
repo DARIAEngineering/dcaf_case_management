05b0830d4d334cd01619266221c4a61b49bd76a3
Rolling2405 <89894749+Rolling2405@users.noreply.github.com>
2026-04-12 15:51:48 -0700

    Revert "Migrate jQuery Bootstrap 4 plugin calls to Bootstrap 5 API"
    This reverts commit 22a6f31eaa70a8acf60a5a805a9b98f92f4513cc.

--- app/javascript/src/tooltips.js
+++ app/javascript/src/tooltips.js
@@ -1,35 +1,27 @@
-// Helper tooltips - uses Bootstrap 5 Tooltip API.
+// Helper tooltips.
 export const activateTooltips = () => {
   // Regular spans
-  document.querySelectorAll('.daria-tooltip').forEach(el => {
-    if (!bootstrap.Tooltip.getInstance(el)) new bootstrap.Tooltip(el);
-  });
+  $('.daria-tooltip').tooltip();
 
   // Some evil magic for things attached to labels with rails-bootstrap-form
   $(".tooltip-header-input:not(:has(span.tooltip-header-help))").append(' <span class="tooltip-header-help">(?)</span>');
-  $('.tooltip-header-input').parent('.form-group').find('.form-control').each((_, x) => {
-    var helpEl = $(x).parents('.form-group').find('.tooltip-header-help')[0];
-    if (helpEl && !bootstrap.Tooltip.getInstance(helpEl)) {
-      new bootstrap.Tooltip(helpEl, {
-        html: true,
-        placement: 'bottom',
-        title: $(x).data('tooltip-text')
-      });
-    }
+  $('.tooltip-header-input').parent('.form-group' ).find('.form-control').each((_, x) => {
+    $(x).parents( '.form-group' ).find( '.tooltip-header-help' ).tooltip({
+      html: true,
+      placement: 'bottom',
+      title: $(x).data('tooltip-text')
+    });
   });
 
   // Similarly evil magic for checkboxes
-  $(".tooltip-header-checkbox:not(:has(span.tooltip-header-help))").append(' <span class="tooltip-header-help">(?)</span>');
+  $(".tooltip-header-checkbox:not(:has(span.tooltip-header-help))").append(' <span class="tooltip-header-help">(?)</span>')
   $('.tooltip-header-checkbox').each((_, x) => {
     let text = $(x).parent().find('input[type=checkbox]').data('tooltip-text');
-    var helpEl = $(x).parent().find('span.tooltip-header-help')[0];
-    if (helpEl && !bootstrap.Tooltip.getInstance(helpEl)) {
-      new bootstrap.Tooltip(helpEl, {
-        html: true,
-        placement: 'bottom',
-        title: text,
-      });
-    }
+    $(x).parent().find( 'span.tooltip-header-help' ).tooltip({
+      html: true,
+      placement: 'bottom',
+      title: text,
+    });
   });
 
 };
